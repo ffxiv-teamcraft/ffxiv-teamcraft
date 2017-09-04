5a4eb3b2ce4d963d2993e32ce73932255eab4af8
Supamiu <contact@flavien-normand.fr>
2017-09-04 11:28:17 +0200

    chore: google oauth user registration ok

--- src/app/popup/register-popup/register-popup.component.ts
+++ src/app/popup/register-popup/register-popup.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import {MdDialogRef} from '@angular/material';
-import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
+import {AngularFireDatabase} from 'angularfire2/database';
 import GoogleAuthProvider = firebase.auth.GoogleAuthProvider;
 
 @Component({
@@ -38,15 +38,17 @@ export class RegisterPopupComponent implements OnInit {
 
     googleOauth(): void {
         this.af.auth.signInWithPopup(new GoogleAuthProvider()).then((oauth) => {
-            const userRef = this.firebase.object(`/users/${oauth.user.uid}`);
-            userRef.subscribe(user => {
-                userRef.set({
-                    email: oauth.user.email,
-                    lists: this.lists
-                });
-                console.log(this.anonymous);
-                this.firebase.object(this.anonymous).remove();
-                this.dialogRef.close();
+            const userRef = this.firebase.database.ref(`/users/${oauth.user.uid}`);
+            userRef.once('value').then(snap => {
+                if (snap.val() === null) {
+                    userRef.set({
+                        email: oauth.user.email,
+                        lists: this.lists
+                    });
+                    this.dialogRef.close();
+                } else {
+                    // TODO error message
+                }
             });
         });
     }
