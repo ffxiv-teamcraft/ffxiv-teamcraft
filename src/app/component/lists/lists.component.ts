9dcde4109285f5f836d5878b198cfc25d8c8d6b5
Supamiu <contact@flavien-normand.fr>
2017-09-24 16:30:34 +0200

    style: lists now collapsed by default

--- src/app/component/lists/lists.component.ts
+++ src/app/component/lists/lists.component.ts
@@ -26,6 +26,8 @@ export class ListsComponent implements OnInit {
 
     @ViewChild('f') myNgForm;
 
+    expanded: string;
+
     constructor(private auth: AngularFireAuth,
                 private dialog: MdDialog, private listManager: ListManagerService,
                 private listService: ListService, private title: Title) {
