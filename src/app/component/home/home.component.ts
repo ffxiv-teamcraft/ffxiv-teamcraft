3847ae9ef95f1db1c4ada4e28b79c641f94aa260
Supamiu <contact@flavien-normand.fr>
2017-09-30 23:02:53 +0200

    chore: [WIP] homepage

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFireDatabase} from 'angularfire2/database';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+    list_count = this.firebase.object('/list_count');
 
-  ngOnInit() {
-  }
+    constructor(private firebase: AngularFireDatabase) {
+    }
 
 }
