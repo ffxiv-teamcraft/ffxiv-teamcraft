b4036deb3914fe09d5dcb76711e35a894e77d778
Supamiu <contact@flavien-normand.fr>
2017-10-05 16:59:15 +0200

    chore: WIP eorzean time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import {LoginPopupComponent} from './component/popup/login-popup/login-popup.com
 import {CharacterAddPopupComponent} from './component/popup/character-add-popup/character-add-popup.component';
 import {UserService} from './core/user.service';
 import {environment} from '../environments/environment';
+import {EorzeanTimeService} from './core/time/eorzean-time.service';
 import Persistence = firebase.auth.Auth.Persistence;
 
 declare const ga: Function;
@@ -48,7 +49,8 @@ export class AppComponent implements OnInit {
                 private dialog: MdDialog,
                 private firebase: AngularFireDatabase,
                 private userService: UserService,
-                private snack: MdSnackBar) {
+                private snack: MdSnackBar,
+                private eorzeanTimeService: EorzeanTimeService) {
         // Google Analytics
         router.events.distinctUntilChanged((previous: any, current: any) => {
             if (current instanceof NavigationEnd) {
@@ -85,6 +87,10 @@ export class AppComponent implements OnInit {
             .subscribe(announcement => {
                 this.announcement = announcement;
             });
+    }
+
+    getETime(): Observable<Date> {
+        return this.eorzeanTimeService.getEorzeanTime();
     }
 
     closeSnack(): void {
