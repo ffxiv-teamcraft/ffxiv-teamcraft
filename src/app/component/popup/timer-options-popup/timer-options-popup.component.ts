363d3816b5f8f5b3ecd546b753ff116fc688ce2d
Supamiu <contact@flavien-normand.fr>
2017-10-10 00:59:34 +0200

    chore: bad access modifier for options

--- src/app/component/popup/timer-options-popup/timer-options-popup.component.ts
+++ src/app/component/popup/timer-options-popup/timer-options-popup.component.ts
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 })
 export class TimerOptionsPopupComponent {
 
-    private options: any = {
+    public options: any = {
         sound: 'Wondrous_tales',
         hoursBefore: 0
     };
