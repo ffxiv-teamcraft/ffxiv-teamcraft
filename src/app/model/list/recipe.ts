f7ec8e7c8f9c31e09c1f095779beba6e4844ca48
Supamiu <contact@flavien-normand.fr>
2017-10-17 13:48:06 +0200

    chore: fix production build

--- src/app/model/list/recipe.ts
+++ src/app/model/list/recipe.ts
@@ -2,6 +2,8 @@ import {CompactMasterbook} from './compact-masterbook';
 import {I18nName} from './i18n-name';
 
 export interface Recipe {
+    // We sometimes have an id property, TODO: remove that and its attributions.
+    id?: number;
     amount?: number;
     recipeId: string;
     itemId: number;
