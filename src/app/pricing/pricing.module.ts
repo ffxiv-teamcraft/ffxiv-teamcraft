11713ef0cc93ea7e8790801d738aeaf57d80dc2a
Supamiu <contact@flavien-normand.fr>
2017-10-18 14:32:49 +0200

    feat: see price for a given craft in pricing view
    Closes #68;

--- src/app/pricing/pricing.module.ts
+++ src/app/pricing/pricing.module.ts
@@ -2,12 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PricingComponent} from './pricing/pricing.component';
 import {PricingService} from './pricing.service';
-import {MdButtonModule, MdCardModule, MdExpansionModule, MdIconModule, MdInputModule, MdListModule} from '@angular/material';
+import {
+    MdButtonModule,
+    MdCardModule,
+    MdExpansionModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdTooltipModule
+} from '@angular/material';
 import {CoreModule} from '../core/core.module';
-import { PricingRowComponent } from './pricing-row/pricing-row.component';
+import {PricingRowComponent} from './pricing-row/pricing-row.component';
 import {FormsModule} from '@angular/forms';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {ItemNamePipe} from '../pipe/item-name.pipe';
 import {PipesModule} from '../pipe/pipes.module';
 
 @NgModule({
@@ -20,6 +27,7 @@ import {PipesModule} from '../pipe/pipes.module';
         MdInputModule,
         MdButtonModule,
         MdIconModule,
+        MdTooltipModule,
         FormsModule,
         FlexLayoutModule,
         PipesModule,
