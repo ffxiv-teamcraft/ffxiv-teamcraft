b0f654bce423114485c2286ac988103e1384d7e2
Supamiu <contact@flavien-normand.fr>
2017-10-26 11:18:34 +0200

    feat: IE 10 & 11, Safari 7 & 8 support
    This was done is the previous commit and I commited it by mistake, just doing a dummy commit for the changelog.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -44,7 +44,6 @@ import 'web-animations-js';  // Run `npm install --save web-animations-js`.
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
 
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
