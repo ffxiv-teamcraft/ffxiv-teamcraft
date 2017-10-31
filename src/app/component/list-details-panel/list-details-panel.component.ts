4ecd43385afc46f2877c89e27ef0c71c61356dda
Supamiu <contact@flavien-normand.fr>
2017-10-31 17:57:11 +0100

    feat: add precraft tier display toggle

--- src/app/component/list-details-panel/list-details-panel.component.ts
+++ src/app/component/list-details-panel/list-details-panel.component.ts
@@ -1,9 +1,10 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {ListRow} from '../../model/list/list-row';
 import {List} from '../../model/list/list';
 
 @Component({
     selector: 'app-list-details-panel',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './list-details-panel.component.html',
     styleUrls: ['./list-details-panel.component.scss']
 })
@@ -21,10 +22,45 @@ export class ListDetailsPanelComponent {
     @Input()
     recipe = false;
 
+    @Input()
+    showTier = false;
+
     @Output()
     done: EventEmitter<number> = new EventEmitter<number>();
 
     @Output()
     update: EventEmitter<void> = new EventEmitter<void>();
+
+    tierBreakdownToggle = false;
+
+    /**
+     * Returns a list of tiers based on dependencies between each list row.
+     * each tier is a list of rows.
+     * @returns {ListRow[][]}
+     */
+    public getTiers(): ListRow[][] {
+        let result = [[]];
+        this.data.forEach(row => {
+            result = this.setTier(row, result);
+        });
+        return result;
+    }
+
+    private setTier(row: ListRow, result: ListRow[][], tier = 0): ListRow[][] {
+        if (result[tier] === undefined) {
+            result[tier] = [];
+        }
+        for (const requirement of row.requires) {
+            if (result[tier].find(r => r.id === requirement.id) !== undefined) {
+                return this.setTier(row, result, tier + 1);
+            }
+        }
+        result[tier].push(row);
+        return result;
+    }
+
+    public toggleTierBreakdown(): void {
+        this.tierBreakdownToggle = !this.tierBreakdownToggle;
+    }
 
 }
