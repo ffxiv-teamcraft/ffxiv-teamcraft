a6442efb17a4f639909738eb15b4a1c558bbe76d
AltanRapsied <34192226+AltanRapsied@users.noreply.github.com>
2017-12-04 17:13:48 +0100

    feature: addition and reordering of tags

--- src/app/model/list/list-tag.enum.ts
+++ src/app/model/list/list-tag.enum.ts
@@ -1,6 +1,9 @@
 export enum ListTag {
     // Theme tags
+    GEAR = 'GEAR',
+    CONSUMABLES = 'CONSUMABLES',
     LEVE = 'LEVE',
+    COMPLETION = 'COMPLETION',
     GLAMOUR = 'GLAMOUR',
     HOUSING = 'HOUSING',
     GEAR = 'GEAR',
@@ -8,22 +11,34 @@ export enum ListTag {
     MISC = 'MISC',
 
     // EQUIPPED BY tags
+    TANK = 'TANK',
+    HEAL = 'HEAL',
+    MELEE = 'MELEE',
+    RANGED = 'RANGED',
+    CASTER = 'CASTER',
+    CRAFTER = 'CRAFTER',
+    GATHERER = 'GATHERER',
+
     PLD = 'PLD',
     WAR = 'WAR',
     DRK = 'DRK',
+
     WHM = 'WHM',
     SCH = 'SCH',
     AST = 'AST',
+
     MNK = 'MNK',
     DRG = 'DRG',
     NIN = 'NIN',
     SAM = 'SAM',
+
     BRD = 'BRD',
     MCH = 'MCH',
+
     BLM = 'BLM',
     SMN = 'SMN',
     RDM = 'RDM',
-
+  
     MIN = 'MIN',
     BOT = 'BOT',
     FSH = 'FSH',
@@ -33,7 +48,16 @@ export enum ListTag {
     BSM = 'BSM',
     CRP = 'CRP',
     CUL = 'CUL',
+    BSM = 'BSM',
+    ARM = 'ARM',
     GSM = 'GSM',
     LTW = 'LTW',
     WVR = 'WVR'
+    WVR = 'WVR',
+    ALC = 'ALC',
+    CUL = 'CUL',
+  
+    MIN = 'MIN',
+    BOT = 'BOT',
+    FSH = 'FSH'
 }
