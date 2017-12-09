d051c93867ca240793070bcfa9f7042fd0239f48
Supamiu <contact@flavien-normand.fr>
2017-12-09 13:32:42 +0100

    fix: error when public list author is anonymous

--- src/app/modules/common-components/list-panel/list-panel.component.ts
+++ src/app/modules/common-components/list-panel/list-panel.component.ts
@@ -87,7 +87,7 @@ export class ListPanelComponent extends ComponentWithSubscriptions implements On
     }
 
     ngOnInit(): void {
-        this.author = this.userService.getCharacter(this.list.authorId);
+        this.author = this.userService.getCharacter(this.list.authorId).catch(err => Observable.of(null));
     }
 
     public isMobile(): boolean {
