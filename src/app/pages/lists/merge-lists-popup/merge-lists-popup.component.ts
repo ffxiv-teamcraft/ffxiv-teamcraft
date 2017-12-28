eae87fbc8b902fdc2b0f859cabb8034db67920c4
Supamiu <contact@flavien-normand.fr>
2017-12-28 20:09:53 +0100

    feat: add list merging
    closes #116

---
+++ src/app/pages/lists/merge-lists-popup/merge-lists-popup.component.ts
@@ -0,0 +1,36 @@
+import {Component, Inject, ViewChild} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef, MatSelectionList} from '@angular/material';
+import {List} from '../../../model/list/list';
+import {ListService} from '../../../core/database/list.service';
+
+@Component({
+    selector: 'app-merge-lists-popup',
+    templateUrl: './merge-lists-popup.component.html',
+    styleUrls: ['./merge-lists-popup.component.scss']
+})
+export class MergeListsPopupComponent {
+
+    @ViewChild('selectedLists') selectionList: MatSelectionList;
+
+    listName = '';
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { lists: List[], authorId: string }, private listService: ListService,
+                private dialogRef: MatDialogRef<MergeListsPopupComponent>) {
+    }
+
+    /**
+     * Creates a new empty list to merge it with all the lists selected.
+     */
+    merge(): void {
+        const resultList = new List();
+        resultList.name = this.listName;
+        resultList.authorId = this.data.authorId;
+        this.selectionList.selectedOptions.selected.forEach(option => {
+            resultList.merge(option.value);
+        });
+        this.listService.add(resultList).first().subscribe(() => {
+            this.dialogRef.close();
+        });
+    }
+
+}
