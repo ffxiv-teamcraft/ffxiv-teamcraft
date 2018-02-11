1329a69f3dad4d76bbeb8e4fa1d020ac1e5a4a55
Supamiu <contact@flavien-normand.fr>
2018-02-11 22:51:22 +0100

    refactor: switch storage to firestore database

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
-'use strict';
-
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+require('firebase/firestore');
 admin.initializeApp(functions.config().firebase);
 
 exports.countlistsCreate = functions.database.ref('/lists/{uid}').onCreate(() => {
@@ -22,3 +21,24 @@ exports.countlistsDelete = functions.database.ref('/lists/{uid}').onDelete(() =>
         return current - 1;
     }).then(() => null);
 });
+
+// Firestore counts
+exports.firestoreCountlistsCreate = functions.firestore.document('/lists/{uid}').onCreate(() => {
+    const ref = admin.database().ref('/list_count');
+    const creationsRef = admin.database().ref('/lists_created');
+    // Increment the number of lists created using the tool.
+    creationsRef.transaction(current => {
+        return current + 1;
+    }).then(() => null);
+    return ref.transaction(current => {
+        return current + 1;
+    }).then(() => null);
+});
+
+exports.firestoreCountlistsDelete = functions.firestore.document('/lists/{uid}').onDelete(() => {
+    const ref = admin.database().ref('/list_count');
+    return ref.transaction(current => {
+        return current - 1;
+    }).then(() => null);
+});
+
---
+++ src/app/core/database/storage/firebase/firestore-storage.ts
@@ -0,0 +1,67 @@
+import {DataModel} from '../data-model';
+import {DataStore} from '../data-store';
+import {Observable} from 'rxjs/Observable';
+import {NgSerializerService} from '@kaiu/ng-serializer';
+import {NgZone} from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
+import * as firebase from 'firebase';
+import {Action} from 'angularfire2/firestore/interfaces';
+import DocumentReference = firebase.firestore.DocumentReference;
+import DocumentSnapshot = firebase.firestore.DocumentSnapshot;
+
+export abstract class FirestoreStorage<T extends DataModel> extends DataStore<T> {
+
+    constructor(protected firestore: AngularFirestore, protected serializer: NgSerializerService, protected zone: NgZone) {
+        super();
+    }
+
+    add(data: T): Observable<string> {
+        return Observable.fromPromise(this.firestore.collection(this.getBaseUri()).add(JSON.parse(JSON.stringify(data))))
+            .map((ref: DocumentReference) => {
+                return ref.id;
+            });
+    }
+
+    get(uid: string): Observable<T> {
+        return this.firestore.collection(this.getBaseUri()).doc(uid).snapshotChanges()
+            .map((snap: Action<DocumentSnapshot>) => {
+                const valueWithKey: T = <T>{$key: snap.payload.id, ...snap.payload.data()};
+                if (!snap.payload.exists) {
+                    throw new Error('Not found');
+                }
+                delete snap.payload;
+                return this.serializer.deserialize<T>(valueWithKey, this.getClass());
+            })
+            .debounceTime(50)
+            .publishReplay(1)
+            .refCount();
+    }
+
+    update(uid: string, data: T): Observable<void> {
+        return this.zone.runOutsideAngular(() => {
+            if (uid === undefined || uid === null || uid === '') {
+                throw new Error('Empty uid');
+            }
+            return Observable.fromPromise(this.firestore.collection(this.getBaseUri()).doc(uid).update(JSON.parse(JSON.stringify(data))));
+        });
+    }
+
+    set(uid: string, data: T): Observable<void> {
+        return this.zone.runOutsideAngular(() => {
+            if (uid === undefined || uid === null || uid === '') {
+                throw new Error('Empty uid');
+            }
+            return Observable.fromPromise(this.firestore.collection(this.getBaseUri()).doc(uid).set(JSON.parse(JSON.stringify(data))));
+        });
+    }
+
+    remove(uid: string): Observable<void> {
+        return this.zone.runOutsideAngular(() => {
+            if (uid === undefined || uid === null || uid === '') {
+                throw new Error('Empty uid');
+            }
+            return Observable.fromPromise(this.firestore.collection(this.getBaseUri()).doc(uid).delete());
+        });
+    }
+
+}
