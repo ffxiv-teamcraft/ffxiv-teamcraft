8d29b667e1956526f9c9b18c147e9b4388987edb
Supamiu <contact@flavien-normand.fr>
2018-02-13 21:29:57 +0100

    chore: update hooks to disable firebase ones

--- functions/index.js
+++ functions/index.js
@@ -4,27 +4,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-exports.countlistsCreate = functions.database.ref('/lists/{uid}').onCreate(() => {
-    const ref = admin.database().ref('/list_count');
-    const creationsRef = admin.database().ref('/lists_created');
-    // Increment the number of lists created using the tool.
-    creationsRef.transaction(current => {
-        return current + 1;
-    }).then(() => null);
-    return ref.transaction(current => {
-        return current + 1;
-    }).then(() => null);
-});
-
-// exports.countlistsDelete = functions.database.ref('/lists/{uid}').onDelete(() => {
-//     const ref = admin.database().ref('/list_count');
-//     return ref.transaction(current => {
-//         return current - 1;
-//     }).then(() => null);
-// });
-//
-// // Firestore counts
-// exports.firestoreCountlistsCreate = functions.firestore.document('/lists/{uid}').onCreate(() => {
+//Firebase counts
+// exports.countlistsCreate = functions.database.ref('/lists/{uid}').onCreate(() => {
 //     const ref = admin.database().ref('/list_count');
 //     const creationsRef = admin.database().ref('/lists_created');
 //     // Increment the number of lists created using the tool.
@@ -35,6 +16,26 @@ exports.countlistsCreate = functions.database.ref('/lists/{uid}').onCreate(() =>
 //         return current + 1;
 //     }).then(() => null);
 // });
+//
+// exports.countlistsDelete = functions.database.ref('/lists/{uid}').onDelete(() => {
+//     const ref = admin.database().ref('/list_count');
+//     return ref.transaction(current => {
+//         return current - 1;
+//     }).then(() => null);
+// });
+
+// Firestore counts
+exports.firestoreCountlistsCreate = functions.firestore.document('/lists/{uid}').onCreate(() => {
+    const ref = admin.database().ref('/list_count');
+    const creationsRef = admin.database().ref('/lists_created');
+    // Increment the number of lists created using the tool.
+    creationsRef.transaction(current => {
+        return current + 1;
+    }).then(() => null);
+    return ref.transaction(current => {
+        return current + 1;
+    }).then(() => null);
+});
 
 exports.firestoreCountlistsDelete = functions.firestore.document('/lists/{uid}').onDelete(() => {
     const ref = admin.database().ref('/list_count');
