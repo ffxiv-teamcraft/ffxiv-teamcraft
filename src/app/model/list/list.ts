332f2460493d5ea3d739266260810e9b5b620fc9
Supamiu <contact@flavien-normand.fr>
2018-02-25 19:51:30 +0100

    fix: item marked as not craftable while every requirement is met

--- src/app/model/list/list.ts
+++ src/app/model/list/list.ts
@@ -344,7 +344,8 @@ export class List extends DataModel {
             if (requirementItem !== undefined) {
                 // While each requirement has enough items remaining, you can craft the item.
                 // If only one misses, then this will turn false for the rest of the loop
-                canCraft = canCraft && (requirementItem.done - requirementItem.used) >= requirement.amount * item.amount_needed;
+                canCraft = canCraft &&
+                    (requirementItem.done - requirementItem.used) >= requirement.amount * (item.amount_needed - item.done);
             }
         }
         return canCraft;
