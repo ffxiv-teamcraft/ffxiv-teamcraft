fc4524c4505be14862355f0fd1d2e2e061237785
Supamiu <contact@flavien-normand.fr>
2018-03-12 22:32:35 +0100

    fix: wrong amount used in some crafts for canBeCrafted flag

--- src/app/model/list/list.ts
+++ src/app/model/list/list.ts
@@ -345,7 +345,7 @@ export class List extends DataModel {
                 // While each requirement has enough items remaining, you can craft the item.
                 // If only one misses, then this will turn false for the rest of the loop
                 canCraft = canCraft &&
-                    (requirementItem.done - requirementItem.used) >= requirement.amount * (item.amount_needed - item.done);
+                    (requirementItem.done - requirementItem.used) >= requirement.amount * (item.amount_needed - (item.done / item.yield));
             }
         }
         return canCraft;
