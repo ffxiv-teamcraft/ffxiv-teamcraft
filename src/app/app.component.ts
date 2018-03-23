c405537264610ed573060675f464dc8b514edfb6
Supamiu <contact@flavien-normand.fr>
2018-03-23 10:41:58 +0100

    fix: error when logging out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -197,7 +197,7 @@ export class AppComponent implements OnInit {
             }
             // Anonymous sign in with "please register" snack.
             this.auth.authState.debounceTime(1000).subscribe(state => {
-                if (state ! == null && state.isAnonymous && !this.isRegistering) {
+                if (state !== null && state.isAnonymous && !this.isRegistering) {
                     this.registrationSnackRef = this.snack.open(
                         this.translate.instant('Anonymous_Warning'),
                         this.translate.instant('Registration'),
