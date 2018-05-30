c4577b48b2c81acbb6dc75e262fd64919debb20d
Supamiu <contact@flavien-normand.fr>
2018-05-30 18:21:21 +0200

    feat: current list shown inside bulk regeneration popup
    closes #384

--- src/app/pages/lists/bulk-regenerate-popup/bulk-regenerate-popup.component.ts
+++ src/app/pages/lists/bulk-regenerate-popup/bulk-regenerate-popup.component.ts
@@ -1,11 +1,11 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {ListService} from '../../../core/database/list.service';
-import {concat, Observable} from 'rxjs';
+import {concat, Observable, of} from 'rxjs';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {ComponentWithSubscriptions} from '../../../core/component/component-with-subscriptions';
 import {ListManagerService} from '../../../core/list/list-manager.service';
 import {List} from '../../../model/list/list';
-import {filter, switchMap, tap} from 'rxjs/operators';
+import {filter, mergeMap, switchMap, tap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-bulk-regenerate-popup',
@@ -16,6 +16,8 @@ export class BulkRegeneratePopupComponent extends ComponentWithSubscriptions imp
 
     progress = 0;
 
+    currentList: string;
+
     constructor(@Inject(MAT_DIALOG_DATA) public data: any,
                 public dialogRef: MatDialogRef<BulkRegeneratePopupComponent>,
                 private listService: ListService, private listManager: ListManagerService) {
@@ -25,8 +27,10 @@ export class BulkRegeneratePopupComponent extends ComponentWithSubscriptions imp
     ngOnInit(): void {
         let done = 0;
         const regenerations: Observable<List>[] = this.data.map(list => {
-            return this.listManager.upgradeList(list)
+            return of(list)
                 .pipe(
+                    tap(l => this.currentList = l.name),
+                    mergeMap(l => this.listManager.upgradeList(l)),
                     switchMap((l: List) => this.listService.set(l.$key, l))
                 );
         });
