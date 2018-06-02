dadf1bf7f4cc684d4d1c80e9aa287dd1d002da3a
Supamiu <contact@flavien-normand.fr>
2018-06-02 16:21:51 +0200

    feat: control is now properly handled in min stats popup

--- src/app/pages/simulator/model/tables.ts
+++ src/app/pages/simulator/model/tables.ts
@@ -1,5 +1,17 @@
 export class Tables {
 
+    public static readonly HQ_TABLE = [
+        1, 1, 1, 1, 1, 2, 2, 2, 2, 3,
+        3, 3, 3, 4, 4, 4, 4, 5, 5, 5,
+        5, 6, 6, 6, 6, 7, 7, 7, 7, 8,
+        8, 8, 9, 9, 9, 10, 10, 10, 11, 11,
+        11, 12, 12, 12, 13, 13, 13, 14, 14, 14,
+        15, 15, 15, 16, 16, 17, 17, 17, 18, 18,
+        18, 19, 19, 20, 20, 21, 22, 23, 24, 26,
+        28, 31, 34, 38, 42, 47, 52, 58, 64, 68,
+        71, 74, 76, 78, 80, 81, 82, 83, 84, 85,
+        86, 87, 88, 89, 90, 91, 92, 94, 96, 98, 100];
+
     // source: https://github.com/Ermad/ffxiv-craft-opt-web/blob/master/app/js/ffxivcraftmodel.js#L1942
     public static readonly NYMEIAS_WHEEL_TABLE = {
         1: 30,
