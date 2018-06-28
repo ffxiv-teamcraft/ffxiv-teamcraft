42e689595d4a111ed7f61b4d3daa3515b45cceb5
Supamiu <contact@flavien-normand.fr>
2018-06-28 20:34:47 +0200

    ci: added linter to travis CI process

--- src/app/pages/lists/external-list-import-popup/ffxiv-crafting-link-parser.ts
+++ src/app/pages/lists/external-list-import-popup/ffxiv-crafting-link-parser.ts
@@ -3,15 +3,13 @@ import {ExternalListData} from './external-list-data';
 
 export class FfxivCraftingLinkParser implements ExternalListLinkParser {
 
-    // http://www.ffxivcrafting.com/list/saved/15538,1:15549,1:15515,1:15516,1:15534,1:15518,1:15519,1:15530,1:15531,1:15532,1:15533,2:5060,2
-
     canParse(url: string): boolean {
         return url.indexOf('ffxivcrafting.com') > -1 || url.indexOf('craftingasaservice.com') > -1;
     }
 
     parse(url: string): ExternalListData[] {
         return url
-            // Split with character '/'
+        // Split with character '/'
             .split('/')
             // Take the last element (everything after the last '/'
             .pop()
