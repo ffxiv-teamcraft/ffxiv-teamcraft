c30c074a36ea68404b235efe428b10dbbac872d2
Supamiu <contact@flavien-normand.fr>
2018-07-04 22:53:25 +0200

    feat: muted alarms now hidden in alarms sidebar
    closes #452

--- src/app/modules/alarms-sidebar/alarms-sidebar/alarms-sidebar.component.ts
+++ src/app/modules/alarms-sidebar/alarms-sidebar/alarms-sidebar.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {AlarmService} from '../../../core/time/alarm.service';
 import {Alarm} from '../../../core/time/alarm';
-import {Observable} from 'rxjs';
+import {combineLatest, Observable} from 'rxjs';
 import {EorzeanTimeService} from '../../../core/time/eorzean-time.service';
-import {map, tap} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
+import {UserService} from '../../../core/database/user.service';
 
 @Component({
     selector: 'app-alarms-sidebar',
@@ -16,7 +17,7 @@ export class AlarmsSidebarComponent implements OnInit {
 
     public time: Date = new Date();
 
-    constructor(private alarmService: AlarmService, private etime: EorzeanTimeService) {
+    constructor(private alarmService: AlarmService, private etime: EorzeanTimeService, private userService: UserService) {
     }
 
     trackByAlarm(index: number, alarm: Alarm): number {
@@ -24,10 +25,16 @@ export class AlarmsSidebarComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.alarms$ = this.etime.getEorzeanTime()
+        const user$ = this.userService.getUserData();
+
+        const alarmGroups$ = combineLatest(this.etime.getEorzeanTime(), user$)
             .pipe(
-                tap(time => this.time = time),
-                map(time => {
+                map(data => {
+                    const time = data[0];
+                    const user = data[1];
+                    const result = user.alarmGroups.map(group => {
+                        return {groupName: group.name, enabled: group.enabled, alarms: []};
+                    });
                     const alarms: Alarm[] = [];
                     this.alarmService.alarms.forEach(alarm => {
                         if (alarms.find(a => a.itemId === alarm.itemId) !== undefined) {
@@ -36,18 +43,41 @@ export class AlarmsSidebarComponent implements OnInit {
                         const itemAlarms = this.alarmService.alarms.filter(a => a.itemId === alarm.itemId);
                         alarms.push(this.alarmService.closestAlarm(itemAlarms, time));
                     });
-                    return alarms.sort((a, b) => {
-                        if (this.alarmService.isAlarmSpawned(a, time)) {
-                            return -1;
+                    alarms.forEach(alarm => {
+                        const group = result.find(row => row.groupName === alarm.groupName);
+                        if (alarm.groupName === undefined || group === undefined) {
+                            const defaultGroup = result.find(row => row.groupName === 'Default group');
+                            defaultGroup.alarms.push(alarm);
+                        } else {
+                            group.alarms.push(alarm);
                         }
-                        if (this.alarmService.isAlarmSpawned(b, time)) {
-                            return 1;
-                        }
-                        return this.alarmService.getMinutesBefore(time, a.spawn) < this.alarmService.getMinutesBefore(time, b.spawn)
-                            ? -1 : 1;
                     });
+                    result.forEach(group => {
+                        group.alarms = group.alarms.sort((a, b) => {
+                            if (this.alarmService.isAlarmSpawned(a, time)) {
+                                return -1;
+                            }
+                            if (this.alarmService.isAlarmSpawned(b, time)) {
+                                return 1;
+                            }
+                            return this.alarmService.getMinutesBefore(time, (a.spawn || 24)) <
+                            this.alarmService.getMinutesBefore(time, (b.spawn || 24)) ? -1 : 1;
+                        });
+                    });
+                    return result;
                 })
             );
+
+        this.alarms$ = alarmGroups$.pipe(
+            map(groups => {
+                return groups.reduce((overlayAlarms, currentGroup) => {
+                    if (currentGroup.enabled) {
+                        overlayAlarms.push(...currentGroup.alarms);
+                    }
+                    return overlayAlarms;
+                }, []);
+            })
+        );
     }
 
 }
