4b7c4fcb19397b257e141dc8a229c3d12036b40d
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2018-07-11 22:45:54 +1000

    ci: Add proper code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -8,8 +8,19 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
+            require('karma-coverage-istanbul-reporter'),
             require('@angular-devkit/build-angular/plugins/karma')
         ],
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly', 'text-summary'],
+            dir: './coverage',
+            skipFilesWithNoCoverage: true,
+            'report-config': {
+                html: {
+                    subdir: 'html'
+                }
+            }
+        },
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
         },
