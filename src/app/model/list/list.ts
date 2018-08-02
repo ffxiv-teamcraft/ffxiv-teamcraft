a1d1d00799c6a0c259815ad5550ebcc7641e5ab7
Flavien Normand <contact@flavien-normand.fr>
2018-08-02 16:24:25 +0200

    ci: use circleci for CI/CD stuff (#530)
    * ci: switched from travis to circle-ci
    
    This enabled a better job splitting, better CD integration for the future and
    a better linting handling (which is why I had to make some changes to the Typescript)
    
    * chore: fixes for circle-ci
    
    * chore: removed yaml anchors
    
    * chore: yml syntax fixes
    
    * chore: trying to fix circle-ci configuration
    
    * chore: trying to fix circle-ci configuration
    
    * chore: trying to fix circle-ci configuration
    
    * chore: trying to fix circle-ci configuration
    
    * chore: trying to fix circle-ci configuration
    
    * chore: trying to fix circleci configuration
    
    * chore: trying to fix circleci configuration
    
    * chore: trying to fix circleci configuration
    
    * chore: trying to fix circleci configuration
    
    * chore: trying to fix circleci configuration
    
    * chore: use npm ci instead of install in ci env
    
    * chore: use LF line endings for circleci
    
    Also, back to npm install, ci doesn't exist in node 8.11
    
    * chore: fixed basic bash error
    
    * chore: add functions setup to circleci
    
    * chore: add functions setup to circleci and split deploy web and desktop
    
    * chore: better bindings for firebase
    
    * chore: tests for auto deploy github release using circleci
    
    * chore: renamed electron setup for better tracking on github badges
    
    * chore: back to node 8.11 in circleci for node-sass
    
    * chore: make electron build always windows even if built on linux image
    
    * chore: added docker image for windows-based builds, needs testing
    
    * chore: changed image for a custom one with wine
    
    * chore: trying to fix electron builder process
    
    * chore: trying to fix electron builder process
    
    * chore: trying to fix electron builder process
    
    * chore: add wine32 to docker image
    
    * chore: integrate wine32 in docker image
    
    * chore: fixed version after testing has been done
    
    * chore: deploy desktop only on master

--- src/app/model/list/list.ts
+++ src/app/model/list/list.ts
@@ -466,7 +466,8 @@ export class List extends DataWithPermissions {
                         amount: element.amount * addition.amount,
                         done: 0,
                         used: 0,
-                        yield: 1
+                        yield: 1,
+                        usePrice: true
                     });
                 } else {
                     const elementDetails = addition.data.getIngredient(element.id);
@@ -479,7 +480,8 @@ export class List extends DataWithPermissions {
                             requires: elementDetails.craft[0].ingredients,
                             done: 0,
                             used: 0,
-                            yield: yields
+                            yield: yields,
+                            usePrice: true
                         });
                         nextIteration.push({
                             item: elementDetails,
@@ -493,7 +495,8 @@ export class List extends DataWithPermissions {
                             amount: element.amount * addition.amount,
                             done: 0,
                             used: 0,
-                            yield: 1
+                            yield: 1,
+                            usePrice: true
                         });
                     } else {
                         this.addToOthers({
@@ -502,7 +505,8 @@ export class List extends DataWithPermissions {
                             amount: element.amount * addition.amount,
                             done: 0,
                             used: 0,
-                            yield: 1
+                            yield: 1,
+                            usePrice: true
                         });
                     }
                 }
