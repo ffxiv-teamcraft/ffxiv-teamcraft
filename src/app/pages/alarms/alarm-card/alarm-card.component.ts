c38c1d7909a804db7a1eb6f9345706dc4ede1508
supamiu <contact@flavien-normand.fr>
2018-09-18 15:10:52 +0200

    feat: added a button to copy ingame macro in clipboard for alarms

--- src/app/pages/alarms/alarm-card/alarm-card.component.ts
+++ src/app/pages/alarms/alarm-card/alarm-card.component.ts
@@ -1,10 +1,14 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Alarm} from '../../../core/time/alarm';
-import {MapService} from '../../../modules/map/map.service';
-import {MapData} from '../../../modules/map/map-data';
-import {Observable} from 'rxjs';
-import {AlarmNotePopupComponent} from '../alarm-note-popup/alarm-note-popup.component';
-import {MatDialog} from '@angular/material';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Alarm } from '../../../core/time/alarm';
+import { MapService } from '../../../modules/map/map.service';
+import { MapData } from '../../../modules/map/map-data';
+import { Observable } from 'rxjs';
+import { AlarmNotePopupComponent } from '../alarm-note-popup/alarm-note-popup.component';
+import { MatDialog } from '@angular/material';
+import { I18nToolsService } from '../../../core/tools/i18n-tools.service';
+import { LocalizedDataService } from '../../../core/data/localized-data.service';
+import { EorzeanTimeService } from '../../../core/time/eorzean-time.service';
+import { SettingsService } from '../../settings/settings.service';
 
 @Component({
     selector: 'app-alarm-card',
@@ -41,11 +45,17 @@ export class AlarmCardComponent implements OnInit {
 
     map: Observable<MapData>;
 
-    constructor(private mapService: MapService, private dialog: MatDialog) {
+    constructor(private mapService: MapService, private dialog: MatDialog, private i18n: I18nToolsService, private l12n: LocalizedDataService,
+                private etime: EorzeanTimeService, private settings: SettingsService) {
     }
 
     deleteAlarm(): void {
         this.delete.emit();
+    }
+
+    generateAlarmMacro():string{
+        return `/alarm "${this.i18n.getName(this.l12n.getItem(this.alarm.itemId))}" et ${this.alarm.spawn < 10 ? '0' : ''}${this.alarm.spawn}00 ${
+            Math.ceil(this.etime.toEarthTime(this.settings.alarmHoursBefore * 60) / 60)}`;
     }
 
     getClassIcon(): string {
