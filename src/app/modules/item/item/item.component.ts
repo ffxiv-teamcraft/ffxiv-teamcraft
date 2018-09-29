aff02ed339518e15f6abbdfbe85d6d096d3d447f
AltanRapsied <34192226+AltanRapsied@users.noreply.github.com>
2018-09-29 23:48:08 +0200

    chore: addition of 4.4 currencies and missing 4.3

--- src/app/modules/item/item/item.component.ts
+++ src/app/modules/item/item/item.component.ts
@@ -65,76 +65,108 @@ export class ItemComponent extends ComponentWithSubscriptions implements OnInit,
 
     public static TRADE_SOURCES_PRIORITIES = {
         // Just in case
-        25: 25, // Wolf Mark
-        29: 25, // MGP
+        25: 50, // Wolf Mark
+        29: 50, // MGP
         // Seals
-        20: 23, // Storm
-        21: 23, // Serpent
-        22: 23, // Flame
-        27: 23, // Allied
-        10307: 23, // Centurio
+        20: 30, // Storm
+        21: 30, // Serpent
+        22: 30, // Flame
+        27: 30, // Allied
+        10307: 30, // Centurio
         // Tomestones
-        28: 20, // Poetics
-        35: 1, // Creation (can't obtain them anymore)
-        36: 20, // Mendacity
-        37: 20, // Genesis
-        7811: 20, // Rowena's Token (Soldiery)
-        9383: 20, // Rowena's Token (Poetics)
-        14298: 20, // Rowena's Token (Lore)
-        16928: 20, // Rowena's Token (Scripture)
-        19107: 20, // Rowena's Token (Creation)
-        21789: 20, // Rowena's Token (Mendacity)
+        28: 28, // Poetics
+        35: 0, // Creation (can't obtain them anymore)
+        36: 28, // Mendacity
+        37: 28, // Genesis
+        7811: 28, // Rowena's Token (Soldiery)
+        9383: 28, // Rowena's Token (Poetics)
+        14298: 28, // Rowena's Token (Lore)
+        16928: 28, // Rowena's Token (Scripture)
+        19107: 28, // Rowena's Token (Creation)
+        21789: 28, // Rowena's Token (Mendacity)
+        23978: 28, // Rowena's Token (Genesis)
         // Scripts
-        10309: 18, // Red crafter
-        10311: 18, // Red gatherer
-        17833: 18, // Yellow crafter
-        17834: 18, // Yellow gatherer
+        10309: 25, // Red crafter
+        10311: 25, // Red gatherer
+        17833: 25, // Yellow crafter
+        17834: 25, // Yellow gatherer
         // Hunt mark log
-        7901: 15, // Blood-spattered
-        10127: 15, // Unstained
-        13625: 15, // Clan
-        15918: 15, // Unstained Clan
-        17523: 15, // Legendary Clan
-        20308: 15, // Veteran's Clan
-        21103: 15, // Mythic Clan
+        7901: 22, // Blood-spattered Mark Log
+        10127: 22, // Unstained Mark Log
+        13625: 22, // Clan Mark Log
+        15918: 22, // Unstained Clan Mark Log
+        17523: 22, // Legendary Clan Mark Log
+        20308: 22, // Veteran's Clan Mark Log
+        21103: 22, // Mythic Clan Mark Log
+        23176: 22, // Doyen's Clan Mark Log
         // Beast tribes
-        21073: 13, // Ixali
-        21074: 13, // Vanu Vanu
-        21075: 13, // Sylph
-        21076: 13, // Amalj'aa
-        21077: 13, // Sahagin
-        21078: 13, // Kobold
-        21079: 13, // Vath
-        21080: 13, // Moogle
-        21081: 13, // Kojin
-        21935: 13, // Ananta
-        22525: 13, // Namazu
-        // Primals
-        7004: 10, // Weekly quest Garuda/Titan/Ifrit
-        7850: 10, // Leviathan
-        9559: 10, // Shiva
-        12672: 10, // Bismarck
-        12673: 10, // Ravana
-        13627: 10, // Thordan
-        14300: 10, // Sephirot
-        15421: 10, // Nidhogg
-        16188: 10, // Sophia
-        17461: 10, // Zurvan
-        19109: 10, // Susano
-        19110: 10, // Lakshmi
-        21196: 10, // Shinryu
-        21773: 10, // Byakko
-        23043: 10, // Tsukuyomi
-        // Raids
-        7577: 8, // Sands of Time
-        7578: 8, // Oil of Time
+        21073: 20, // Ixali
+        21074: 20, // Vanu Vanu
+        21075: 20, // Sylph
+        21076: 20, // Amalj'aa
+        21077: 20, // Sahagin
+        21078: 20, // Kobold
+        21079: 20, // Vath
+        21080: 20, // Moogle
+        21081: 20, // Kojin
+        21935: 20, // Ananta
+        22525: 20, // Namazu
+        // World bosses
+        6155: 18, // Behemoth
+        6164: 18, // Odin
+        12252: 18, // Coeurlregina
+        12253: 18, // Proto Ultima
+        20521: 18, // Tamamo-no-Gozen
+        20638: 18, // Ixion
+        // Extremes
+        7004: 15, // Weekly quest Garuda/Titan/Ifrit
+        7850: 15, // Leviathan
+        9559: 15, // Shiva
+        12672: 15, // Bismarck
+        12673: 15, // Ravana
+        13627: 15, // Thordan
+        14300: 15, // Sephirot
+        15421: 15, // Nidhogg
+        16188: 15, // Sophia
+        17461: 15, // Zurvan
+        19109: 15, // Susano
+        19110: 15, // Lakshmi
+        21196: 15, // Shinryu
+        21773: 15, // Byakko
+        23043: 15, // Tsukuyomi
+        23962: 15, // Suzaku
+        // 24 man raids
+        13626: 12, // Mhachi Farthing
+        15947: 12, // Mhachi Penny
+        17595: 12, // Mhachi Shilling
+        21198: 12, // Rabanastran Coin
+        23174: 12, // Gougan Coin
+        // 8 man raids (upgrades)
+        7577: 10, // Sands of Time
+        7578: 10, // Oil of Time
+        9385: 10, // Carboncoat
+        9386: 10, // Carbontwine
+        10325: 10, // Illuminati Gobdip
+        10326: 10, // Illuminati Gobtwine
+        10327: 10, // Illuminati Gobcoat
+        14896: 10, // Illuminati Deep Gobdip
+        14897: 10, // Illuminati Taut Gobtwine
+        14898: 10, // Illuminati Dark Gobcoat
+        16542: 10, // Illuminati Darkest Gobcoat
+        16543: 10, // Illuminati Tautest Gobtwine
+        16544: 10, // Illuminati Deepest Gobdip 
+        19104: 10, // Lost Allagan Roborant
+        19105: 10, // Lost Allagan Twine
+        19106: 10, // Lost Allagan Glaze
+        21786: 10, // Ryumyaku Solvent
+        21787: 10, // Ryumyaku Weave
+        21788: 10, // Ryumyaku Polish
+        23975: 10, // Scaevan Ester
+        23976: 10, // Scaevan Weave
+        23977: 10, // Scaevan Shellac
+        // 8 man raids (token)
         7812: 8, // Unidentified Allagan Tomestone
         9384: 8, // Encrypted Tomestone
-        9385: 8, // Carboncoat
-        9386: 8, // Carbontwine
-        10325: 8, // Illuminati Gobdip
-        10326: 8, // Illuminati Gobtwine
-        10327: 8, // Illuminati Gobcoat
         12674: 8, // Tarnished Gordian Lens
         12675: 8, // Tarnished Gordian Shaft
         12676: 8, // Tarnished Gordian Crank
@@ -146,7 +178,6 @@ export class ItemComponent extends ComponentWithSubscriptions implements OnInit,
         12682: 8, // Gordian Manifesto - Page 2
         12683: 8, // Gordian Manifesto - Page 3
         12684: 8, // Gordian Manifesto - Page 4
-        13626: 8, // Mhachi Farthing
         14299: 8, // High-capacity Tomestone
         14301: 8, // Tarnished Midan Lens
         14302: 8, // Tarnished Midan Shaft
@@ -160,14 +191,7 @@ export class ItemComponent extends ComponentWithSubscriptions implements OnInit,
         14310: 8, // Midan Manifesto - Page 3
         14311: 8, // Midan Manifesto - Page 4
         14895: 8, // Midan Gear
-        14896: 8, // Illuminati Deep Gobdip
-        14897: 8, // Illuminati Taut Gobtwine
-        14898: 8, // Illuminati Dark Gobcoat
-        15947: 8, // Mhachi Penny
         16541: 8, // Micro Tomestone
-        16542: 8, // Illuminati Darkest Gobcoat
-        16543: 8, // Illuminati Tautest Gobtwine
-        16544: 8, // Illuminati Deepest Gobdip
         16545: 8, // Alexandrian Gear
         16546: 8, // Tarnished Alexandrian Lens
         16547: 8, // Tarnished Alexandrian Shaft
@@ -180,10 +204,6 @@ export class ItemComponent extends ComponentWithSubscriptions implements OnInit,
         16554: 8, // Alexandrian Manifesto - Page 2
         16555: 8, // Alexandrian Manifesto - Page 3
         16556: 8, // Alexandrian Manifesto - Page 4
-        17595: 8, // Mhachi Shilling
-        19104: 8, // Lost Allagan Roborant
-        19105: 8, // Lost Allagan Twine
-        19106: 8, // Lost Allagan Glaze
         19108: 8, // Early Model Tomestone
         19111: 8, // Deltascape Lens
         19112: 8, // Deltascape Shaft
@@ -197,8 +217,6 @@ export class ItemComponent extends ComponentWithSubscriptions implements OnInit,
         19120: 8, // Deltascape Datalog v3.0
         19121: 8, // Deltascape Datalog v4.0
         19122: 8, // Deltascape Crystalloid
-        21197: 8, // Dreadwyrm Totem
-        21198: 8, // Rabanastran Coin
         21774: 8, // Sigmascape Lens
         21775: 8, // Sigmascape Shaft
         21776: 8, // Sigmascape Crank
@@ -211,14 +229,23 @@ export class ItemComponent extends ComponentWithSubscriptions implements OnInit,
         21783: 8, // Sigmascape Datalog v3.0
         21784: 8, // Sigmascape Datalog v4.0
         21785: 8, // Sigmascape Crystalloid
-        23174: 8, // Gougan Coin
-        // World bosses
-        6155: 5, // Behemoth
-        6164: 5, // Odin
-        12252: 5, // Coeurlregina
-        12253: 5, // Proto Ultima
-        20521: 5, // Tamamo-no-Gozen
-        20638: 5, // Ixion
+        21790: 8, // Prototype Tomestone
+        23963: 8, // Alphascape Lens
+        23964: 8, // Alphascape Shaft
+        23965: 8, // Alphascape Crank
+        23966: 8, // Alphascape Spring
+        23967: 8, // Alphascape Pedal
+        23968: 8, // Alphascape Chain
+        23969: 8, // Alphascape Bolt
+        23970: 8, // Alphascape Datalog v1.0
+        23971: 8, // Alphascape Datalog v2.0
+        23972: 8, // Alphascape Datalog v3.0
+        23973: 8, // Alphascape Datalog v4.0
+        23974: 8, // Alphascape Crystalloid
+        23979: 8, // Military-grade Tomestone
+        // Ultimate
+        21197: 5, // Bahamut
+        23175: 5, // Ultima Arma
         // Gardening
         15857: 3, // Althyk Lavender
         15858: 3, // Voidrake
