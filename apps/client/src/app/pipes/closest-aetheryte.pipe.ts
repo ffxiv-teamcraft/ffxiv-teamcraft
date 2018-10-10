eabec3b183007bf1b64d85b479435856e54f5dd1
Supamiu <contact@flavien-normand.fr>
2018-10-10 22:29:58 +0200

    feat: vendors details popup
    #576

--- apps/client/src/app/pipes/closest-aetheryte.pipe.ts
+++ apps/client/src/app/pipes/closest-aetheryte.pipe.ts
@@ -3,7 +3,7 @@ import { MapService } from '../modules/map/map.service';
 import { Aetheryte } from '../core/data/aetheryte';
 import { Observable } from 'rxjs';
 import { Vector2 } from '../core/tools/vector2';
-import { map } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 @Pipe({
   name: 'closestAetheryte'
@@ -13,8 +13,9 @@ export class ClosestAetherytePipe implements PipeTransform {
   constructor(private mapService: MapService) {
   }
 
-  transform( zoneid: number,  position: Vector2): Observable<Aetheryte> {
+  transform(zoneid: number, position: Vector2): Observable<Aetheryte> {
     return this.mapService.getMapById(zoneid).pipe(
+      filter(mapData => mapData !== undefined),
       map(mapData => this.mapService.getNearestAetheryte(mapData, position))
     );
   }
