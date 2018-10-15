723952c974985c6b7bdc5e07c3e7337264c6c0ce
supamiu <contact@flavien-normand.fr>
2018-10-15 15:59:10 +0200

    feat: more monsters location data

--- data-extraction/xivdb-mapper/index.js
+++ data-extraction/xivdb-mapper/index.js
@@ -4,49 +4,69 @@ const http = require('http');
 const path = require('path');
 
 const outputFolder = path.join(__dirname, '../../apps/client/src/app/core/data/sources/');
+const memoryData = [];
 
 const nodes = {};
 const aetherytes = [];
 const monsters = {};
 const npcs = {};
 
+fs.existsSync('output') || fs.mkdirSync('output');
 
-http.get('http://xivapi.com/mapdata/download', (res) => {
-  res.setEncoding('utf8');
-  res.pipe(csv())
-    .on('data', function(row) {
-      if (row.ContentIndex === 'BNPC') {
-        handleMonster(row);
-      } else {
-        switch (row.Type) {
-          case 'NPC':
-            handleNpc(row);
-            break;
-          case 'Gathering':
-            handleNode(row);
-            break;
-          case 'Aetheryte':
-            handleAetheryte(row);
-            break;
-          default:
-            break;
-        }
-      }
-
+http.get('http://xivapi.com/memoryData/download', (memoryResponse) => {
+  memoryResponse.setEncoding('utf8');
+  memoryResponse.pipe(csv())
+    .on('data', function(memoryRow) {
+      memoryData.push(memoryRow);
     })
-    .on('end', function() {
-      const nodesData = JSON.stringify(nodes);
-      // Write data that needs to be joined with game data first
-      fs.writeFileSync('output/nodes-position.json', nodesData);
-      console.log('nodes written');
-      const aetherytesData = JSON.stringify(aetherytes);
-      fs.writeFileSync('output/aetherytes.json', aetherytesData);
-      console.log('aetherytes written');
-      const npcsData = JSON.stringify(npcs);
-      fs.writeFileSync('output/npcs.json', npcsData);
-      console.log('npcs written');
+    .on('end', () => {
+      console.log('Extracted memory data');
+      extractData();
     });
 });
+
+extractData = () => {
+  http.get('http://xivapi.com/mapdata/download', (res) => {
+    res.setEncoding('utf8');
+    res.pipe(csv())
+      .on('data', function(row) {
+        console.log(row);
+        if (row.ContentIndex === 'BNPC') {
+          handleMonster(row, memoryData);
+        } else {
+          switch (row.Type) {
+            case 'NPC':
+              handleNpc(row);
+              break;
+            case 'Gathering':
+              handleNode(row);
+              break;
+            case 'Aetheryte':
+              handleAetheryte(row);
+              break;
+            default:
+              break;
+          }
+        }
+
+      })
+      .on('end', function() {
+        const nodesData = JSON.stringify(nodes);
+        // Write data that needs to be joined with game data first
+        fs.writeFileSync('output/nodes-position.json', nodesData);
+        console.log('nodes written');
+        const aetherytesData = JSON.stringify(aetherytes);
+        fs.writeFileSync('output/aetherytes.json', aetherytesData);
+        console.log('aetherytes written');
+        const npcsData = JSON.stringify(npcs);
+        fs.writeFileSync('output/npcs.json', npcsData);
+        console.log('npcs written');
+        const monstersData = JSON.stringify(monsters);
+        fs.writeFileSync(path.join(outputFolder, 'monsters.json'), monstersData);
+        console.log('monsters written');
+      });
+  });
+};
 
 handleNode = (row) => {
   nodes[+row.ENpcResidentID] = {
@@ -69,7 +89,17 @@ handleAetheryte = (row) => {
   });
 };
 
-handleMonster = (row) => {
+handleMonster = (row, memoryData) => {
+  console.log(row);
+  const monsterMemoryRow = memoryData.find(mRow => mRow.Hash === row.Hash);
+  monsters[row.BNpcNameID] = {
+    zoneid: +row.PlaceNameID,
+    x: Math.round(+row.PosX),
+    y: Math.round(+row.PosY)
+  };
+  if (monsterMemoryRow !== undefined) {
+    monsters[row.BNpcNameID].level = +monsterMemoryRow.Level;
+  }
 };
 
 handleNpc = (row) => {
@@ -79,20 +109,3 @@ handleNpc = (row) => {
     y: Math.round(+row.PosY)
   };
 };
-
-// fs.createReadStream('monsters.csv')
-//     .pipe(csv())
-//     .on('data', function (row) {
-//         const position = JSON.parse(row.position);
-//         monsters[row.id] = {
-//             zoneid: +row.placename,
-//             x: position.ingame.x,
-//             y: position.ingame.y,
-//             level: JSON.parse(row.data).level
-//         };
-//     })
-//     .on('end', function () {
-//         const res = JSON.stringify(monsters);
-//         fs.writeFileSync('output/monsters.json', res);
-//         console.log("monsters CSV parse success");
-//     });
