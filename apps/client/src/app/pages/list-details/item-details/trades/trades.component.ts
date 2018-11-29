d7b0993ee98aaf72f92a84d403698faf0e3ef19c
supamiu <contact@flavien-normand.fr>
2018-11-29 09:59:51 +0100

    feat: total price in trades popup

--- apps/client/src/app/pages/list-details/item-details/trades/trades.component.ts
+++ apps/client/src/app/pages/list-details/item-details/trades/trades.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ItemDetailsPopup } from '../item-details-popup';
+import { TradeEntry } from '../../../../modules/list/model/trade-entry';
+import { Trade } from '../../../../modules/list/model/trade';
 
 @Component({
   selector: 'app-trades',
@@ -11,6 +13,16 @@ export class TradesComponent extends ItemDetailsPopup {
 
   constructor() {
     super();
+  }
+
+  public totalPrice(trade: Trade): TradeEntry[] {
+    const itemsPerTrade = trade.items.find(item => item.id === this.item.id).amount;
+    return trade.currencies.map(currency => {
+      return {
+        ...currency,
+        amount: Math.ceil(currency.amount * this.item.amount / itemsPerTrade)
+      }
+    });
   }
 
 }
