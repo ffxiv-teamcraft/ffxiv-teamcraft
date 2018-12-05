c8aebd42f0fd6694466abaf24bb503ec6b5aa40e
Supamiu <contact@flavien-normand.fr>
2018-12-05 14:33:07 +0100

    feat: alarms overlay for desktop app

---
+++ apps/client/src/app/pages/alarms-overlay/alarms-overlay/alarms-overlay.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IpcService } from '../../../core/electron/ipc.service';
+
+@Component({
+  selector: 'app-alarms-overlay',
+  templateUrl: './alarms-overlay.component.html',
+  styleUrls: ['./alarms-overlay.component.less']
+})
+export class AlarmsOverlayComponent {
+
+  public overlayOpacity = 100;
+
+  constructor(private ipc: IpcService) {
+    this.ipc.overlayUri = '/alarms-overlay';
+    this.ipc.on(`overlay:/alarms-overlay:opacity`, (event, value) => {
+      this.overlayOpacity = value * 100;
+    });
+    this.ipc.send('overlay:get-opacity', { uri: '/alarms-overlay' });
+  }
+
+  close(): void {
+    window.close();
+  }
+
+  setOverlayOpacity(opacity: number): void {
+    this.ipc.send('overlay:set-opacity', { uri: '/alarms-overlay', opacity: opacity / 100 });
+  }
+}
