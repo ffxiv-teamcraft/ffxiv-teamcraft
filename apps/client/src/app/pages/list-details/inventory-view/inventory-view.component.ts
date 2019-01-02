b8ae674f8f15df5697d04b1bb120d61c1f509a69
Supamiu <contact@flavien-normand.fr>
2019-01-02 23:02:55 +0100

    chore: WIP tests for integrated inventory view in list details

--- apps/client/src/app/pages/list-details/inventory-view/inventory-view.component.ts
+++ apps/client/src/app/pages/list-details/inventory-view/inventory-view.component.ts
@@ -1,27 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { List } from '../../../modules/list/model/list';
 import { Inventory } from '../../../model/other/inventory';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-inventory-view',
   templateUrl: './inventory-view.component.html',
   styleUrls: ['./inventory-view.component.less']
 })
-export class InventoryViewComponent implements OnInit {
+export class InventoryViewComponent {
 
-  public list: List;
-
-  public display: { id: number, icon: number, amount: number }[][] = [];
-
-  constructor() {
+  @Input()
+  public set list(l: List) {
+    this.list$.next(l);
   }
 
-  ngOnInit() {
-    const inventory = new Inventory();
-    this.list.forEach(item => {
-      inventory.add(item.id, item.icon, item.done - item.used);
-    });
-    this.display = inventory.getDisplay();
+  private list$: ReplaySubject<List> = new ReplaySubject<List>();
+
+  public display$: Observable<{ id: number, icon: number, amount: number }[][]>;
+
+  public constructor() {
+    this.display$ = this.list$.pipe(
+      map(list => {
+        const inventory = new Inventory();
+        list.forEach(item => {
+          inventory.add(item.id, item.icon, item.done - item.used);
+        });
+        return inventory.getDisplay();
+      })
+    );
   }
 
 }
