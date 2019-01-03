eeec18c74e9690729b093224261f6b4a6b1d6ab7
Supamiu <contact@flavien-normand.fr>
2019-01-03 09:50:41 +0100

    fix(layouts): layout selection now persists on page reload

--- apps/client/src/app/core/layout/+state/layouts.facade.ts
+++ apps/client/src/app/core/layout/+state/layouts.facade.ts
@@ -161,9 +161,14 @@ export class LayoutsFacade {
 
   select(layout: ListLayout): void {
     this.store.dispatch(new SelectLayout(layout.$key));
+    localStorage.setItem('layout:selected', layout.$key);
   }
 
   loadAll() {
     this.store.dispatch(new LoadLayouts());
+    const selectedKey = localStorage.getItem('layout:selected');
+    if (selectedKey !== null) {
+      this.store.dispatch(new SelectLayout(selectedKey));
+    }
   }
 }
