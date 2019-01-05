2aa0c4bc45cdb900f0aa67e0e1f9105151783d4f
Supamiu <contact@flavien-normand.fr>
2019-01-05 11:49:31 +0100

    fix(simulator): fixed an issue with rotation swapping

--- apps/client/src/app/pages/simulator/components/simulator/simulator.component.ts
+++ apps/client/src/app/pages/simulator/components/simulator/simulator.component.ts
@@ -285,9 +285,7 @@ export class SimulatorComponent implements OnInit, OnDestroy {
     combineLatest(this.rotation$, this.crafterStats$).pipe(
       takeUntil(this.onDestroy$)
     ).subscribe(([rotation, stats]) => {
-      if (this.actions$.value.length === 0) {
-        this.actions$.next(this.registry.deserializeRotation(rotation.rotation));
-      }
+      this.actions$.next(this.registry.deserializeRotation(rotation.rotation));
       if (rotation.food && this.selectedFood === undefined) {
         this.selectedFood = this.foods.find(f => rotation.food && f.itemId === rotation.food.id && f.hq === rotation.food.hq);
       }
