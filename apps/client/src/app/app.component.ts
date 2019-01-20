ff7e4a843f2123e25d3e18ac10692f2dfe0da46e
Supamiu <contact@flavien-normand.fr>
2019-01-20 10:53:56 +0100

    feat: added a message in every page to show when a new version is available

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -38,6 +38,7 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { CustomLinksFacade } from './modules/custom-links/+state/custom-links.facade';
 import { ObservableMedia } from '@angular/flex-layout';
 import { LayoutsFacade } from './core/layout/+state/layouts.facade';
+import * as semver from 'semver';
 
 declare const gtag: Function;
 
@@ -86,6 +87,8 @@ export class AppComponent implements OnInit {
 
   public navigating = true;
 
+  public newVersionAvailable$: Observable<boolean>;
+
   get desktopUrl(): SafeUrl {
     return this.sanitizer.bypassSecurityTrustUrl(`teamcraft://${window.location.pathname}`);
   }
@@ -105,6 +108,12 @@ export class AppComponent implements OnInit {
     this.desktop = this.platformService.isDesktop();
 
     this.iconService.fetchFromIconfont({ scriptUrl: 'https://at.alicdn.com/t/font_931253_pxv80d5yyj8.js' });
+
+    this.newVersionAvailable$ = this.firebase.object('app_version').valueChanges().pipe(
+      map((value: string) => {
+        return semver.ltr(environment.version, value);
+      })
+    );
 
     this.time$ = this.eorzeanTime.getEorzeanTime().pipe(
       map(date => {
