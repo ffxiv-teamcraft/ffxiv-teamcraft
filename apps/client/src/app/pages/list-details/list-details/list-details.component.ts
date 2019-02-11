80c6a0d08d2d5681e0a36375af6db98a12bb40e1
Supamiu <contact@flavien-normand.fr>
2019-02-11 18:27:29 +0100

    fix(list): fixed an issue with some lists being deleted in details while they still exist

--- apps/client/src/app/pages/list-details/list-details/list-details.component.ts
+++ apps/client/src/app/pages/list-details/list-details/list-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LayoutsFacade } from '../../../core/layout/+state/layouts.facade';
 import { ListsFacade } from '../../../modules/list/+state/lists.facade';
 import { ActivatedRoute, Router } from '@angular/router';
-import { filter, first, map, mergeMap, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { filter, first, map, mergeMap, shareReplay, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
 import { LayoutRowDisplay } from '../../../core/layout/layout-row-display';
 import { List } from '../../../modules/list/model/list';
@@ -28,13 +28,14 @@ import { TextQuestionPopupComponent } from '../../../modules/text-question-popup
 import { I18nToolsService } from '../../../core/tools/i18n-tools.service';
 import { LocalizedDataService } from '../../../core/data/localized-data.service';
 import { LinkToolsService } from '../../../core/tools/link-tools.service';
+import { TeamcraftComponent } from '../../../core/component/teamcraft-component';
 
 @Component({
   selector: 'app-list-details',
   templateUrl: './list-details.component.html',
   styleUrls: ['./list-details.component.less']
 })
-export class ListDetailsComponent implements OnInit, OnDestroy {
+export class ListDetailsComponent extends TeamcraftComponent implements OnInit, OnDestroy {
 
   public display$: Observable<ListDisplay>;
 
@@ -80,6 +81,7 @@ export class ListDetailsComponent implements OnInit, OnDestroy {
               private teamsFacade: TeamsFacade, private authFacade: AuthFacade,
               private discordWebhookService: DiscordWebhookService, private i18nTools: I18nToolsService,
               private l12n: LocalizedDataService, private linkTools: LinkToolsService) {
+    super();
     this.list$ = combineLatest(this.listsFacade.selectedList$, this.permissionLevel$).pipe(
       filter(([list]) => list !== undefined),
       tap(([list, permissionLevel]) => {
@@ -111,6 +113,17 @@ export class ListDetailsComponent implements OnInit, OnDestroy {
       .pipe(
         map(([team, userId, permissionsLevel]) => team.leader === userId || permissionsLevel >= PermissionLevel.OWNER)
       );
+
+    combineLatest(this.list$, this.listsFacade.compacts$).pipe(
+      filter(([list, compacts]) => list.notFound && compacts.some(l => l.$key === list.$key)),
+      switchMap(([list, compacts]) => {
+        const listCompact = compacts.find(l => l.$key === list.$key);
+        return this.listManager.upgradeList(listCompact);
+      }),
+      takeUntil(this.onDestroy$)
+    ).subscribe(regeneratedList => {
+      this.listsFacade.updateList(regeneratedList);
+    });
   }
 
   ngOnInit() {
@@ -119,7 +132,8 @@ export class ListDetailsComponent implements OnInit, OnDestroy {
     this.activatedRoute.paramMap
       .pipe(
         map(params => params.get('listId')),
-        tap((listId: string) => this.listsFacade.load(listId))
+        tap((listId: string) => this.listsFacade.load(listId)),
+        takeUntil(this.onDestroy$)
       )
       .subscribe(listId => {
         this.listsFacade.select(listId);
@@ -128,7 +142,8 @@ export class ListDetailsComponent implements OnInit, OnDestroy {
       filter(team => team && team.notFound),
       switchMap(() => {
         return this.list$.pipe(first());
-      })
+      }),
+      takeUntil(this.onDestroy$)
     ).subscribe(list => {
       delete list.teamId;
       this.listsFacade.updateList(list);
@@ -288,7 +303,8 @@ export class ListDetailsComponent implements OnInit, OnDestroy {
       first(),
       switchMap(() => {
         return modalRef.getContentComponent().changes$;
-      })
+      }),
+      takeUntil(this.onDestroy$)
     ).subscribe(() => {
       this.listsFacade.updateList(list);
     });
@@ -320,6 +336,7 @@ export class ListDetailsComponent implements OnInit, OnDestroy {
     this.list$.pipe(first()).subscribe(list => {
       this.listsFacade.unload(list.$key);
     });
+    super.ngOnDestroy();
   }
 
 }
