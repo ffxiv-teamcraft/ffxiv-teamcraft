a3054c2050f002780dc3d444a935dba79082a895
Supamiu <contact@flavien-normand.fr>
2019-02-19 10:10:20 +0100

    chore: fixed e2e tests and connected them back into CI flow

--- apps/client-e2e/src/support/app.po.ts
+++ apps/client-e2e/src/support/app.po.ts
@@ -2,7 +2,7 @@ export const getPageTitle = () => cy.get('a.logo');
 
 export const changeLanguage = (newLanguage: 'EN' | 'DE' | 'FR' | 'JA' | 'PT' | 'BR' | 'ES' | 'KO' | 'ZH') => {
   cy.get('.language-swap > .ant-select-selection').click();
-  cy.get(`.ant-select-dropdown-menu-item`).contains(newLanguage).click();
+  cy.get('.ant-select-dropdown-menu-item').contains(` ${newLanguage} `).click();
 };
 
 export const toggleSider = () => cy.get('.ant-layout-sider-trigger').click();
