a5cdd38166df34b2c9606cc8f279b6ca733e70ac
Supamiu <contact@flavien-normand.fr>
2019-02-22 14:45:36 +0100

    fix(simulator): fixed an issue with some ingredients possible as HQ while they aren't
    closes #740

--- apps/client/src/app/pages/simulator/components/simulator/simulator.component.ts
+++ apps/client/src/app/pages/simulator/components/simulator/simulator.component.ts
@@ -52,6 +52,7 @@ import { NameQuestionPopupComponent } from '../../../../modules/name-question-po
 import { LinkToolsService } from '../../../../core/tools/link-tools.service';
 import { RotationPickerService } from '../../../../modules/rotations/rotation-picker.service';
 import { RecipeChoicePopupComponent } from '../recipe-choice-popup/recipe-choice-popup.component';
+import { fakeHQItems } from '../../../../core/data/sources/fake-hq-items';
 
 @Component({
   selector: 'app-simulator',
@@ -254,7 +255,7 @@ export class SimulatorComponent implements OnInit, OnDestroy {
     this.hqIngredientsData$ = this.recipe$.pipe(
       map(recipe => {
         return (recipe.ingredients || [])
-          .filter(i => i.id > 20 && i.quality !== undefined)
+          .filter(i => i.id > 20 && i.quality !== undefined && !fakeHQItems.some(id => i.id === id))
           .map(ingredient => ({ id: ingredient.id, amount: 0, max: ingredient.amount, quality: ingredient.quality }));
       })
     );
