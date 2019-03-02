472536fe3050cfa7bd97e41b1e4d62110952908c
Supamiu <contact@flavien-normand.fr>
2019-03-02 14:31:33 +0100

    chore: trying to fix e2e tests with click behavior

--- apps/client-e2e/src/support/app.po.ts
+++ apps/client-e2e/src/support/app.po.ts
@@ -2,8 +2,8 @@ export const getPageTitle = () => cy.get('a.logo');
 
 export const changeLanguage = (newLanguage: 'EN' | 'DE' | 'FR' | 'JA' | 'PT' | 'BR' | 'ES' | 'KO' | 'ZH') => {
   const index = ['EN', 'DE', 'FR', 'JA', 'PT', 'BR', 'ES', 'KO', 'ZH'].indexOf(newLanguage) + 1;
-  cy.get('.language-swap > .ant-select-selection').click();
-  cy.get(`.ant-select-dropdown-menu > :nth-child(${index})`).click();
+  cy.get('.language-swap > .ant-select-selection').click({multiple: true});
+  cy.get(`.ant-select-dropdown-menu > :nth-child(${index})`).click({multiple: true});
 };
 
 export const toggleSider = () => cy.get('.ant-layout-sider-trigger').click();
