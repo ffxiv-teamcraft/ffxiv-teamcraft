d1ce8b8f9ba456e45f2833d2d8e8ff24712eb230
Supamiu <contact@flavien-normand.fr>
2019-04-09 08:00:21 +0200

    fix(alarms): better display for long timers

--- apps/client/src/app/core/eorzea/timer.pipe.ts
+++ apps/client/src/app/core/eorzea/timer.pipe.ts
@@ -7,7 +7,8 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class TimerPipe implements PipeTransform {
 
-  constructor(private translate: TranslateService){}
+  constructor(private translate: TranslateService) {
+  }
 
   transform(duration: number): string {
     // 259200 is 3 days, we'll display a specific timer for that because of weather transition accuracy issues
@@ -18,7 +19,11 @@ export class TimerPipe implements PipeTransform {
     const minutes = Math.floor(duration / 60) % 60;
     const hours = Math.floor(duration / 3600) % 24;
     const days = Math.floor(duration / 86400);
-    return `${days > 0 ? days + ':' : ''}${hours > 0 ? hours + ':' : ''}${minutes}:${seconds < 10 ? 0 : ''}${seconds}`;
+    const secondsString = `${seconds < 10 ? 0 : ''}${seconds}`;
+    const minutesString = `${minutes < 10 ? 0 : ''}${minutes}`;
+    const hoursString = `${hours < 10 ? 0 : ''}${hours}`;
+    const daysString = `${days < 10 ? 0 : ''}${days}`;
+    return `${days > 0 ? daysString + ':' : ''}${hours > 0 ? hoursString + ':' : ''}${minutesString}:${secondsString}`;
   }
 
 }
