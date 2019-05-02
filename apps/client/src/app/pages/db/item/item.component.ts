84ad98909aeebb9dc4ad66242e291d98be2778d2
Supamiu <contact@flavien-normand.fr>
2019-05-02 20:58:34 +0200

    chore: structure for item page

---
+++ apps/client/src/app/pages/db/item/item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.less']
+})
+export class ItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ apps/client/src/app/pages/db/item/item.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemComponent } from './item.component';
+
+@NgModule({
+  declarations: [ItemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ItemModule { }
