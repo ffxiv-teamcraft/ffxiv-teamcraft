3e8a4e171470c36f07fe6963bf97bec0ba917de5
Supamiu <contact@flavien-normand.fr>
2019-05-16 14:33:46 +0200

    chore: changed the way we load translations in server

--- apps/client/src/app/app.server.module.ts
+++ apps/client/src/app/app.server.module.ts
@@ -1,14 +1,12 @@
-import { NgModule, PLATFORM_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
-import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslationsLoaderFactory } from './translations-loader';
-import { PlatformService } from './core/tools/platform.service';
+import { TranslateServerLoader } from './translate-server-loader';
 
 @NgModule({
   imports: [
@@ -22,10 +20,9 @@ import { PlatformService } from './core/tools/platform.service';
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: TranslationsLoaderFactory,
-        deps: [HttpClient, PLATFORM_ID, PlatformService]
+        useClass: TranslateServerLoader
       }
-    }),
+    })
   ],
   bootstrap: [AppComponent]
 })
