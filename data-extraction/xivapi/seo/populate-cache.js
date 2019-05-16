e4ad3bb025de1c1da1b3708197db2eecf41f8e84
Supamiu <contact@flavien-normand.fr>
2019-05-16 08:24:07 +0200

    chore: proper cache update system now done

--- data-extraction/xivapi/seo/populate-cache.js
+++ data-extraction/xivapi/seo/populate-cache.js
@@ -1,34 +1,63 @@
-const { getAllPages } = require('../tools.js');
-const { switchMap } = require('rxjs/operators');
-const { combineLatest, concat } = require('rxjs');
+const { getAllPages, get } = require('../tools.js');
+const { switchMap, bufferCount, map, tap, mergeMap, takeUntil, first } = require('rxjs/operators');
+const { combineLatest, concat, BehaviorSubject, Subject } = require('rxjs');
 const request = require('request');
-const { Subject } = require('rxjs');
+const _cliProgress = require('cli-progress');
 
 const langs = ['en', 'de', 'ja', 'fr'];
 
-function prepareCache(sheet, page){
-  return getAllPages(`https://xivapi.com/${sheet}?columns=ID,Name_*`).pipe(
-    switchMap(page => {
-      return concat([].concat.apply([],
-        page.Results.map(item => {
-          return langs.map(lang => {
-            const done$ = new Subject();
-            const options = {
-              url: `https://beta.ffxivteamcraft.com/db/${lang}/${page}/${item.ID}/${item[`Name_${lang}`].split(' ').join('-')}`,
-              headers: {
-                'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
-              }
-            };
-            request(
-              options,
-              () => {
+function prepareCache(sheet, pageName) {
+  return get(`https://xivapi.com/${sheet}`).pipe(
+    switchMap(firstPage => {
+      return getAllPages(`https://xivapi.com/${sheet}?columns=ID,Name_*`).pipe(
+        map(page => {
+          return [].concat.apply([],
+            page.Results.map(item => {
+              return langs.map(lang => {
+                return `https://beta.ffxivteamcraft.com/db/${lang}/${pageName}/${item.ID}/${item[`Name_${lang}`].split(' ').join('-')}`;
+              });
+            })
+          );
+        }),
+        bufferCount(firstPage.Pagination.PageTotal),
+        first(),
+        map(buffered => [].concat.apply([], buffered)),
+        switchMap(urls => {
+          console.log(urls.length);
+          // create a new progress bar instance and use shades_classic theme
+          const progress = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic);
+          progress.start(urls.length, 0);
+          const index$ = new BehaviorSubject(0);
+          const complete$ = new Subject();
+          return index$.pipe(
+            mergeMap(index => {
+              const done$ = new Subject();
+              const options = {
+                url: urls[index],
+                headers: {
+                  'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)'
+                }
+              };
+              request(options, (err) => {
+                if (err) {
+                  console.error(err);
+                }
                 done$.next();
+              });
+              return done$;
+            }),
+            tap(() => {
+              progress.update(index$.value);
+              if (urls[index$.value + 1]) {
+                index$.next(index$.value + 1);
+              } else {
+                complete$.next();
+                progress.stop();
               }
-            );
-            return done$;
-          });
+            }),
+            takeUntil(complete$)
+          );
         })
-        )
       );
     })
   );
@@ -37,7 +66,7 @@ function prepareCache(sheet, page){
 combineLatest([
   // prepareCache('Item', 'item'),
   // prepareCache('InstanceContent', 'instance'),
-  prepareCache('Fate', 'fate'),
+  prepareCache('Fate', 'fate')
   // prepareCache('Leve', 'leve'),
   // prepareCache('ENpcResident', 'npc'),
   // prepareCache('BNpcName', 'mob'),
