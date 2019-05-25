a90549de1ea52499c3fe906820d9aa14e0fcdb10
Supamiu <contact@flavien-normand.fr>
2019-05-25 15:05:49 +0200

    feat(map): added aetheryte shard names and more accurate aetheryte names overall

--- data-extraction/xivapi/index.js
+++ data-extraction/xivapi/index.js
@@ -37,7 +37,8 @@ let todo = [
   'combos',
   'statuses',
   'traits',
-  'items'
+  'items',
+  'aetherytes'
 ];
 
 const onlyIndex = process.argv.indexOf('--only');
@@ -993,5 +994,24 @@ if (hasTodo('items')) {
   }, null, () => {
     persistToJsonAsset('items', names);
     persistToTypescript('rarities', 'rarities', rarities);
+  });
+}
+
+if (hasTodo('aetherytes')) {
+  const names = {};
+  getAllPages('https://xivapi.com/Aetheryte?columns=ID,AethernetName.Name_*').subscribe(page => {
+    page.Results.forEach(aetheryte => {
+      if (aetheryte.AethernetName.Name_en === null) {
+        return;
+      }
+      names[aetheryte.ID] = {
+        en: aetheryte.AethernetName.Name_en,
+        de: aetheryte.AethernetName.Name_de,
+        ja: aetheryte.AethernetName.Name_ja,
+        fr: aetheryte.AethernetName.Name_fr
+      };
+    });
+  }, null, () => {
+    persistToTypescript('aetheryte-names', 'aetheryteNames', names);
   });
 }
