143e8d3af3a4cca8e30f69733127a2af83259e03
Supamiu <contact@flavien-normand.fr>
2019-05-26 22:17:06 +0200

    chore: brought older themes back

--- apps/client/src/app/modules/settings/theme.ts
+++ apps/client/src/app/modules/settings/theme.ts
@@ -11,27 +11,28 @@ export class Theme {
     '#292929'
   );
 
-  // public static readonly GREEN = new Theme('GREEN', '#009a4d', '#b77cb3', 'white');
-  //
-  // public static readonly BLUE = new Theme('BLUE', '#15459a', '#dac900', 'white');
-  //
-  // public static readonly RED = new Theme('RED', '#921210', '#00da5c', 'white');
-  //
-  // public static readonly PURPLE = new Theme('PURPLE', '#8e24aa', '#0edac7', 'white');
-  //
-  // public static readonly CUSTOM = new Theme('CUSTOM', '#8e24aa', '#0edac7', 'white');
+  public static readonly GREEN = new Theme('GREEN', '#009a4d', '#b77cb3', 'white');
 
-  public static ALL_THEMES = [Theme.DEFAULT];//, Theme.GREEN, Theme.BLUE, Theme.RED, Theme.PURPLE];
+  public static readonly BLUE = new Theme('BLUE', '#15459a', '#dac900', 'white');
+
+  public static readonly RED = new Theme('RED', '#921210', '#00da5c', 'white');
+
+  public static readonly PURPLE = new Theme('PURPLE', '#8e24aa', '#0edac7', 'white');
+
+  // This is just a placeholder
+  public static readonly CUSTOM = new Theme('CUSTOM', '#8e24aa', '#0edac7', 'white');
+
+  public static ALL_THEMES = [Theme.DEFAULT, Theme.GREEN, Theme.BLUE, Theme.RED, Theme.PURPLE];
 
   constructor(
     public readonly name: string,
     public primary: string,
     public highlight: string,
     public text: string,
-    public topbar: string,
-    public trigger: string,
-    public triggerHover:string,
-    public background: string
+    public topbar: string = '#001529',
+    public trigger: string = '#002140',
+    public triggerHover:string = '#2A3948',
+    public background: string = '#292929'
   ) {
   }
 
