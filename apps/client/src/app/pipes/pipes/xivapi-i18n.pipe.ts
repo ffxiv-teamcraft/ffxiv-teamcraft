acabfd0de5ec0800466407607b7f291af9301585
Supamiu <contact@flavien-normand.fr>
2019-06-21 11:26:25 +0200

    chore: trying to fix lang in SSR rendering

--- apps/client/src/app/pipes/pipes/xivapi-i18n.pipe.ts
+++ apps/client/src/app/pipes/pipes/xivapi-i18n.pipe.ts
@@ -1,6 +1,8 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Inject, Pipe, PipeTransform, PLATFORM_ID } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { isPlatformServer } from '@angular/common';
 
 @Pipe({
   name: 'xivapiI18n',
@@ -8,11 +10,17 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class XivapiI18nPipe implements PipeTransform {
 
-  constructor(private translate: TranslateService, private sanitizer: DomSanitizer) {
+  constructor(private translate: TranslateService, private sanitizer: DomSanitizer,
+              private activatedRoute: ActivatedRoute, @Inject(PLATFORM_ID) private platform: Object) {
   }
 
   transform(value: any, fieldName = 'Name', sanitized = false): SafeHtml {
-    const name = value[`${fieldName}_${this.translate.currentLang}`] || value[`${fieldName}_en`];
+    let lang = this.translate.currentLang;
+    if (isPlatformServer(this.platform) && this.activatedRoute.snapshot.paramMap.get('language')) {
+      lang = this.activatedRoute.snapshot.paramMap.get('language');
+      console.log('LANG FROM URL', lang);
+    }
+    const name = value[`${fieldName}_${lang}`] || value[`${fieldName}_en`];
     if (sanitized) {
       return this.sanitizer.bypassSecurityTrustHtml(name);
     }
