0abb193e8152b70c73a4e69922165616d9181ed5
Supamiu <contact@flavien-normand.fr>
2019-06-27 13:26:42 +0200

    fix(data-extraction): added a queue system to avoid spamming xivapi too hard

--- data-extraction/xivapi/tools.js
+++ data-extraction/xivapi/tools.js
@@ -1,30 +1,51 @@
 const request = require('request');
 const Rx = require('rxjs');
-const { mergeMap, switchMap, delay, map, tap, takeUntil, skip } = require('rxjs/operators');
+const { mergeMap, switchMap, delay, map, tap, takeUntil, skip, filter } = require('rxjs/operators');
 const path = require('path');
 const fs = require('fs');
-const Multiprogress = require("multi-progress");
+const Multiprogress = require('multi-progress');
 const multi = new Multiprogress(process.stdout);
 
 const outputFolder = path.join(__dirname, '../../apps/client/src/app/core/data/sources/');
 const assetOutputFolder = path.join(__dirname, '../../apps/client/src/assets/data/');
 
+const queue = [];
 
-const get = (url, body) => {
-  const res$ = new Rx.Subject();
-  if (body !== undefined) {
-    request(url, {
-      body: body,
+const queueEmpty$ = Rx.interval(1000).pipe(
+  filter(() => {
+    return queue.length === 0;
+  })
+);
+
+Rx.interval(250).pipe(
+  takeUntil(queueEmpty$),
+  filter(() => {
+    return queue.length > 0;
+  })
+).subscribe(() => {
+  const operation = queue.pop();
+  if (operation.body !== undefined) {
+    request(operation.url, {
+      body: operation.body,
       json: true
-    }, (err, _, res) => res$.next(res));
+    }, (err, _, res) => operation.res$.next(res));
   } else {
-    request(url, { json: true }, (err, _, res) => {
+    request(operation.url, { json: true }, (err, _, res) => {
       if (err) {
         console.error(err);
       }
-      res$.next(res);
+      operation.res$.next(res);
     });
   }
+});
+
+const get = (url, body) => {
+  const res$ = new Rx.Subject();
+  queue.push({
+    url: url,
+    body: body,
+    res$: res$
+  });
   return res$;
 };
 
@@ -56,7 +77,7 @@ const getAllPages = (endpoint, body) => {
             console.error(result);
           }
           if (result.Pagination.Page === 1) {
-            progress = multi.newBar(`[:bar] :current/:total :etas - ${endpoint}`, {
+            progress = multi.newBar(`[:bar] :current/:total :etas - ${endpoint.substring(0, 120)}${endpoint.length > 120 ? '...':''}`, {
               complete: '=',
               incomplete: ' ',
               width: 50,
@@ -79,7 +100,7 @@ const getAllPages = (endpoint, body) => {
 };
 
 module.exports.getAllEntries = (endpoint, key, startsAt0) => {
-  console.log(`CRAWLING ${endpoint}`);
+  let progress;
   const allIds = startsAt0 ? ['0'] : [];
   const index$ = new Rx.Subject();
   getAllPages(addQueryParam(addQueryParam(endpoint, 'key', key), 'columns', 'ID')).subscribe(page => {
@@ -92,10 +113,18 @@ module.exports.getAllEntries = (endpoint, key, startsAt0) => {
     switchMap(index => {
       return get(addQueryParam(`${endpoint}/${allIds[index]}`, 'key', key)).pipe(
         tap(result => {
+          if(progress === undefined){
+            progress = multi.newBar(`[:bar] :current/:total :etas - ${endpoint.substring(0, 120)}${endpoint.length > 120 ? '...':''}`, {
+              complete: '=',
+              incomplete: ' ',
+              width: 50,
+              total: allIds.length
+            });
+          }
+          progress.tick();
           completeFetch.push(result);
           if (allIds[index + 1] !== undefined) {
             index$.next(index + 1);
-            console.log(`${endpoint} : ${index}/${allIds.length}`);
           }
         })
       );
