40938a30b478a5986f1c34aea3c90485499a5f7f
Supamiu <contact@flavien-normand.fr>
2019-07-05 22:02:09 +0200

    feat(reduction): added fish reduction data thanks to Hiems Whiterock and M'aila Batih

--- data-extraction/xivapi/index.js
+++ data-extraction/xivapi/index.js
@@ -1149,3 +1149,54 @@ if (hasTodo('actions')) {
     persistToJsonAsset('craft-actions', craftActions);
   });
 }
+
+if (hasTodo('reductions')) {
+  // Base handmade data
+  const reductions = {
+    12936: [5214, 5218, 12968, 12971],
+    12937: [12966, 12967, 12969, 12972],
+    12938: [5220, 5224],
+    12939: [12970, 12973],
+    12940: [12831, 12833],
+    15648: [15948, 15949],
+    20014: [20009, 19916, 20181],
+    20015: [20010],
+    20013: [20012, 20011, 20180],
+    20017: [20024],
+    20016: [19937],
+    23182: [23220, 23221]
+  };
+  const items = require('../../apps/client/src/assets/data/items.json');
+  const sheetRows = [];
+  // Credits to Hiems Whiterock / M'aila Batih for the data sheet
+  fs.createReadStream(path.join(__dirname, 'input/shb-fish-desynth.csv'), 'utf-8')
+    .pipe(csv())
+    .on('data', (row) => {
+      sheetRows.push(row);
+    })
+    .on('end', () => {
+      // Pop first item, as it's the credit row
+      sheetRows.pop();
+      sheetRows.forEach((row, index) => {
+        const itemReductions = [];
+        const itemId = +Object.keys(items).find(key => items[key].en.toLowerCase() === row.Item.toLowerCase());
+        if (isNaN(itemId)) {
+          console.log('Invalid row', index, row);
+        } else {
+          for (let i = 0; i < 5; i++) {
+            if (row[`r${i}`] && row[`r${i}`].length > 0) {
+              const reductionId = +Object.keys(items).find(key => items[key].en.toLowerCase() === row[`r${i}`].toLowerCase());
+              if (isNaN(reductionId)) {
+                console.log('Invalid row reduction', index, i, row);
+              } else {
+                itemReductions.push(reductionId);
+              }
+            }
+          }
+          reductions[itemId] = itemReductions;
+        }
+      });
+      persistToTypescript('reductions', 'reductions', reductions);
+    });
+
+}
