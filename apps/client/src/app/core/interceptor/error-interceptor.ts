a20a48eda9c82408f182277b3997e078153f39bb
Supamiu <contact@flavien-normand.fr>
2019-07-13 10:09:13 +0200

    feat: added proper error detection for http requests

---
+++ apps/client/src/app/core/interceptor/error-interceptor.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { NzMessageService } from 'ng-zorro-antd';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private message: NzMessageService, private translate: TranslateService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(err => {
+        if (req.url.toLowerCase().indexOf('xivapi.com/search') > -1) {
+          this.message.error(this.translate.instant('ERRORS.Http_request_error', {tool: 'XIVAPI Search'}));
+        } else if (req.url.toLowerCase().indexOf('xivapi.com') > -1) {
+          this.message.error(this.translate.instant('ERRORS.Http_request_error', {tool: 'XIVAPI'}));
+        } else if (req.url.toLowerCase().indexOf('garlandtools.com') > -1) {
+          this.message.error(this.translate.instant('ERRORS.Http_request_error', {tool: 'Garlandtools'}));
+        }
+        return throwError(err);
+      })
+    );
+  }
+
+}
