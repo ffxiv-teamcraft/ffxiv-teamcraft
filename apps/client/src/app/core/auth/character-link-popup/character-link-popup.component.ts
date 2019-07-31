e88940dafb36cfa5929ca54c4c4f2dce6de4838e
Supamiu <contact@flavien-normand.fr>
2019-07-31 10:59:13 +0200

    chore: auth and layout fixes

--- apps/client/src/app/core/auth/character-link-popup/character-link-popup.component.ts
+++ apps/client/src/app/core/auth/character-link-popup/character-link-popup.component.ts
@@ -37,10 +37,7 @@ export class CharacterLinkPopupComponent {
 
   private koreanServers = ['초코보', '모그리', '카벙클', '톤베리'];
 
-  public user$: Observable<TeamcraftUser> = this.store.pipe(
-    select('auth'),
-    map(state => state.user)
-  );
+  public localstorageUser: TeamcraftUser = JSON.parse(localStorage.getItem('auth:user'));
 
   constructor(private xivapi: XivapiService, private store: Store<any>, private modalRef: NzModalRef) {
     this.servers$ = this.xivapi.getServerList().pipe(
