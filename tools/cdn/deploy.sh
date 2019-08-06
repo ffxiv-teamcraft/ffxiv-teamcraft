c58bea03319b19e78bc5c6f9efee439c8457ff6e
Supamiu <contact@flavien-normand.fr>
2019-08-06 16:10:36 +0200

    chore(ci): small fix for network copy

--- tools/cdn/deploy.sh
+++ tools/cdn/deploy.sh
@@ -8,7 +8,9 @@ PACKAGE_VERSION=$(cat package.json \
 
 echo VERSION ${PACKAGE_VERSION}
 
-rsync -avz ./dist/apps/client/* dalamud@cdn.ffxivteamcraft.com:~/cdn.ffxivteamcraft.com/${PACKAGE_VERSION}
+ssh-keyscan -H cdn.ffxivteamcraft.com >> ~/.ssh/known_hosts
+
+scp -rp ./dist/apps/client/* dalamud@cdn.ffxivteamcraft.com:~/cdn.ffxivteamcraft.com/${PACKAGE_VERSION}
 
 ssh dalamud@cdn.ffxivteamcraft.com << EOF
   ln -s ./cdn.ffxivteamcraft.com/${PACKAGE_VERSION} ./cdn.ffxivteamcraft.com/latest
