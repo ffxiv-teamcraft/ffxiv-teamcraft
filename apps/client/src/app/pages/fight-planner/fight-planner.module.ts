ff92e84014f91bbc831cf077b27ce288b7f13f8a
Supamiu <contact@flavien-normand.fr>
2019-08-14 12:38:47 +0200

    chore(fight-planner): module and component generation

---
+++ apps/client/src/app/pages/fight-planner/fight-planner.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FightPlannerComponent } from './fight-planner/fight-planner.component';
+
+@NgModule({
+  declarations: [FightPlannerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FightPlannerModule { }
