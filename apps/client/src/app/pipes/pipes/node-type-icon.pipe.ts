7592fe1e981ed92e6b7dbea26110e67824948e56
Supamiu <contact@flavien-normand.fr>
2019-12-05 19:11:14 +0100

    chore: better icons for node types
    Signed-off-by: Supamiu <contact@flavien-normand.fr>

--- apps/client/src/app/pipes/pipes/node-type-icon.pipe.ts
+++ apps/client/src/app/pipes/pipes/node-type-icon.pipe.ts
@@ -10,10 +10,19 @@ export class NodeTypeIconPipe implements PipeTransform {
     './assets/icons/nodes/060437.png',
     './assets/icons/nodes/060433.png',
     './assets/icons/nodes/060432.png',
-    'https://garlandtools.org/db/images/060445.png'];
+    './assets/icons/nodes/060465.png'
+  ];
 
-  transform(type: number): string {
-    return NodeTypeIconPipe.icons[type];
+  public static timed_icons = [
+    './assets/icons/nodes/060464.png',
+    './assets/icons/nodes/060463.png',
+    './assets/icons/nodes/060462.png',
+    './assets/icons/nodes/060461.png',
+    './assets/icons/nodes/060466.png'
+  ];
+
+  transform(type: number, timed?: boolean): string {
+    return timed ? NodeTypeIconPipe.timed_icons[type] : NodeTypeIconPipe.icons[type];
   }
 
 }
