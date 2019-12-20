a19cc3100df6f142ec7863bc0c2450e59a012a64
Supamiu <contact@flavien-normand.fr>
2019-12-20 19:15:40 +0100

    chore(perf): added intersection observer optimizer directive
    Signed-off-by: Supamiu <contact@flavien-normand.fr>

---
+++ apps/client/src/app/core/tools/lazy-component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, ChangeDetectorRef, Directive, ElementRef } from '@angular/core';
+import { Subject } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+@Directive({
+  selector: '[appLazyComponent]'
+})
+export class LazyComponentDirective implements AfterViewInit {
+
+  private _intersectionObserver?: IntersectionObserver;
+
+  private intersects$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(
+    private _element: ElementRef,
+    private cdRef: ChangeDetectorRef
+  ) {
+    this.intersects$.pipe(
+      distinctUntilChanged()
+    ).subscribe((visible) => {
+      if (visible) {
+        this.cdRef.reattach();
+      } else {
+        this.cdRef.detach();
+      }
+    });
+  }
+
+  public ngAfterViewInit() {
+    this._intersectionObserver = new IntersectionObserver(entries => {
+      this.checkForIntersection(entries);
+    }, {});
+    this._intersectionObserver.observe(<Element>(this._element.nativeElement));
+  }
+
+  private checkForIntersection = (entries: Array<IntersectionObserverEntry>) => {
+    entries.forEach((entry: IntersectionObserverEntry) => {
+      this.intersects$.next(this.checkIfIntersecting(entry));
+    });
+  };
+
+  private checkIfIntersecting(entry: IntersectionObserverEntry) {
+    return (<any>entry).isIntersecting && entry.target === this._element.nativeElement;
+  }
+}
