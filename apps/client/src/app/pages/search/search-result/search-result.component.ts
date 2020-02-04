f7ca1b3a8c594038c0ade9fea6f8ced207b93b09
Supamiu <contact@flavien-normand.fr>
2020-02-04 09:36:44 +0100

    fix(search): performance improvements for results display

---
+++ apps/client/src/app/pages/search/search-result/search-result.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { SearchType } from '../search-type';
+import { SearchResult } from '../../../model/search/search-result';
+import { HtmlToolsService } from '../../../core/tools/html-tools.service';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SearchResultComponent {
+
+  @Input()
+  row: SearchResult;
+
+  @Input()
+  odd: boolean;
+
+  @Input()
+  currentLang: string;
+
+  @Output()
+  addItemsToList = new EventEmitter();
+
+  @Output()
+  createQuickList = new EventEmitter();
+
+  @Output()
+  openInSimulator = new EventEmitter();
+
+  @Output()
+  selectChange = new EventEmitter();
+
+  searchTypes = SearchType;
+
+  constructor(private htmlTools: HtmlToolsService) {
+  }
+
+  public getStars(amount: number): string {
+    return this.htmlTools.generateStars(amount);
+  }
+
+}
