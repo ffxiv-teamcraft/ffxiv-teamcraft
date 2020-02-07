a47bbf9724bff645fea8a033d5193e8c8fc09077
Innpaul Zhang <zyp108421@gmail.com>
2020-02-07 22:19:46 +0800

    chore: tools for generating CN data files from csv

---
+++ data-extraction/cn-data-exporter/read-csv.js
@@ -0,0 +1,57 @@
+module.exports = function (content, fields) {
+  const lines = []
+  let line = []
+
+  let inQuote = false
+  let buf = ''
+  for (let i = 0; i < content.length; ++i) {
+    let chr = content[i]
+    if (inQuote) {
+      if (chr === '"') {
+        inQuote = false
+      } else {
+        buf += chr
+      }
+      continue
+    }
+
+    switch (chr) {
+      case ',':
+        line.push(buf)
+        buf = ''
+        break
+      case '"':
+        inQuote = !inQuote
+        break
+      case '\r': // @todo: find a better way to handle \r
+        break
+      case '\n':
+        line.push(buf)
+        buf = ''
+        lines.push(line)
+        line = []
+        break
+      default:
+        buf += chr
+        break
+    }
+  }
+
+  if (line.length) {
+    lines.push(line)
+  }
+
+  if (!Array.isArray(fields)) {
+    fields = lines[1]
+  }
+
+  return lines.slice(3).map(line => fields.reduce((obj, field, i) => {
+    if (field) {
+      obj[field] = line[i]
+    } else {
+      obj[`#${i}`] = line[i]
+    }
+
+    return obj
+  }, { _: line }))
+}
