02d77b2f9bc6eae495f847f2d56cb2374bd4a6bf
Supamiu <contact@flavien-normand.fr>
2020-02-12 23:01:08 +0100

    chore(ci): yet another try !
    Signed-off-by: Supamiu <contact@flavien-normand.fr>

--- tools/desktop/deploy.sh
+++ tools/desktop/deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-apt-get install rsync
-
 ssh-keyscan -H ssh.ffxivteamcraft.com >> ~/.ssh/known_hosts
 
-rsync -avz ./release/squirrel-windows/* dalamud@ssh.ffxivteamcraft.com:~/update.ffxivteamcraft.com/
+scp -p ./release/squirrel-windows/* dalamud@ssh.ffxivteamcraft.com:~/update.ffxivteamcraft.com/
