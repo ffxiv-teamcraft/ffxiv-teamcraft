a4d9486f8cfc809155e987b36503dcf6f2d77f73
Supamiu <contact@flavien-normand.fr>
2020-02-13 15:34:41 +0100

    chore(ci): yet another try !

--- tools/desktop/deploy.sh
+++ tools/desktop/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+sudo apt-get install rsync
+
+touch ~/.ssh/known_hosts
+
 ssh-keyscan -H ssh.ffxivteamcraft.com >> ~/.ssh/known_hosts
 
 rsync -avz ./release/squirrel-windows/* dalamud@ssh.ffxivteamcraft.com:~/update.ffxivteamcraft.com/
