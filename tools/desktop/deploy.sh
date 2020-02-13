d52514261cdc7da35943450311dfb40d098494f9
Supamiu <contact@flavien-normand.fr>
2020-02-13 14:48:47 +0100

    chore(ci): yet another try !

--- tools/desktop/deploy.sh
+++ tools/desktop/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ssh-keyscan -H ssh.ffxivteamcraft.com >> ~/.ssh/known_hosts
 
-scp -p ./release/squirrel-windows/* dalamud@ssh.ffxivteamcraft.com:~/update.ffxivteamcraft.com/
+rsync -avz ./release/squirrel-windows/* dalamud@ssh.ffxivteamcraft.com:~/update.ffxivteamcraft.com/
