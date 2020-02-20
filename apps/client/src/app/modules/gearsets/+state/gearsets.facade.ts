103178b64403ce3d50a5de62015eec63d8cbdf4f
Supamiu <contact@flavien-normand.fr>
2020-02-20 20:42:24 +0100

    fix(gearsets): fixed an issue with ariyala import
    Signed-off-by: Supamiu <contact@flavien-normand.fr>

--- apps/client/src/app/modules/gearsets/+state/gearsets.facade.ts
+++ apps/client/src/app/modules/gearsets/+state/gearsets.facade.ts
@@ -29,7 +29,7 @@ import { Memoized } from '../../../core/decorators/memoized';
 import { AriyalaLinkParser } from '../../../pages/lists/list-import-popup/link-parser/ariyala-link-parser';
 import { HttpClient } from '@angular/common/http';
 import { AriyalaMateria } from '../../../pages/lists/list-import-popup/link-parser/aryiala-materia';
-import * as jobAbbrs from '../../../core/data/sources/job-abbr.json';
+import * as jobNames from '../../../core/data/sources/job-name.json';
 import { XivapiService } from '@xivapi/angular-client';
 import { PermissionLevel } from '../../../core/database/permissions/permission-level.enum';
 
@@ -187,7 +187,7 @@ export class GearsetsFacade {
           dataset = data.datasets[Object.keys(data.datasets)[0]];
         }
         const gearset = new TeamcraftGearset();
-        gearset.job = +Object.keys(jobAbbrs).find(k => jobAbbrs[k].en === data.content);
+        gearset.job = +Object.keys(jobNames).find(k => jobNames[k].en === data.content);
         gearset.name = url;
         gearset.mainHand = this.getAriyalaEquipmentPiece(dataset, 'mainhand');
         gearset.offHand = this.getAriyalaEquipmentPiece(dataset, 'offhand');
