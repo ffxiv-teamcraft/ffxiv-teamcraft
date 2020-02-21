d482516ef9c97f026dd6c05c9b0c7823e7715dfa
Robert Dailey <rcdailey@gmail.com>
2020-02-21 12:08:50 -0600

    Added "Consolidate Stacks" Inventory Optimizer
    A new inventory optimizer that will suggest high-quality (HQ) stacks of
    items that can have their quality lowered so that it can be joined with
    an existing normal-quality (NQ) stack of that same type of item. This is
    a very reliable way to free up inventory slots.
    
    Because users may not want all stacks of items suggested, filtering is
    provided by expansion. For example, if a user wants all items in
    Heavensward and below suggested (Heavensward + A Realm Reborn), they
    will select "Heavensward" from the filter select box. Items with HQ and
    NQ stacks from Stormblood and Shadowbringers will *not* be selected in
    this case because they are filtered out. You can also clear the filter
    box (so it says "Pick Expansion") which completely disables filtering,
    showing all available items in your inventory to which the optimization
    applies.
    
    In general, if item database information is incomplete, an item does not
    get filtered even if it may have been otherwise. Examples:
    
    * The specified item does not have an assigned patch.
    * A patch to which an item is assigned is not defined.

---
+++ apps/client/src/app/pages/inventory-optimizer/optimizations/consolidate-stacks.ts
@@ -0,0 +1,79 @@
+import { InventoryOptimizer } from './inventory-optimizer';
+import { InventoryItem } from '../../../model/user/inventory/inventory-item';
+import { UserInventory } from '../../../model/user/inventory/user-inventory';
+import { ListRow } from '../../../modules/list/model/list-row';
+import { TranslateService } from '@ngx-translate/core';
+import { InventoryFacade } from '../../../modules/inventory/+state/inventory.facade';
+import { LazyDataService } from '../../../core/data/lazy-data.service';
+import { Memoized } from '../../../core/decorators/memoized';
+
+export class ConsolidateStacks extends InventoryOptimizer {
+
+  static SELECTION_KEY = 'optimizer:consolidate-stacks:selection';
+
+  constructor(private translate: TranslateService, private inventoryFacade: InventoryFacade, private lazyData: LazyDataService) {
+    super();
+  }
+
+  @Memoized()
+  getPatch(id: number): any {
+    return this.lazyData.patches.find(p => p.ID === id);
+  }
+
+  itemInExpansion(itemId: number, expansion: number): boolean {
+    // If the specified item does not have a patch associated with it, we do not filter it.
+    const itemPatch = this.lazyData.data.itemPatch[itemId];
+    if (itemPatch === null) {
+      return true;
+    }
+
+    // If no patch is defined for this patch that the item is assigned to, do not filter it.
+    const patch = this.getPatch(itemPatch);
+    if (patch === null) {
+      return true;
+    }
+
+    return patch.ExVersion <= expansion;
+  }
+
+  protected _getOptimization(item: InventoryItem, inventory: UserInventory, data: ListRow): { [p: string]: number | string } | null {
+    const expansion = localStorage.getItem(ConsolidateStacks.SELECTION_KEY);
+
+    // After filtering is completed, this container will have a list of HQ-only items that need to
+    // be merged with a separate type-same NQ stack.
+    const toConsolidate = inventory.toArray()
+      .filter(i => {
+        // Implementation Note:
+        //
+        // Majority of items will be filtered out because the IDs do not match. Make the `itemId`
+        // comparison the first condition we check for performance reasons. This means we spend less
+        // time iterating the IGNORED_CONTAINERS container. In general, comparisons are performed
+        // from least-expensive to most-expensive (algorithmically).
+        return i.itemId === item.itemId
+          // If we have no expansion selected to filter on, treat *all* items as "in expansion"
+          && (expansion === null || this.itemInExpansion(item.itemId, +expansion))
+          && item.hq === false
+          && i.hq === true
+          && i.spiritBond === 0
+          && !InventoryOptimizer.inSameSlot(i, item)
+          && item.quantity + i.quantity < this.lazyData.data.stackSizes[i.itemId]
+          && InventoryOptimizer.IGNORED_CONTAINERS.indexOf(i.containerId) === -1;
+      });
+
+    if (toConsolidate.length === 0) {
+      return null;
+    }
+
+    return {
+      containers: toConsolidate.map(dupe => {
+        return dupe.retainerName ||
+          this.translate.instant(`INVENTORY.BAG.${this.inventoryFacade.getContainerName(dupe.containerId)}`);
+      }).join(', ')
+    };
+  }
+
+  getId(): string {
+    return 'CONSOLIDATE_STACKS';
+  }
+
+}
