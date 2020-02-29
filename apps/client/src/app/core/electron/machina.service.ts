4c119210b929e63fc9ecd5abd5ebd937697b12e9
Ra-Workspace <ra.workspace@gmail.com>
2020-03-01 03:56:24 +0900

    chore(desktop): delete unnecessary retainer name checker

--- apps/client/src/app/core/electron/machina.service.ts
+++ apps/client/src/app/core/electron/machina.service.ts
@@ -46,17 +46,10 @@ export class MachinaService {
   private retainerSpawns$: Observable<string> = combineLatest([this.retainerInformations$, this.ipc.npcSpawnPackets$, this.settings.region$]).pipe(
     map(([retainers, spawn, region]) => {
       let name: string = spawn.name;
-      const splitForCheck = name.split('');
-      // If there's a char below SPACE (\u0020), it's simply not possible for this name to be valid, let's strip the invalid part
-      const borkedData = splitForCheck.findIndex((char) => {
-        return char < ' ';
-      });
       if ((region === Region.Korea && environment.koreanGameVersion < 5.2) || (region === Region.China && environment.chineseGameVersion < 5.2)) {
-        let uint8array = new TextEncoder().encode(name);
+        let uint8array: Uint8Array = new TextEncoder().encode(name);
         name = new TextDecoder().decode(uint8array.slice(4));
-      } else if (borkedData > -1) {
-        name = name.substring(borkedData);
-      }
+      } 
       return [retainers, name];
     }),
     filter(([retainers, name]: [any[], string]) => name.length > 0 && retainers.some(retainer => retainer.name === name)),
