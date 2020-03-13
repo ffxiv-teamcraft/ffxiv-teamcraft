a2de37c4fac39b3b76b27e744e7a6bf19e12a82b
Supamiu <contact@flavien-normand.fr>
2020-03-13 15:35:33 +0100

    chore: more work on mappy debug overlay, now propery tracks player !

--- apps/client/src/app/pages/mappy-overlay/mappy-overlay/mappy-overlay.component.ts
+++ apps/client/src/app/pages/mappy-overlay/mappy-overlay/mappy-overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { IpcService } from '../../../core/electron/ipc.service';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -6,6 +6,7 @@ import { LazyDataService } from '../../../core/data/lazy-data.service';
 import { MapService } from '../../../modules/map/map.service';
 import { Vector2 } from '../../../core/tools/vector2';
 import { MapData } from '../../../modules/map/map-data';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mappy-overlay',
@@ -13,6 +14,15 @@ import { MapData } from '../../../modules/map/map-data';
   styleUrls: ['./mappy-overlay.component.less']
 })
 export class MappyOverlayComponent {
+
+  scale = 1;
+  pan: Vector2 = { x: -1024, y: -1024 };
+  editedPan = { x: 0, y: 0 };
+
+  playerMarkerSize: Vector2 = {
+    x: 48,
+    y: 48
+  };
 
   public state$: ReplaySubject<any> = new ReplaySubject<any>();
 
@@ -22,30 +32,72 @@ export class MappyOverlayComponent {
       return {
         ...state,
         map: mapData,
-        mapStyle: mapData ? this.getMapStyle(mapData, state.playerCoords) : {}
+        player: this.getPosition(mapData, state.playerCoords)
       };
     })
   );
 
-  constructor(private ipc: IpcService, private lazyData: LazyDataService, private mapService: MapService) {
+  @ViewChild('canvasElement', { static: false })
+  public canvasElement: TemplateRef<HTMLCanvasElement>;
+
+  constructor(private ipc: IpcService, private lazyData: LazyDataService, private mapService: MapService,
+              private sanitizer: DomSanitizer) {
     this.ipc.on('mappy-state', (event, data) => {
       this.state$.next(data);
     });
     this.ipc.send('mappy-state:get');
   }
 
-  public getMapStyle(mapData: MapData, coords: Vector2): any {
+  public getPosition(mapData: MapData, coords: Vector2): any {
     const c = mapData.size_factor / 100;
     const raw = {
-      x: (41 / c) * (coords.x / 2048) + 1,
-      y: (41 / c) * (coords.y / 2048) + 1
+      x: (41 / c) * ((coords.x + 1024) / 2048) + 1,
+      y: (41 / c) * ((coords.y + 1024) / 2048) + 1
     };
-    const position = this.mapService.getPositionOnMap(mapData, raw);
-    return {
-      transform: `translate(${50 - position.x}%, ${50 - position.y}%)`,
-      position: position,
-      raw: raw
-    };
+    return this.mapService.getPositionOnMap(mapData, raw);
+  }
+
+  /* Method which adds style to the image */
+  imageTransform(): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(`translate(${this.pan['x'] + this.editedPan['x']}px,${
+      this.pan['y'] + this.editedPan['y']
+    }px) rotate(0deg) scale(${this.scale})`);
+  }
+
+  /* Method will be called on pan image */
+  onPan(e: any): void {
+    this.editedPan = { x: e['deltaX'], y: e['deltaY'] };
+    if (e.isFinal) {
+      this.pan = {
+        x: this.pan.x + this.editedPan.x,
+        y: this.pan.y + this.editedPan.y
+      };
+      this.editedPan = { x: 0, y: 0 };
+    }
+  }
+
+  /* Method will be called when user zooms image */
+  onZoomP(): void {
+    this.scale = this.scale + 0.1;
+  }
+
+  /* Method will be called when user zooms out image */
+  onZoomM(): void {
+    if (this.scale <= 0.5) {
+      return;
+    } else {
+      this.scale = this.scale - 0.1;
+    }
+  }
+
+  /* Method will be called on mouse wheel scroll */
+  onScroll(e: any): void {
+    if (e['deltaY'] < 0) {
+      this.onZoomP();
+    }
+    if (e['deltaY'] > 0) {
+      this.onZoomM();
+    }
   }
 
 }
