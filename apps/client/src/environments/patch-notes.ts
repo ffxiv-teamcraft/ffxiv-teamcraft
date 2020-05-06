7fb02046bfbd12f77ad166faec1b01ddd78a6dc2
Supamiu <contact@flavien-normand.fr>
2020-05-06 20:35:35 +0200

    chore: patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,6 +1,8 @@
 export const patchNotes = `### Bug Fixes
 
+* **core:** fixed a build issue preventing proper release.
 * **db:** fish rankings are no longer affected by spot filter.
+* **desktop:** fixed an issue with auto updater not having single instance lock.
 * **profile:** fixed jobs panel display.
 * **search:** better handling of search filters and overall form display.
 * **search:** first option in history isn't automatically selected anymore.
@@ -10,4 +12,4 @@ export const patchNotes = `### Bug Fixes
 ### Features
 
 * **profile:** added a public profile link in place of the removed apkallu falls button.
-* **profile:** added fishing rankings summary in public profile.`;
+* **profile:** added fishing rankings summary in public profile.`;
