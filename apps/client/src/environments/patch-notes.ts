e931fac8880cc2fc0dd7a56728354501bf131745
Supamiu <contact@flavien-normand.fr>
2020-05-08 15:54:36 +0200

    chore: patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,15 +1,6 @@
 export const patchNotes = `### Bug Fixes
 
-* **core:** fixed a build issue preventing proper release.
-* **db:** fish rankings are no longer affected by spot filter.
-* **desktop:** fixed an issue with auto updater not having single instance lock.
-* **profile:** fixed jobs panel display.
-* **search:** better handling of search filters and overall form display.
-* **search:** first option in history isn't automatically selected anymore.
-* **tutorial-popup:** fixed missing translation.
-
-
-### Features
-
-* **profile:** added a public profile link in place of the removed apkallu falls button.
-* **profile:** added fishing rankings summary in public profile.`;
+* **db:** fixed a bug preventing collectability markers from showing on G2 skybuilders items.
+* **gearset:** fixed an issue with filters getting too few items for levels < 60.
+* **list:** fixed "mark as done in log" button for precrafts.
+* **workshop:** fixed permissions propagation.`;
