dd753ef5fcf7ddca3f974b6a19c4a2017f6c8673
Supamiu <contact@flavien-normand.fr>
2020-05-08 17:26:58 +0200

    fix(desktop): fixed updater spawning error on application patching

--- desktop/main.js
+++ desktop/main.js
@@ -178,28 +178,11 @@ ipcMain.on('update:check', () => {
 /**
  * End autoupdater
  */
-
-const gotTheLock = app.requestSingleInstanceLock();
-
-if (!gotTheLock) {
-  app.quit();
-} else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    // Someone tried to run a second instance, we should focus our window.
-    if (win) {
-      if (win.isMinimized()) {
-        win.restore();
-      }
-      win.focus();
-    }
-  });
-
-  // Create window on electron intialization
-  app.on('ready', () => {
-    createWindow();
-    createTray();
-  });
-}
+// Create window on electron intialization
+app.on('ready', () => {
+  createWindow();
+  createTray();
+});
 
 function createWindow() {
   app.setAsDefaultProtocolClient('teamcraft');
