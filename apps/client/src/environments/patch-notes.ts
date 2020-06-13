b3873d410447cce39ceb4caabe6a8e1a5c7ae51b
Supamiu <contact@flavien-normand.fr>
2020-06-13 09:58:17 +0200

    chore(release): patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,10 +1,9 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed custom alarm names in alarms picker.
-* **inventory-optimizer:** fixed scrolling in details panels with < 10 items.
-
-
-### Features
-
-* **data:** cn 5.18.
-* **desktop:** support for 5.18 chinese packet capture.`;
+* **log-tracking:** fixed unreliable save system with slower connections.
+* fixed an issue with close button not showing on tips blocks.
+* **desktop:** you can now properly hide the packet capture banner.
+* **gearset:** materia selector now properly works with smaller displays.
+* **list:** item comments popup now opens properly.
+* **simulator:** fixed infinite loading for korean region with english language.
+* **universalis:** fixed wrong world change detection.`;
