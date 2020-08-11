7d3063471ed1f62ce5e8b6d702e5de074ba2f60b
Supamiu <contact@flavien-normand.fr>
2020-08-11 21:00:41 +0200

    chore(release): 5.3 patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,13 +1,28 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** fixed reduction results reporter and reduction data.
-* **db:** fixed small display issue in details pages.
-* **list:** fixed "add all alarms for this item" button.
-* **marketboard:** enabled marketboard button for chinese servers accross the entire app.
-* **teams:** fixed default team permission level on lists (Participate).
+* **alarms:** fixed some broken fishing alarms.
+* **desktop:** better firewall rule cleanup and lock management.
+* **desktop:** fix "cannot acquire lock" error.
+* **desynth:** filter only desynthesizable.
+* **gathering:** fix redundant weather content if weather is not present.
+* **list:** fixed position detection for optimized path on drops.
+* **list:** fixed wrong zone label for some fishing nodes.
+* **log-tracker:** better detection for pages required for achievements.
+* **marketboard:** removed marketboard button from items that cannot be sold.
 
 
 ### Features
 
-* **data:** update for cn patch-5.2.
-* **desktop:** support for chinese 5.2 update.`;
+* **core:** adding support for mouse scrolling in various number inputs.
+* **data:** full 5.3 patch support.
+* **db:** added ingame fishing drawing to fish db pages (thanks hez).
+* **desktop:** new item search overlay to wuickly find item details.
+* **desktop:** new player metrics system (opt-in, you have to enable it inside the settings page).
+* **inventory-optimizer:** new optimizer for 100% spiritbond gear pieces.
+* **inventory-optimizer:** new optimizer for items easily gatherable.
+* **list:** you can now include final items in the main layout (new layout option).
+* **list:** you can now mark items as done from the requirements popup.
+* **lists:** lists shared with read access now show up in shared list category.
+* **marketboard:** added last update to marketboard details.
+* **search:** removed collectable flag on search results for global region.
+* **search:** search in English if search in Korean has no results.`;
