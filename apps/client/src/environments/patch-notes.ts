38a027fe611afeec5c7cb367e10d7ce8f31cd3da
Supamiu <contact@flavien-normand.fr>
2020-08-11 22:37:58 +0200

    chore(release): 7.3.1 patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,28 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed some broken fishing alarms.
-* **desktop:** better firewall rule cleanup and lock management.
-* **desktop:** fix "cannot acquire lock" error.
-* **desynth:** filter only desynthesizable.
-* **gathering:** fix redundant weather content if weather is not present.
-* **list:** fixed position detection for optimized path on drops.
-* **list:** fixed wrong zone label for some fishing nodes.
-* **log-tracker:** better detection for pages required for achievements.
-* **marketboard:** removed marketboard button from items that cannot be sold.
-
-
-### Features
-
-* **core:** adding support for mouse scrolling in various number inputs.
-* **data:** full 5.3 patch support.
-* **db:** added ingame fishing drawing to fish db pages (thanks hez).
-* **desktop:** new item search overlay to wuickly find item details.
-* **desktop:** new player metrics system (opt-in, you have to enable it inside the settings page).
-* **inventory-optimizer:** new optimizer for 100% spiritbond gear pieces.
-* **inventory-optimizer:** new optimizer for items easily gatherable.
-* **list:** you can now include final items in the main layout (new layout option).
-* **list:** you can now mark items as done from the requirements popup.
-* **lists:** lists shared with read access now show up in shared list category.
-* **marketboard:** added last update to marketboard details.
-* **search:** removed collectable flag on search results for global region.
-* **search:** search in English if search in Korean has no results.`;
+* **alarms:** fixed an issue with new items crashing alarms page.
+* **list:** fixed an issue preventing some items from being added to a list.`;
