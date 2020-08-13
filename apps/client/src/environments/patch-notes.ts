96634f9525bd398a5ac6faaf34493c7470e4d1e7
Supamiu <contact@flavien-normand.fr>
2020-08-13 19:25:28 +0200

    chore(release): 7.3.4 patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,4 +1,11 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed error at app startup.
-* **desktop:** fixed error when opening metrics dashboard.`;
+* **core:** fixed extracts file staying in cache and not clearing properly.
+* **currency-spending:** page now works properly.
+* **desktop:** fix for firewall rule not created properly.
+* **desktop:** fixed firewall rules management for packet capture.
+* **desktop:** fixed google oauth.
+* **inventory-optimizer:** fixed optimizer not loading in specific cases.
+* **list:** item picker's button is now disabled if nothing is selected.
+* **metrics:** changed filter for "Items crafted" to make it show what it's supposed to.
+* **settings:** fixed missing translation for reset password confirmation tooltip.`;
