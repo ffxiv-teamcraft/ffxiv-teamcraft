0b2aa70b47d8c013a983a5bd826c082247258510
Supamiu <contact@flavien-normand.fr>
2020-08-14 10:52:47 +0200

    fix(search): fixed an issue with DE search and Ä character

--- apps/client/src/app/core/api/data.service.ts
+++ apps/client/src/app/core/api/data.service.ts
@@ -171,7 +171,7 @@ export class DataService {
    */
   public searchItem(query: string, filters: SearchFilter[], onlyCraftable: boolean, sort: [string, 'asc' | 'desc'] = [null, 'desc']): Observable<SearchResult[]> {
     // Filter HQ and Collectable Symbols from search
-    query = query.replace(/[\ue03a-\ue03d]/g, '');
+    query = query.replace(/[\ue03a-\ue03d]/g, '').toLowerCase();
 
     const xivapiFilters: XivapiSearchFilter[] = [].concat.apply([], filters
       .filter(f => {
