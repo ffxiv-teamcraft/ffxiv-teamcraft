7a449632e24b1eb05eb75e368839953cedb6202b
Supamiu <contact@flavien-normand.fr>
2020-08-16 01:56:04 +0200

    chore(release): hotfix patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,21 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **auth:** fixed an issue with log completion on characterless accounts.
-* **db:** fixed missing nodes for levinstrike reduction.
-* **db:** fixed npc->shop links for scrip shops.
-* **desktop:** fixed an issue with item search overlay's opacity not being applied properly.
-* **desktop:** fixed vendor exchanges not recorded properly with inventory tracker.
-* **list:** fixed an issue sometimes hiding items in zone breakdown.
-* **lists:** you can now open favorite community lists.
-* **search:** fixed an issue with DE search and Ä character.
-* **simulator:** removed label for the 3000 control bonus cap.
-* **ux:** fixed font color for login error alert.
-
-
-### Features
-
-* **db:** 5.3 patch content for patch search & page.
-* **db:** spot list is now in first line of fish details page.
-* **desktop:** added button to create firewall rule in settings.
-* **desktop:** new automated missing firewall rule detector with one click fixer.
-* **inventory-optimizer:** don't process hidden optimizers anymore.`;
+* **desktop:** fixed possible firewall error after update.`;
