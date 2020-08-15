80da587dddd7a2d04ad025512649fe2a4a7b494a
Supamiu <contact@flavien-normand.fr>
2020-08-15 18:25:43 +0200

    chore(release): 7.3.5 patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,11 +1,21 @@
 export const patchNotes = `### Bug Fixes
 
-* **core:** fixed extracts file staying in cache and not clearing properly.
-* **currency-spending:** page now works properly.
-* **desktop:** fix for firewall rule not created properly.
-* **desktop:** fixed firewall rules management for packet capture.
-* **desktop:** fixed google oauth.
-* **inventory-optimizer:** fixed optimizer not loading in specific cases.
-* **list:** item picker's button is now disabled if nothing is selected.
-* **metrics:** changed filter for "Items crafted" to make it show what it's supposed to.
-* **settings:** fixed missing translation for reset password confirmation tooltip.`;
+* **auth:** fixed an issue with log completion on characterless accounts.
+* **db:** fixed missing nodes for levinstrike reduction.
+* **db:** fixed npc->shop links for scrip shops.
+* **desktop:** fixed an issue with item search overlay's opacity not being applied properly.
+* **desktop:** fixed vendor exchanges not recorded properly with inventory tracker.
+* **list:** fixed an issue sometimes hiding items in zone breakdown.
+* **lists:** you can now open favorite community lists.
+* **search:** fixed an issue with DE search and Ä character.
+* **simulator:** removed label for the 3000 control bonus cap.
+* **ux:** fixed font color for login error alert.
+
+
+### Features
+
+* **db:** 5.3 patch content for patch search & page.
+* **db:** spot list is now in first line of fish details page.
+* **desktop:** added button to create firewall rule in settings.
+* **desktop:** new automated missing firewall rule detector with one click fixer.
+* **inventory-optimizer:** don't process hidden optimizers anymore.`;
