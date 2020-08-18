4505c8f7d3a8b93d5b7d0f8e9b9fb3384e831dbe
Supamiu <contact@flavien-normand.fr>
2020-08-18 20:06:51 +0200

    chore: dashboard default layout fix

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,17 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed possible firewall error after update.`;
+* **desktop:** better automated firewall rule.
+* **desktop:** fixed discord webhook creation for teams.
+* **desynth:** added better filtering for desynth leveling guide.
+* **gc-supply:** changed list generation button's color to primary.
+* **pricing:** do not use HQ price for items that cannot be HQ.
+* **simulator:** fixed a bug with buffs ticking on some failed actions.
+* **user:** possible fix for account having network issues.
+
+
+### Features
+
+* **data:** update for cn patch-5.21.
+* **db:** added new collectable thresholds inside simulator.
+* **helpers:** new collectables listing page.
+* **pcap:** support for inventoryTransaction in CN region.`;
