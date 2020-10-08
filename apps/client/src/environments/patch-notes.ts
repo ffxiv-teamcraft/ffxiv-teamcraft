322b1405b29477e699e1f6ced416f57de078da07
Supamiu <contact@flavien-normand.fr>
2020-10-08 16:51:00 +0200

    chore(release): patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,5 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **food-picker:** fixed an issue with some items being ignored entirely.
-* **gearset:** fixed an issue preventing new gearsets from being created.
-* **gearset:** fixed an issue with BLU weapons melding.`;
+* **alarms:** fixed alarms overlay being empty.
+* **gearset:** progression is now properly saved for materias.`;
