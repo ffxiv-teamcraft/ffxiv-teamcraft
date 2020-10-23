216c5f1cfd9f9673514a5d09f4ba5c30385694b7
Supamiu <contact@flavien-normand.fr>
2020-10-23 17:20:00 +0200

    feat(list): you can now split a list in smaller lists

---
+++ apps/client/src/app/modules/list/list-split-popup/list-split-popup.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { List } from '../model/list';
+import { ListPickerService } from '../../list-picker/list-picker.service';
+import { ListRow } from '../model/list-row';
+import { ListsFacade } from '../+state/lists.facade';
+import { NzModalRef } from 'ng-zorro-antd';
+
+@Component({
+  selector: 'app-list-split-popup',
+  templateUrl: './list-split-popup.component.html',
+  styleUrls: ['./list-split-popup.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListSplitPopupComponent {
+
+  public list: List;
+
+  public removeFromList = true;
+
+  public selectedItems = [];
+
+  public constructor(private listPicker: ListPickerService, private listsFacade: ListsFacade,
+                     private modalRef: NzModalRef) {
+  }
+
+  setSelection(item: ListRow, selected: boolean): void {
+    if (selected) {
+      this.selectedItems.push(item.id);
+    } else {
+      this.selectedItems = this.selectedItems.filter(i => i !== item.id);
+    }
+  }
+
+  split(): void {
+    const itemsToAdd = this.list.finalItems.filter(row => this.selectedItems.includes(row.id));
+    this.listPicker.addToList(...itemsToAdd);
+    if (this.removeFromList) {
+      this.list.finalItems = this.list.finalItems.filter(row => !this.selectedItems.includes(row.id));
+      this.list.clean();
+      this.listsFacade.updateList(this.list);
+    }
+    this.modalRef.close();
+  }
+
+}
