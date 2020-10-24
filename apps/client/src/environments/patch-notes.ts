153426184c640287432bf423e4573e2062bca7af
Supamiu <contact@flavien-normand.fr>
2020-10-24 11:36:30 +0200

    chore(release): patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,30 +1,15 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed an issue with autofill sometimes not filling properly.
-* **gearset:** fixed an issue with cost computing being inacurate when progression is set.
-* **inventory:** fixed "track items on sale" setting always being enabled.
-* **list:** fixed an issue with missing spearfishing locations (some might be wrong tho).
-* **list:** fixed an issue with some spearfishing items breaking list display.
-* **list:** fixed ignore button in total price popup.
-* **list:** update perf improvements, possible autofill fix.
-* **log-tracker:** fixed a rollback on several entries being unchecked quickly.
-* **simulator:** fixed reset rotation button not working.
+* **alarms:** going further in the future looking for weather spawns now.
+* **autofill:** better integrity check on list autofill update.
+* **desktop:** fixed an error with auto log filling as you craft.
+* **inventory:** big performance improvement for inventory capture & autofill.
+* **layout:** maximum vendor price now applies to every vendor-related filters.
 
 
 ### Features
 
-* **community-rotations:** rotations are now sorted by length ASC.
-* **desktop:** better optimized build for electron (now using aot properly).
-* **desktop:** new menu entries in tray bar icon menu (new overlays & clickthrough option).
-* **food-picker:** shareable urls and sorting based on selected stats.
-* **gearsets:** you can now mass import your gear and keep them updated with the new sync button.
-* **inventory:** new button to copy entire inventory as JSON or CSV.
-* **layout:** DoH and DoL levels are now affecting layout filters.
-* **list:** new global setting for maximum vendor price in list layouts.
-* **list:** you can now define priorities on jobs to pick for items with multiple recipes.
-* **lists:** added # of items in list next to its name in list picker.
-* **log-tracker:** added leve icon next to recipes beign used for leves.
-* **pricing:** new checkbox to ignore completed items in pricing mode.
-* **simulator:** new detailed display option for actions list.
-* **simulator:** you can now change the end message of your generated macros.
-* **hotkey:** new Ctrl + Shift + C shortcut to open link popup or open in desktop app.`;
+* **db:** you can now mark gatherable items as done in log from their db page.
+* **list:** added link to the fishing spot db page on fish details popup.
+* **list:** you can now split a list in smaller lists.
+* **recipe-finder:** new filter to consider only recipes you never crafted.`;
