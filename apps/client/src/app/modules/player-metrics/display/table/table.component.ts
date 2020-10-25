19740b7607a0aad494d25cf04dd921fa0b296eb2
Supamiu <contact@flavien-normand.fr>
2020-10-25 09:07:50 +0100

    feat(metrics): you can now sort table columns display

--- apps/client/src/app/modules/player-metrics/display/table/table.component.ts
+++ apps/client/src/app/modules/player-metrics/display/table/table.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AbstractMetricDisplayComponent } from '../abstract-metric-display-component';
 import { ProbeSource } from '../../model/probe-source';
 import { TranslateService } from '@ngx-translate/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-table',
@@ -11,6 +12,24 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class TableComponent extends AbstractMetricDisplayComponent {
   ProbeSource = ProbeSource;
+
+  public columns = [
+    {
+      title: 'Value',
+      compare: (a, b) => a.data[1] - b.data[1],
+      priority: 2
+    },
+    {
+      title: 'Source',
+      compare: (a, b) => a.source - b.source,
+      priority: 3
+    },
+    {
+      title: 'Timestamp',
+      compare: (a, b) => a.timestamp - b.timestamp,
+      priority: 1
+    }
+  ];
 
   constructor(public translate: TranslateService) {
     super();
