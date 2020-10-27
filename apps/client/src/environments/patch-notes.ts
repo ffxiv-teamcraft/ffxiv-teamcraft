dc76576563efc0af45c1fc9e25785b8b0fb7d8b0
Supamiu <contact@flavien-normand.fr>
2020-10-27 19:06:15 +0100

    chore(release): patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,15 +1,10 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** going further in the future looking for weather spawns now.
-* **autofill:** better integrity check on list autofill update.
-* **desktop:** fixed an error with auto log filling as you craft.
-* **inventory:** big performance improvement for inventory capture & autofill.
-* **layout:** maximum vendor price now applies to every vendor-related filters.
+* **db:** fishing spot misses popup weather filter is now working properly.
 
 
 ### Features
 
-* **db:** you can now mark gatherable items as done in log from their db page.
-* **list:** added link to the fishing spot db page on fish details popup.
-* **list:** you can now split a list in smaller lists.
-* **recipe-finder:** new filter to consider only recipes you never crafted.`;
+* **core:** support for 5.35 hotfix update.
+* **metrics:** new metric source: Retainer.
+* **metrics:** you can now sort table columns display.`;
