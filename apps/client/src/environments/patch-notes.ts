c17689f9566e212838ae5badcf62eaab8087d89d
Supamiu <contact@flavien-normand.fr>
2020-11-10 21:51:22 +0100

    chore(release): patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,13 +1,18 @@
 export const patchNotes = `### Bug Fixes
 
-* **auth:** fixed an issue with log tracker on anonymous accounts freezing some pages.
-* **db:** fixed an error when opening some items' hunting details.
-* **desktop:** fixed an issue with some items being moved and not tracked properly.
-* **layout:** fixed some misslabeling in layout editor popup.
+* **desktop:** fixed misslabelling happening when moving an item to a retainer using drag and drop.
+* **pricing:** fixed housing furnitures considered as HQ for price detection.
+* **search:** fixed an issue with lower level filters not applied properly (dated items).
+* **search:** fixed search type not being applied properly in some cases.
+* **workshop:** fixed an issue preventing workshop content from loading properly in lists page.
 
 
 ### Features
 
-* **list:** crystals in default crystal panel are now ordered by ID.
-* **loading-screen:** new patreon supporter: Jajali Jali * Gilgamesh.
-* **patreon:** new supporter: Mae Sorbet and Iranon Aira of Mateus.`;
+* **alarms-sidebar:** new icon to disable the alarm from sidebar.
+* **data:** support for korean v5.25 update.
+
+
+### Performance Improvements
+
+* **assets:** added line break removal in prod assets hashing process.`;
