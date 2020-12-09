181bf51d173ae52b1a9a880c47f5d98173664f0e
Supamiu <contact@flavien-normand.fr>
2020-12-09 16:17:44 +0100

    fix(nodes): fixed positions for some of the new ephemeral node locations

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -3,12 +3,11 @@ export const patchNotes = `### Bug Fixes
 * **alarms:** you can now properly add alarms to folders again.
 * **gathering:** fixed some items being considered as fishing while they weren't.
 * **lists:** changed stars notation for (#★) format.
-* **log-tracker:** fixed Quarrying 66-70 page missing from the tracker.
+* **log-trcker:** fixed Quarrying 66-70 page missing from the tracker.
 * **nodes:** fixed location and timer for 5.4 nodes and new collectable nodes.
 * **recipes:** fixed recipes for some FC crafts.
 
 
 ### Performance Improvements
 
-* **web:** moved hosting back to firebase's scaled solution.
-`;
+* **web:** moved hosting back to firebase's scaled solution.`;
