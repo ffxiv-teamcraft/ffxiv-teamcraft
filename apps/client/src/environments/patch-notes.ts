b8565c887642ad803bb4296852863f23a49a3a74
Supamiu <contact@flavien-normand.fr>
2020-12-09 13:51:59 +0100

    chore(release): patch notes

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,24 +1,14 @@
 export const patchNotes = `### Bug Fixes
 
-* **commissions:** fixed a bug with commission being rated twice.
-* **commissions:** fixed font size being too big in commissions page.
-* **commissions:** fixed missing translations in rating popup.
-* **commissions:** new empty commission lists are now created with Readonly permissions.
-* **crafting-replays:** fixed missing translation in result popup.
-* **db:** fix for new items inside lists and packet capture.
-* **db:** fixed some gathering nodes missing from search.
-* **desktop:** fixed google login showing javascript security warning.
-* **desktop:** fixed message error about service worker not being registered.
-* **fishing:** fixed an issue with some alarms being inaccurate in lists.
-* **map:** fixed navigation map having weird icon display.
-* **permissions:** fixed an issue preventing addition of more than 2 players to a list's permissions.
-* **simulator:** put HQ label in front of consumables.
+* **alarms:** you can now properly add alarms to folders again.
+* **gathering:** fixed some items being considered as fishing while they weren't.
+* **lists:** changed stars notation for (#★) format.
+* **log-tracker:** fixed Quarrying 66-70 page missing from the tracker.
+* **nodes:** fixed location and timer for 5.4 nodes and new collectable nodes.
+* **recipes:** fixed recipes for some FC crafts.
 
 
-### Features
+### Performance Improvements
 
-* **commissions:** you can now add items to the commission from its details page.
-* **db:** new toggle in fish hour graph to include fish eyes reports.
-* **db:** support for 5.4 data.
-* **fishing:** support for new fish eyes system.
-* **simulator:** support for 5.4 changes.`;
+* **web:** moved hosting back to firebase's scaled solution.
+`;
