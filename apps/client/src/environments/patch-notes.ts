3107a1cb7167bc02219481a5e9aeefc1f2ccb641
Supamiu <contact@flavien-normand.fr>
2020-12-17 21:09:07 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,9 +1,12 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** no more fishing spot link from spearfishing items.
-* **spearfishing:** fixed a lot of missing or wrong informations.
+* **db:** fixed wrong coordinates or spawn times for some nodes.
+* **inventory:** fixed CSV format sometimes not having retainer name.
+* **rotations:** fixed an issue with rotations drag and drop system.
 
 
 ### Features
 
-* **desktop:** support for CN 5.3 hotfix packet capture.`;
+* **alarms:** added hookset information to alarm, not retroactive.
+* **list:** added rotation result to list items with linked rotation.
+* **simulator:** you can now include wait time in consumables notification.`;
