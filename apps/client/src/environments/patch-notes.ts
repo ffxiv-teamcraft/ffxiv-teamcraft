e8e1dbb123ee1244fce36be458a74acf03acb135
Supamiu <contact@flavien-normand.fr>
2020-12-28 20:47:21 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,6 +1,7 @@
 export const patchNotes = `### Bug Fixes
 
 * **db:** fixed position for rarefied raw petalite.
+* **desktop:** fixed an issue with restart loop when you have a space in your windows profile name.
 * **layout:** consider levels 5 by 5 for panel filtering based on gathering level.
 * **total-trades:** fixed some currencies that could not be ignored despite having alternatives.
 
