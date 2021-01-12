b004520cefae70131a82270387d69ff131ad3cfc
Supamiu <contact@flavien-normand.fr>
2021-01-12 23:08:34 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,12 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** fixed mob page not loading properly.
-* **layout:** fixed can skip showing negative values in some cases.
-* **simulator:** fixed an issue with step by step mode and buffs duration.
-
-
-### Features
-
-* **data:** support for korean v5.3 update.
-* **db:** 5.41 data.
-* **simulator:** support for new conditions.`;
+* **log-tracker:** fixed log tracker not opening properly.`;
