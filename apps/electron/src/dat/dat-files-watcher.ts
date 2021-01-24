2cbeef99b19b8a289103f2f8dc0d4c8accb8c57b
Supamiu <contact@flavien-normand.fr>
2021-01-24 15:10:00 +0100

    fix(desktop): character detection should now be more reliable

--- apps/electron/src/dat/dat-files-watcher.ts
+++ apps/electron/src/dat/dat-files-watcher.ts
@@ -82,11 +82,11 @@ export class DatFilesWatcher {
       if (this.mainWindow.win) {
         if (filename.endsWith('ITEMODR.DAT')) {
           this.parseItemODR(join(watchDir, filename), contentId);
-        }
-        this.mainWindow.win.webContents.send('dat:content-id', contentId);
-        if (contentId !== this.lastContentId) {
-          log.log(`New content ID: ${contentId}`);
-          this.lastContentId = contentId;
+          this.mainWindow.win.webContents.send('dat:content-id', contentId);
+          if (contentId !== this.lastContentId) {
+            log.log(`New content ID: ${contentId}`);
+            this.lastContentId = contentId;
+          }
         }
       }
     }
