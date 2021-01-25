0c3d804ed92dd97bbeaa9a59134508181820c3ce
Supamiu <contact@flavien-normand.fr>
2021-01-25 19:18:43 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,12 +1,26 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed alarms not loading properly in some cases.
-* **desktop:** character detection should now be more reliable.
-* **leveling-equipment:** fixed ring being shown in offhand slot.
-* **list:** fixed layout button not using proper copy mode.
-* **retainer-ventures:** now sorted by total price.
+* **alarms:** fixed alarms not showing notification on web version.
+* **alarms:** fixed alarms regenerating forever in some cases.
+* **auth:** fixed google login not working properly in some cases.
+* **community-rotations:** fixed an issue with simulation result not showing.
+* **db:** fixed fish page data sometimes not loading properly.
+* **desktop:** fixed various issues with multi character inventory tracking.
+* **layout:** fixed buttons and support menu entries overlapping.
+* **layout:** fixed copy isearch menu entry not working as intended.
+* **leveling-equipment:** fixed wrong suggestions in some cases.
+* **leveling-equipment:** ignore items that are linked to a festival.
+* **list:** fixed path optimizer popups.
+* **nodes:** fixed some nodes showing 4h spawn duration instead of 3h.
+* **retainer-ventures:** fixed result ordering.
+* **retainers:** now showing remaining time instead of end time.
 
 
 ### Features
 
-* **desktop:** you can now change the characters folder to watch.`;
+* **db:** added a link from baits to the fish they can catch.
+* **desktop:** new button in settings to unlink your characters from ingame characters.
+* **desktop:** you can now delete one inventory again in inventory page.
+* **loading:** better loading screen for lazy data on web app.
+* **profile:** new entry in top-right profile menu to quickly select another character.
+* **settings:** new button to disconnect from patreon.`;
