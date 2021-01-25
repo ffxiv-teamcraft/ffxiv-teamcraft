eb3af9307ac1e6a20fefa8a93c65fe3ecd3c6d69
Supamiu <contact@flavien-normand.fr>
2021-01-25 16:03:32 +0100

    fix(auth): fixed google login not working properly in some cases

--- apps/electron/src/oauth/oauth.ts
+++ apps/electron/src/oauth/oauth.ts
@@ -40,7 +40,7 @@ export class Oauth {
         useContentSize: true
       });
 
-      authWindow.loadURL(url, { userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36' });
+      authWindow.loadURL(url, { userAgent: 'Chrome' });
       authWindow.show();
 
       authWindow.on('closed', () => {
