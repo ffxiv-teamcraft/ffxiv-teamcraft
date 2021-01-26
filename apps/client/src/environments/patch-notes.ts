dc04b8b89a00c694845679fc2dc98948c7346218
Supamiu <contact@flavien-normand.fr>
2021-01-26 18:52:04 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,11 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed a bug with alarms sometimes not playing the sound notification.
-* **alarms:** fixed an issue with "add alarms to a group" not working properly.
-* **currency-spending:** removed outdated red scrips.
-* **desktop:** better multi character detection combining packets and proper content checks.
-* **desktop:** fixed a bug with inventory resetting in some cases.
-* **list:** fixed requirements popup not showing on website.
-* **list:** fixed wrong tooltip on copy menu entry.
-* **list:** fixed zone breakdown sometimes not loading properly.
-* **retainer-ventures:** fixed retainer selector not showing properly.`;
+* **db:** fixed fishing page not loading properly.
+* **layout:** fixed wrong grouping logic with zone breakdown display.`;
