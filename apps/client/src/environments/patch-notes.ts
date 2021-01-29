1af9de0f360f0ffdc773ca8763e72e681cb71f56
Supamiu <contact@flavien-normand.fr>
2021-01-29 12:18:59 +0100

    fix(i18n): fixed missing translations in settings

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -12,4 +12,4 @@ export const patchNotes = `### Bug Fixes
 
 * **desktop:** keep linked character in cache if there's only one character on the user's account.
 * **layout:** new filters: IS_NORMAL_GATHERING and REQUIRES_WEATHER.
-* **settings:** new setting categories for inventory and characters tracking.`;
+* **settings:** new setting categories for inventory and characters tracking.`;
