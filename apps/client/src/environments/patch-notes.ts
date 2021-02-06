6379019a2909b08a43a1c1365809948e650b75e2
Supamiu <contact@flavien-normand.fr>
2021-02-06 10:17:16 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,18 +1,9 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fix for the errors happening even after the app has been closed.
-* **desktop:** fixed a possible crash in the alarms overlay.
-* **desktop:** fixed an error message when the app is started if inventory is empty.
-* **desktop:** fixed inventory reset issue with FC chest.
-* **desktop:** no more windows notification sound when Teamcraft has its own sound for it.
-* **gearset:** fixed wrong trade cost for some older gearset pieces.
-* **i18n:** fixed missing translation in settings popup.
-* **list-overlay:** fixed an issue with panels being collapsed by default not being shown properly.
-* **profile:** fixed missleading label on character deletion button tooltip.
-* **ux:** removed the loading screen when loading a database page for the first time.
+* **db:** fixed coords for some nodes.
+* **db:** fixed db button not aligned properly for some nodes.
 
 
 ### Features
 
-* **db:** support for 5.45 update.
-* airship-exploration-result reporter.`;
+* **settings:** new import/export feature in general settings.`;
