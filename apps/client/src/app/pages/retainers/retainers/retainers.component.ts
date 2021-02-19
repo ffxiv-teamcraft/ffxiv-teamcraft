aa9a0a43855e3db328f6ea66b0eca6add98cb2a6
Supamiu <contact@flavien-normand.fr>
2021-02-19 15:36:59 +0100

    feat(retainers): retainers are now grouped by character

--- apps/client/src/app/pages/retainers/retainers/retainers.component.ts
+++ apps/client/src/app/pages/retainers/retainers/retainers.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { interval, Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { groupBy } from 'lodash';
+import { combineLatest, interval, Observable } from 'rxjs';
+import { map, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { AuthFacade } from '../../../+state/auth.facade';
 import { Retainer, RetainersService } from '../../../core/electron/retainers.service';
 import { SettingsService } from '../../../modules/settings/settings.service';
 
@@ -13,27 +15,45 @@ import { SettingsService } from '../../../modules/settings/settings.service';
 })
 export class RetainersComponent {
 
-  retainers$: Observable<Retainer[]> = this.retainersService.retainers$.pipe(
-    switchMap(retainers => {
+  public loading = true;
+
+  display$: Observable<{ characterName: string, retainers: Retainer[] }[]> = combineLatest([this.retainersService.retainers$, this.auth.characterEntries$]).pipe(
+    map(([retainers, characters]) => {
+      const indexed = groupBy(retainers, 'character');
+      return Object.entries(indexed)
+        .map(([contentId, charRetainers]) => {
+          const characterEntry = characters.find(c => c.contentId === contentId);
+          return {
+            characterName: characterEntry ? characterEntry.character.Character.Name : 'Unknown',
+            retainers: charRetainers
+          };
+        });
+    }),
+    shareReplay(1),
+    switchMap(display => {
       return interval(1000).pipe(
         map(() => {
-          return Object.values<Retainer>(retainers)
-            .filter(retainer => !!retainer.name)
-            .sort((a, b) => a.order - b.order)
-            .map(retainer => {
-              return {
-                ...retainer,
-                taskDone: retainer.taskComplete <= Date.now() / 1000,
-                remainingTime: retainer.taskComplete - Math.floor(Date.now() / 1000)
-              };
+          return display
+            .map(row => {
+              row.retainers = row.retainers.filter(retainer => !!retainer.name)
+                .sort((a, b) => a.order - b.order)
+                .map(retainer => {
+                  return {
+                    ...retainer,
+                    taskDone: retainer.taskComplete <= Date.now() / 1000,
+                    remainingTime: retainer.taskComplete - Math.floor(Date.now() / 1000)
+                  };
+                });
+              return row;
             });
         })
       );
-    })
+    }),
+    tap(() => this.loading = false)
   );
 
   constructor(private retainersService: RetainersService, public translate: TranslateService,
-              public settings: SettingsService) {
+              public settings: SettingsService, private auth: AuthFacade) {
   }
 
 }
