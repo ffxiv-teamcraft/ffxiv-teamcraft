d380807dbb5705518e52bfc80001a75bb0f82144
Supamiu <contact@flavien-normand.fr>
2021-03-07 12:54:41 +0100

    feat(gearset): added melding chances for materia slots

--- apps/client/src/app/modules/gearsets/materia-slot-icon/materia-slot-icon.component.ts
+++ apps/client/src/app/modules/gearsets/materia-slot-icon/materia-slot-icon.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { EquipmentPiece } from '../../../model/gearset/equipment-piece';
 import { MateriaService } from '../materia.service';
 
@@ -8,13 +8,15 @@ import { MateriaService } from '../materia.service';
   styleUrls: ['./materia-slot-icon.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MateriaSlotIconComponent {
+export class MateriaSlotIconComponent implements OnInit {
 
   @Input()
   equipmentPiece: EquipmentPiece;
 
   @Input()
   index: number;
+
+  meldingChances = 0;
 
   constructor(private materiaService: MateriaService) {
   }
@@ -30,6 +32,10 @@ export class MateriaSlotIconComponent {
     const materia = this.materiaService.getMateria(this.equipmentPiece.materias[this.index]);
     const grade = materia.tier - 1;
     return `${grade < 10 ? '0' : ''}${grade}`;
+  }
+
+  ngOnInit(): void {
+    this.meldingChances = this.materiaService.getMeldingChances(this.equipmentPiece, this.equipmentPiece.materias[this.index], this.index);
   }
 
 }
