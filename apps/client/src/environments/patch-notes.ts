67a20d9a3d11edaf49b9bca245953e7b72fecc70
Supamiu <contact@flavien-normand.fr>
2021-04-08 20:40:10 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,16 +1,7 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed search sharing button not giving the right url.
-* **leveling-equipment:** masterbook items are no longer being considered.
-* **recipe-finder:** fixed an issue with some recipes not being found properly.
-* **requirements:** fixed inventory position tag not showing properly.
-* **simulator:** fixed observe combo skipping with standard touch.
-* **voyage-tracker:** fixed a issue with voyage tracker not loading in some cases.
-
-
-### Features
-
-* **data:** update for cn patch-5.4.
-* **desktop:** complete refactor of inventory tracking for better reliability.
-* **rotation:** added a button to generate macro in the rotation panel.
-* **simulator:** support for CN 5.4.`;
+* **desktop:** fixed an error popup that could happen during some fights.
+* **desktop:** fixed an error with new contentId inventory tracking.
+* **desktop:** fixed character detection.
+* **desktop:** fixed inventory tracking issues with skipped containers and missing items.
+* **simulator:** fixed standard touch combo.`;
