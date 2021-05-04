811666385280b008f900eb1440f6dbb0e7f14906
Supamiu <contact@flavien-normand.fr>
2021-05-04 20:27:02 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,15 +1,21 @@
 export const patchNotes = `### Bug Fixes
 
-* **fishing-overlay:** fixed chum status tracking.
-* **fishing-spot:** uniform sorting of tug type data table.
-* **inventory:** remove remnants of unknown inventories when character is set.
-* **log-tracker:** fixed position maps not showing properly inside "see on map" popup.
-* **simulator:** failed combo actions are no longer being considered as combo enablers.
-* **simulator:** fixed specialist stats toggle sometimes not updating stats.
-* **voyage-tracker:** fix import.
+* **alarms:** fixed fishing alarms inaccuracies (requires alarm regeneration).
+* **crafting-replays:** fixed an issue with crafting replay folder not loading.
+* **db:** lots of new fishing data and various recommended bait fixes.
+* **desktop:** more restrictive file change detection to avoid false positives.
+* **fishing-reporter:** better fix for chum detection.
+* **log-tracker:** fixed big log import from carbuncleplushy not working as intended.
+* **log-tracker:** fixed optimized path popup using missing coordinates.
+* **treasure-finder:** fixed wrong miniature position for HW maps.
 
 
 ### Features
 
-* **inventory:** character inventories sharing the same name now include server name.
-* **log-tracker:** you can now import FSH progress from CarbunclePlushy.`;
+* **crafting-replays:** new button to delete all your offline replays at once.
+* **currency-spending:** items are now sorted by rentability (price per currency spent).
+* **db:** changed bite times graph on fishing spot page for box plot.
+* **fishing-log-tracker:** you can now export completion as JSON in your clipboard.
+* **inventory:** items are now sorted by price DESC when they have been filled.
+* **recipe-finder:** new leve icon next to the recipes used for a levequest.
+* **settings:** new Notifications settings section to configure each notification separately.`;
