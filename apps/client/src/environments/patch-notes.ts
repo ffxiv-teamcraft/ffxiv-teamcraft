7deffd716b98ff69a21b6da65878c6645f291634
Supamiu <contact@flavien-normand.fr>
2021-05-18 23:41:08 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,21 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** complete revamp of the "create alarm in group" system.
-* **alarms:** fixed fishing alarms not being accurate with weather.
-* **fishing-reporter:** fixed random items being detected as bait.
-* **fishing-reporter-overlay:** swapped current and previous weather.
-* **list:** fixed an issue with some items not being added properly to a list.
-* **log-tracker:** fixed an issue with hide completed tabs not selecting another tab properly.
-* **simulator:** fixed a bug with duration per macro not refreshing properly.
-
-
-### Features
-
-* **alarms:** folders names are now shown below alarms in folder addition popup.
-* **db:** support for korean v5.4 update.
-* **simulator:** support for korean 5.4.
-
-
-### Performance Improvements
-
-* **list:** shorter loading time and overall list performance.`;
+* **core:** fixed several pages not loading properly.`;
