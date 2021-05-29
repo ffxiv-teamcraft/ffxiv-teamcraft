24f1eb6e7f9e17f28029ff790f6dee329012c046
Supamiu <contact@flavien-normand.fr>
2021-05-29 12:42:17 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,5 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed machina not starting if powershell times out for some reason.
-* **lists:** fixed job priority system not working as intended on some lists.
-* **pricing-mode:** fixed label overlap on some resolutions.`;
+* **desktop:** fixed an issue with npcap detection poping a Javascript error on startup.`;
