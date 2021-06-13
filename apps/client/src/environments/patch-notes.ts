a3a484836c62c853d2e4d0343e251773d4894f31
Supamiu <contact@flavien-normand.fr>
2021-06-13 15:57:23 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,17 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed an issue with npcap detection poping a Javascript error on startup.`;
+* **alarms:** fixed custom alarm creation with spawn at 0:00.
+* **currency-spending-guide:** better npc indications to trade the items from.
+* **db:** fixed broken links for some instances and specific content.
+* **desktop:** fixed wrong reports for marketboard history.
+* **desktop:** possible fix for freeze on startup that requires window manipulation to unfreeze.
+* **simulator:** fixed stats edition sometimes toggling specialist bonus while it shouldn't.
+* **ux:** proper implementation for prefers-reduced-motion browser setting.
+
+
+### Features
+
+* **data:** update for cn patch-5.45.
+* **db:** added ET hour next to weather transition spawns.
+* **desktop:** stats are now updated as you switch jobs ingame, can be disabled in settings.
+* **patreon:** new supporter: Syl Varil.`;
