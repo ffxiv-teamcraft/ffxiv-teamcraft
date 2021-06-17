b9a18920eeb5db26784ea4aa96a4c199acd2a570
Supamiu <contact@flavien-normand.fr>
2021-06-17 14:53:05 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,17 +1,12 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed custom alarm creation with spawn at 0:00.
-* **currency-spending-guide:** better npc indications to trade the items from.
-* **db:** fixed broken links for some instances and specific content.
-* **desktop:** fixed wrong reports for marketboard history.
-* **desktop:** possible fix for freeze on startup that requires window manipulation to unfreeze.
-* **simulator:** fixed stats edition sometimes toggling specialist bonus while it shouldn't.
-* **ux:** proper implementation for prefers-reduced-motion browser setting.
+* **currency-spending:** fixed faux leves and Bozjan clusters not working as intended.
+* **db:** fixed db pages for set items not loading properly.
+* **leveling-equipment:** fixed wrong suggestion for higher level crafters/gatherers.
+* **simulator:** fixed stats not being persisted properly to the form.
+* **voyage-tracker:** fixed missing translations.
 
 
 ### Features
 
-* **data:** update for cn patch-5.45.
-* **db:** added ET hour next to weather transition spawns.
-* **desktop:** stats are now updated as you switch jobs ingame, can be disabled in settings.
-* **patreon:** new supporter: Syl Varil.`;
+* **db:** support for korean v5.41 update.`;
