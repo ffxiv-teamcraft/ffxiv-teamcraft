a6852b756a8865179be5b246ced6cf9cb64bf763
Supamiu <contact@flavien-normand.fr>
2021-06-23 19:38:39 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,19 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **profile:** better tracking system for stats updates.
-* **recipe-finder:** removed db button to avoid missclick.
-
-
-### Features
-
-* **community-lists:** lists are now sorted by # of copies made.
-* **community-rotations:** automated sorting to find best rotation.
-* **crafting-replays:** you can now generate a crafting macro from a replay.
-* **inventory-optimizer:** new optimizer for items only used for one material recipe.
-* **leveling-equipment:** search params are now remembered when going back in history.
-* **recipe-finder:** you can now filter for items that are used in leves.
-
-
-### Performance Improvements
-
-* **lists:** lowering RAM usage over time by removing some runtime cache.`;
+* **auth:** fixed a bug preventing anonymous users from creating lists or using the simulator.
+* **desktop:** load opcodes from github if not loading from CN region.`;
