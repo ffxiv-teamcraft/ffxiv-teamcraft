7370690662ba93315f85c60c842f0555851b215a
Flavien Normand <contact@flavien-normand.fr>
2021-07-04 12:50:00 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,24 +1,5 @@
 export const patchNotes = `### Bug Fixes
 
-* **community-rotations:** better sorting for rotations.
-* **db:** fixed tomestone pages being very slow to load.
-* **gearset-editor:** fixed job swap not working as expected.
-* **gearsets:** fixed materias not being saved properly when switching pieces.
-* **layouts:** removed export button for default layout.
-* **log-tracker:** fixed an issue with log page sometimes becoming empty.
-* **log-tracker:** fixed an issue with page fill button not working as expected.
-* **settings:** proper category for "show all alarms" setting.
-
-
-### Features
-
-* **inventory:** sort items by price after they have been filled from marketboard.
-* **layout:** new TIMER order function to order by closest timer.
-* **leveling-equipment:** you can now click the list notification to be redirected to it.
-* **list-details:** hunting details popup now shows all known locations for monsters.
-* **simulator:** new community rotation finder button.
-
-
-### Performance Improvements
-
-* **gathering-location:** auto switch to compact mode if there's too many results.`;
+* **currency-spending:** better loading experience.
+* **list:** fixed panel ordering jumping from a state to another whenever the list changes.
+* **search:** you can now search for items wioth shorter names (Rue for instance).`;
