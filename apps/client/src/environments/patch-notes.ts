921ea7dcb9244903a337b6f0caf290aa42156e30
Flavien Normand <contact@flavien-normand.fr>
2021-07-05 16:48:26 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,5 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **currency-spending:** better loading experience.
-* **list:** fixed panel ordering jumping from a state to another whenever the list changes.
-* **search:** you can now search for items wioth shorter names (Rue for instance).`;
+* **currency-spending:** removed unusable currencies.`;
