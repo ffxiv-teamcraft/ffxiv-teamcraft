f977f7ecbdc6351a74ce2863f400297c2394c0f4
Flavien Normand <contact@flavien-normand.fr>
2021-07-11 14:03:44 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,10 @@
 export const patchNotes = `### Bug Fixes
 
-* **currency-spending:** removed unusable currencies.`;
+* **currency-spending:** possible fix for results always reloading.
+
+
+### Performance Improvements
+
+* **list:** better layout cache system for faster list details loading.
+* **list:** modifications history now only retains the last 25 changes to improve loading time.
+* **list:** new progress write system for multiplayer autofill, a bit slower but way more reliable.`;
