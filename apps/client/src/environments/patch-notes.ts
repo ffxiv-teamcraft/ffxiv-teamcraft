b663fd8c02904f0df78f18a5e786190d5f7538cd
Flavien Normand <contact@flavien-normand.fr>
2021-07-12 22:12:02 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,5 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **list:** fixed fill from inventory not filling crafts properly.
-* **list:** fixed list items not being added properly.
-* **list:** fixed list panel reset not working as expected.`;
+* **list:** fixed items tracking going into chaos mode.`;
