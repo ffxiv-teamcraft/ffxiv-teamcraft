803d82f2b544236d1f9467f88a8e6c3f18ce5cb2
Flavien Normand <contact@flavien-normand.fr>
2021-07-15 21:37:12 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,5 @@
 export const patchNotes = `### Bug Fixes
 
-* **list:** fixed items tracking going into chaos mode.`;
+* **list:** fixed autofill sometimes stopping its action entirely.
+* **list:** fixed possible rollbacks when autofilling a list.
+* **list:** no more transactional updates for lists over 50 final items.`;
