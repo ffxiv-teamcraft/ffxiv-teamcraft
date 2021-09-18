e775d4091ea27ef6e6c09836b99a6b7ff76a05e2
Flavien Normand <contact@flavien-normand.fr>
2021-09-18 20:11:09 +0200

    chore: add new service to proxy environment using region

---
+++ apps/client/src/app/core/environment.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { SettingsService } from '../modules/settings/settings.service';
+import { Region } from '../modules/settings/region.enum';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvironmentService {
+
+  public get gameVersion(): number {
+    switch (this.settings.region) {
+      case Region.China:
+        return environment.chineseGameVersion;
+      case Region.Korea:
+        return environment.koreanGameVersion;
+      case Region.Global:
+        return environment.globalGameVersion;
+    }
+  }
+
+  public get maxLevel(): number {
+    if (this.gameVersion >= 6) {
+      return 90;
+    }
+    return 80;
+  }
+
+  constructor(private settings: SettingsService) {
+  }
+}
