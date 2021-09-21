60d897586337ba493b349947a6e76f73b1488ca5
Flavien Normand <contact@flavien-normand.fr>
2021-09-21 15:41:06 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,4 +1,5 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** better fix for weather transition detection.
-* **profile:** hotfix for profile and marketboard popup not loading at all.`;
+* **alarms:** fixed alarms with same weather transition crashing the system.
+* **currency-spending:** now using server prices only for price checking.
+* **leveling-equipment:** cannot wear the same unique ring twice anymore.`;
