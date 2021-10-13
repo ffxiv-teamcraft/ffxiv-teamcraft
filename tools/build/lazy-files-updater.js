8fb13c1b1abf7e7fb09ec6170f8fe9bd83963ba9
Flavien Normand <contact@flavien-normand.fr>
2021-10-13 07:08:43 +0200

    chore: first part of automated types generation

--- tools/build/lazy-files-updater.js
+++ tools/build/lazy-files-updater.js
@@ -3,6 +3,13 @@ const colors = require('colors/safe');
 const fs = require('fs');
 const _ = require('lodash');
 const hashFiles = require('hash-files');
+const {
+  quicktype,
+  InputData,
+  jsonInputForTargetLanguage,
+  JSONSchemaInput,
+  FetchingJSONSchemaStore
+} = require('quicktype-core');
 
 console.log(colors.cyan(`Updating lazy loaded files list`));
 
@@ -26,16 +33,42 @@ fs.writeFileSync(
           ...acc,
           [propertyName]: {
             fileName: row,
-            hashedFileName: `${row.replace('.json', '')}.${hash}.json`,
-          },
+            hashedFileName: `${row.replace('.json', '')}.${hash}.json`
+          }
         };
       }, {}),
     null,
     2
-  )};`.replace(/"/g, "'")
+  )};`.replace(/"/g, '\'')
 );
 
 console.log(colors.green('Lazy loaded files list updated'));
+
+console.log(colors.blue(`Updating lazy loaded data Models`));
+(async () => {
+  const jsonString = fs.readFileSync(path.join(__dirname, '../../apps/client/src/assets/data/', 'items.json'));
+  const jsonInput = jsonInputForTargetLanguage('typescript');
+  await jsonInput.addSource({
+    name: 'Items',
+    samples: [jsonString]
+  });
+
+  const inputData = new InputData();
+  inputData.addInput(jsonInput);
+
+  const items = await quicktype({
+    inputData,
+    lang: 'typescript',
+    combineClasses: true,
+    rendererOptions: {
+      'just-types': true
+    }
+  });
+
+  console.log(items);
+})();
+console.log(colors.blue(`Lazy loaded data Models updated`));
+
 
 console.log(colors.cyan(`Updating lazy loaded data interface`));
 
@@ -50,7 +83,7 @@ export interface LazyData {
     })
     .map(getPropertyName)
     .join(': any;\n  ')}: any;
-}`.replace(/"/g, "'")
+}`.replace(/"/g, '\'')
 );
 
 const extractsHash = hashFiles.sync({ files: [path.join(__dirname, '../../apps/client/src/assets/extracts/extracts.json')] });
