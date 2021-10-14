92f1c9eca21794cd8633da9689fda852535a51d9
Flavien Normand <contact@flavien-normand.fr>
2021-10-14 13:29:20 +0200

    fix(db): fixed spearfishing spot pages not loading

--- apps/client/src/app/pages/db/spearfishing-spot/spearfishing-spot.component.ts
+++ apps/client/src/app/pages/db/spearfishing-spot/spearfishing-spot.component.ts
@@ -167,7 +167,7 @@ export class SpearfishingSpotComponent extends TeamcraftPageComponent {
   }
 
   public getGatheringItem(itemId: number): any {
-    return this.lazyData.data.nodes
+    return Object.values(this.lazyData.data.nodes)
       .find(node => node.items.includes(itemId));
   }
 
