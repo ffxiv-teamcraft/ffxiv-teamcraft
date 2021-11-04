463092bbcc3e42a812117a51e0e6d96b0dcce199
Flavien Normand <contact@flavien-normand.fr>
2021-11-04 18:14:18 +0100

    chore(seo): better prerenderer

--- ssr/puppeteer/prerender.mjs
+++ ssr/puppeteer/prerender.mjs
@@ -3,9 +3,11 @@ import items from '../../apps/client/src/assets/data/items.json';
 import puppeteer from 'puppeteer';
 import _ from 'lodash';
 import colors from 'colors/safe.js';
+import cliProgress from 'cli-progress';
 
 const { chunk } = _;
 const { green } = colors;
+const { SingleBar } = cliProgress;
 
 const CHUNK_SIZE = 10;
 
@@ -16,16 +18,23 @@ const CHUNK_SIZE = 10;
   const browserWSEndpoint = await browser.wsEndpoint();
 
   const itemIds = Object.keys(items).slice(1);
+  const progress = new SingleBar({
+    format: '[{bar}] {percentage}% | ETA: {eta}s | {value}/{total}',
+    hideCursor: true,
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591'
+  });
   const languages = Object.keys(items[itemIds[0]]);
   const chunks = chunk(itemIds, CHUNK_SIZE);
-  console.log(green(`Starting prerendering of ${itemIds.length * languages.length} item db pages in ${Math.ceil(itemIds.length / CHUNK_SIZE)} batches of ${CHUNK_SIZE * languages.length} pages`));
-  console.log(`This can take up to ${Math.ceil(chunks.length * 30 / 60 / 60)} hours.\n`);
+  progress.start(itemIds.length * languages.length, 0);
   for (const chunk of chunks) {
     await Promise.all(chunk.map(id => {
-      console.log(`Item#${id}`);
       return Promise.all(languages.map(lang => {
         const path = `/db/${lang}/item/${id}/${items[id][lang].split(' ').join('-')}`;
-        return ssr(path, browserWSEndpoint).catch(() => {
+        return ssr(path, browserWSEndpoint).then(() => {
+          progress.increment();
+        }).catch(() => {
+          progress.increment();
           return void 0;
         });
       }));
