63a9b935b2a5dcb79dc38776a306e7d8012daede
Flavien Normand <contact@flavien-normand.fr>
2021-11-15 13:36:12 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,12 +1,30 @@
 export const patchNotes = `### Bug Fixes
 
-* **airship:** updated abbreviations to match community ones.
-* **alarms:** fixed a bug with some alarms ringing multiple times for nothing.
-* **db:** fixed npc names not loading properly in levequest pages.
-* **db:** fixed spearfishing spot pages not loading.
-* **log-tracker:** fixed selection not resetting on tab change.
+* **alarms:** fixed alarms triggered by a weather window starting at midnight.
+* **currency-spending:** now counts history beyond 5 sales.
+* **db:** fixed duplicates in fishing log tracker.
+* **db:** fixed fishing pages sometimes not loading properly.
+* **db:** fixed inconsistent spot bite times graph error.
+* **i18n:** removed spaces before exclamation mark in english.
+* **reduction:** fixed duplicate reductions from extractor.
+* **treasures:** better coords system for the treasure previews.
 
 
 ### Features
 
-* **db:** importing 500+.`;
+* **autofill:** new crafting/gathering detection system for less DB stress.
+* **data:** update for cn patch-5.57.
+* **db:** fish baits are now sorted by # of catch.
+* **db:** import of 500+ allagan reports.
+* **db:** leves data in item page now show amount required and job icon.
+* **db:** support for korean v5.55 update.
+* **inventory-optimizer:** new button to export optimization panel as JSON.
+* **inventory-optimizer:** new toggle to hide empty entries.
+* **inventory-synthesis:** better ordering for items, based on requirements.
+* **layout:** new filter: IS_FINAL_ITEM.
+* **lists:** added a wiki link to the verification warning banner.
+* **navigation:** you can now copy step label as string in your clipboard by clicking on it.
+* **pricing:** now filling HQ price for vendor items too.
+* **retainers:** market data is now collapsible.
+* **settings:** added Ishgard as possible starting point.
+* **settings:** new "maximum total vendor price" setting to set max allowed total price for a row.`;
