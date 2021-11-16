2655288e111998f5808ef97b2bca712c7accc2d9
Flavien Normand <contact@flavien-normand.fr>
2021-11-16 23:38:46 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,30 +1,13 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed alarms triggered by a weather window starting at midnight.
-* **currency-spending:** now counts history beyond 5 sales.
-* **db:** fixed duplicates in fishing log tracker.
-* **db:** fixed fishing pages sometimes not loading properly.
-* **db:** fixed inconsistent spot bite times graph error.
-* **i18n:** removed spaces before exclamation mark in english.
-* **reduction:** fixed duplicate reductions from extractor.
-* **treasures:** better coords system for the treasure previews.
-
-
-### Features
-
-* **autofill:** new crafting/gathering detection system for less DB stress.
-* **data:** update for cn patch-5.57.
-* **db:** fish baits are now sorted by # of catch.
-* **db:** import of 500+ allagan reports.
-* **db:** leves data in item page now show amount required and job icon.
-* **db:** support for korean v5.55 update.
-* **inventory-optimizer:** new button to export optimization panel as JSON.
-* **inventory-optimizer:** new toggle to hide empty entries.
-* **inventory-synthesis:** better ordering for items, based on requirements.
-* **layout:** new filter: IS_FINAL_ITEM.
-* **lists:** added a wiki link to the verification warning banner.
-* **navigation:** you can now copy step label as string in your clipboard by clicking on it.
-* **pricing:** now filling HQ price for vendor items too.
-* **retainers:** market data is now collapsible.
-* **settings:** added Ishgard as possible starting point.
-* **settings:** new "maximum total vendor price" setting to set max allowed total price for a row.`;
+* **allagan-reports:** fixed allagan reports count not loading for data checkers.
+* **db:** fixed missing meta name for fishing spots.
+* **inventory:** better retainers tracking.
+* **inventory-optimizer:** fixed item icons and names acting up when scrolling.
+* **layout:** fixed precrafts not being rendered properly in default list layout.
+* **levequests:** fixed levequests page not loading.
+* **list:** fixed an issue with FC crafts not being added to lists properly.
+* **recipe-finder:** fixed item input not working anymore.
+* **ssr:** fixed preview embeds not working properly.
+* **tooltips:** fixed item tooltips not showing stats properly.
+* **voyage-tracker:** fixed airship sectors total count not displayed.`;
