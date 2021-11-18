878542693cf79f42d15a2760e2f817e963772c02
Flavien Normand <contact@flavien-normand.fr>
2021-11-18 19:53:59 +0100

    fix(log-tracker): Ornaments are now accounted for achievements

--- apps/client/src/app/pages/extractor/extractor/extractor.component.ts
+++ apps/client/src/app/pages/extractor/extractor/extractor.component.ts
@@ -79,7 +79,7 @@ export class ExtractorComponent {
               return notebookDivision[key].pages.includes(tab.id);
             });
             const division = notebookDivision[(tab as any).divisionId];
-            (tab as any).requiredForAchievement = /\d{1,2}-\d{1,2}/.test(division.name.en) || division.name.en.startsWith('Housing');
+            (tab as any).requiredForAchievement = /\d{1,2}-\d{1,2}/.test(division.name.en) || division.name.en.startsWith('Housing') || division.name.en.startsWith('Ornaments');
             tab.recipes = tab.recipes.map(entry => {
               (entry as any).leves = Object.entries<any>(leves)
                 .filter(([, leve]) => {
