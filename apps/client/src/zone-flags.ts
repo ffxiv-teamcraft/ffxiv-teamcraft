cc44807b212b761ab67a1ba8e7d572b487101da0
Flavien Normand <contact@flavien-normand.fr>
2021-11-19 11:10:52 +0100

    perf(core): global runtime performance improvements

---
+++ apps/client/src/zone-flags.ts
@@ -0,0 +1,7 @@
+(window as any).__Zone_disable_requestAnimationFrame = true;
+(window as any).__Zone_disable_toString = true;
+(window as any).__Zone_disable_on_property = true;
+(window as any).__Zone_disable_requestAnimationFrame = true;
+(window as any).__Zone_disable_timers = true;
+(window as any).__Zone_disable_canvas = true;
+(window as any).__Zone_disable_XHR = true;
