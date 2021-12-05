7a3898f34127bfc753548f423f63edb16498e52a
Flavien Normand <contact@flavien-normand.fr>
2021-12-05 15:42:19 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,7 +1,6 @@
 export const patchNotes = `### Bug Fixes
 
-* **settings:** fixed custom sound selection crashing the app.
-* **simulator:** fixed delicate synthesis not affected by inner quiet.
-* **simulator:** fixed HQ ingredients quality contribution calc.
-* **simulator:** fixed preparatory touch managing to reach 11 stacks of IQ.
-* **weather:** fixed weather rates missing for new maps, causing weather prediction issues.`;
+* **db:** fixed wrong scrips being referenced in trades and collectables.
+* **layout:** fixed ability to craft not being computed properly.
+* **lists:** fixed recipe ingredients not being added properly.
+* **simulator:** fixed progression being doubled and other small flooring inaccuracies.`;
