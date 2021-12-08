78a1713cf600ab12e3d53b62a218531f3612e8c2
M Arbo <marbo.phd@gmail.com>
2021-12-08 10:56:45 -0600

    fix(log-tracker): housing item categories count for achievement

--- apps/client/src/app/pages/extractor/extractor/extractor.component.ts
+++ apps/client/src/app/pages/extractor/extractor/extractor.component.ts
@@ -79,7 +79,9 @@ export class ExtractorComponent {
               return notebookDivision[key].pages.includes(tab.id);
             });
             const division = notebookDivision[(tab as any).divisionId];
-            (tab as any).requiredForAchievement = /\d{1,2}-\d{1,2}/.test(division.name.en) || division.name.en.startsWith('Housing') || division.name.en.startsWith('Ornaments');
+            (tab as any).requiredForAchievement = /\d{1,2}-\d{1,2}/.test(division.name.en) || division.name.en.startsWith('Fixtures') ||
+              division.name.en.indexOf('Furnishings') > -1 || division.name.en.startsWith('Table') ||
+              division.name.en.startsWith('Wall-mounted') || division.name.en.startsWith('Ornaments');
             tab.recipes = tab.recipes.map(entry => {
               (entry as any).leves = Object.entries<any>(leves)
                 .filter(([, leve]) => {
