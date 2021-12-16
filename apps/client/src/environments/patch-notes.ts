dc62cbaac8750ef976fad971f97a6afdd3f36fa4
Supamiu <contact@flavien-normand.fr>
2021-12-16 16:34:07 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,21 +1,17 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** better display for npc pages.
-* **gearsets:** fixed import from ariyala sometimes breaking.
-* **list:** possible fix for item removal.
-* **lists:** automatic HQ flags won't appear on items that cannot be HQ anymore.
-* **lists:** fixed lists not assignable to a team.
-* **lists:** fixed vendors popup not closing properly when clicking on db link.
-* **retainer-ventures:** removed venture cost from the display table.
+* **collectables:** fixed level filters not being applied properly.
+* **db:** fixed vendors panel not showing at all.
+* **layout:** fixed maximum total vendor price not being applied properly.
+* **teleport:** adjustments in TP cost formula.
+* **treasures:** added missing 6.0 maps to treasures finder.
 
 
 ### Features
 
-* **alarms:** added copy to clipboard on item name click event in sidebar.
-* **db:** new data imports and update from allagan reports.
-* **metrics:** added localized numbers formatting for amounts.
+* **desktop:** new update popup prompt inside the app instead of focus-hijacking popup.
 
 
 ### Performance Improvements
 
-* **community-lists:** lists won't be searched without enough details anymore.`;
+* **community-lists:** brand new search engine using algolia.`;
