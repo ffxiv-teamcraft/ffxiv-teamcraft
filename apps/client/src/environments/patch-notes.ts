4846a1f1b0bdac94401bb1b69fe9ff5cd78b166b
Supamiu <contact@flavien-normand.fr>
2021-12-20 16:53:16 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,7 +1,20 @@
 export const patchNotes = `### Bug Fixes
 
-* **ariyala:** fixed ariyala import for both gearsets and lists.
-* **collectables:** fixed collectables not being filtered by level properly.
-* **list:** fixed recipes addition sometimes not applying recipe id properly.
-* **lists:** fixed a bug with list copy sometimes not copying properly.
-* **ventures:** fixed venture thresholds not being accurate.`;
+* **db:** fixed spearfishing node positions missing icons.
+* **gc-supply:** fixed job icons not loading properly.
+* **gearsets:** fixed missing job name in gearsets sync popup.
+* **layout:** better filtering for housing vendors.
+* **log-tracker:** added missing spearfishing nodes.
+* **log-tracker:** fixed log pages ordering in FSH tabs.
+* **reset-timers:** fixed some timers displaying weird 0-X counts.
+* **search:** fixed checkboxes breaking the selection page.
+* **simulator:** fixed custom simulator.
+* **user-picker:** proper loader display when request is running.
+
+
+### Features
+
+* **collectables:** categories are now sorted by level DESC.
+* **community-lists:** added a search button to not trigger useless and broad search requests.
+* **db:** allagn reports import.
+* **layout:** town bicolor gemstone traders are now ignored in area breakdown.`;
