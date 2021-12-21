348d40f08a25785f08278fbc91670f38e2ac21d0
Supamiu <contact@flavien-normand.fr>
2021-12-21 13:12:46 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,20 +1,3 @@
-export const patchNotes = `### Bug Fixes
+export const patchNotes = `### Features
 
-* **db:** fixed spearfishing node positions missing icons.
-* **gc-supply:** fixed job icons not loading properly.
-* **gearsets:** fixed missing job name in gearsets sync popup.
-* **layout:** better filtering for housing vendors.
-* **log-tracker:** added missing spearfishing nodes.
-* **log-tracker:** fixed log pages ordering in FSH tabs.
-* **reset-timers:** fixed some timers displaying weird 0-X counts.
-* **search:** fixed checkboxes breaking the selection page.
-* **simulator:** fixed custom simulator.
-* **user-picker:** proper loader display when request is running.
-
-
-### Features
-
-* **collectables:** categories are now sorted by level DESC.
-* **community-lists:** added a search button to not trigger useless and broad search requests.
-* **db:** allagn reports import.
-* **layout:** town bicolor gemstone traders are now ignored in area breakdown.`;
+* **db:** support for 6.01 content update.`;
