b0d14725e5689433baed4bf458ab42b706d8dc62
Supamiu <contact@flavien-normand.fr>
2021-12-28 11:41:41 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,13 @@
 export const patchNotes = `### Bug Fixes
 
-* **gathering-locations:** fixed search by string sometimes breaking the results display.`;
+* **db:** fixed quicksearch sometimes showing weird undefined results.
+* **gearset:** fixed confidence rate resetting when including other tools in DoH sets.
+* **gearsets:** fixed total materias and total trades popups in gearset editor.
+* **list:** fixed copy to clipboard not copying all the tiers.
+* **retainers:** fixed "missing name" label for when there's no task assigned to a retainer.
+
+
+### Features
+
+* **db:** import of over 200 allagan reports from a single user (big thanks to Rivahn Fireshadow).
+* **layouts:** new layout in the default pool: crafts and timed first.`;
