ad2978943f046a05284951301eb8c8d8bd1979da
Flavien Normand <contact@flavien-normand.fr>
2022-01-10 18:39:30 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,14 +1,6 @@
 export const patchNotes = `### Bug Fixes
 
-* **gearset:** fixed stats formulas.
-* **gearsets:** added wrapping to the import buttons.
-* **lists:** fixed list completion sometimes not applied properly.
-
-
-### Features
-
-* **gearsets:** tribe is now saved across gearsets.
-* **pricing:** brought back "fill with only server" button.
-* **pricing:** new button in spendings card header to fill all the prices from MB at once.
-* **pricing:** new toggle to fill precraft prices from materials or manually.
-* **pricing:** panels are now collapsed or expanded based on layout settings.`;
+* **levequests:** fixed exp formula for leves < 70 on level > 70.
+* **pricing:** fixed panels collapsing themselves randomly on desktop app.
+* **profits:** fixed level filters sometimes not applied properly.
+* **profits:** fixed page loading forever with empty profile.`;
