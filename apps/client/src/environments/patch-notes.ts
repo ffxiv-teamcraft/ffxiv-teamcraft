5e1c41be0b0a960995301fda81f14aeca4786d07
Flavien Normand <contact@flavien-normand.fr>
2022-01-10 09:46:17 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,11 +1,30 @@
 export const patchNotes = `### Bug Fixes
 
-* **allagan-reports:** fixed spawn data not showing on spearfishing.
-* **currency-spending:** fixed npc name sometimes missing.
-* **db:** fixed some old links to faux leaves.
-* **db:** fixed wolf mark trades not using right currency.
-* **i18n:** fixed some japanese action names not being parsed properly.
-* **layout:** grid optimizations for better list display.
-* **log-tracker:** remove useless ids shown in parenthesis.
-* **profile:** added new masterbooks to masterbooks popup.
-* **recipe-finder:** fixed finder sometimes running forever.`;
+* **community-rotations:** added 55 as possible durability value.
+* **core:** fixed Endwalker expansion missing in some filters.
+* **currency-spending:** fixed duplicate items in results.
+* **currency-spending:** fixed page sometimes not loading data properly.
+* **db:** added *2 distance weight to garlemald train station aetheryte.
+* **favorites:** fixed favorite rotation folders sometimes not expanding properly.
+* **gearsets:** updated base values for combat gearsets, thanks Fürst for the help.
+* **layout:** fixed an issue with levels filters sometimes acting up on gathering.
+* **layout:** fixed ONLY_FROM_VENDOR layout filter.
+* **leveling-equipment:** removed Waist gear row.
+* **retainers:** fixed retainers select box not available (needs retainers reset).
+* **rotation-finder:** now applying consumables from rotation properly.
+* **search:** remove trailing and leading spaces to avoid search issues.
+* **simulator:** fixed specialist flag not applied properly from shared link with stats.
+* **simulator:** remove final appraisal is now only enabled when FA buff is up.
+* **ux:** added different color on timer for when node is spawned.
+* **ux:** possible fir for middle click on item icons triggering scroll behavior.
+
+
+### Features
+
+* **core:** new profits helper page under currencies to find best items to craft or gather.
+* **gearsets:** you can now import a gearset from etro.
+* **list:** support for crystals tracking.
+* **lists:** you can now import a list from an Etro gearset.
+* **pricing-mode:** complete refactor of the pricing mode, faster, more reliable.
+* **simulator:** new "show more" button in rotation finder and stats toggle.
+* **simulator:** new toggle to show full buttons instead of small icons.`;
