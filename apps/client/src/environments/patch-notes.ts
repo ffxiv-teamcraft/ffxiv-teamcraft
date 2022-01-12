60967854b7af4de666cd1f5e008cf8c54ea81c0a
Flavien Normand <contact@flavien-normand.fr>
2022-01-12 18:52:00 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,5 +1,16 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** app should now properly open on last visited link.
-* **lists:** fixed a bug preventing lists from updating properly in database.
-* **lists:** fixed navigation breakdown not marking final items as done properly.`;
+* **db:** better icon for spearfishing spot page.
+* **desktop:** possible fix for page not persisted when quitting the app.
+* **gearset:** properly remove offhands on gear sync.
+* **gearsets:** fixed stats effects formulas.
+* **layout:** Crafts and Timed first layout now doesn't hide completed rows by default anymore.
+* **layouts:** sidebar collapse button no longer takes space in smaller layouts.
+* **pricing:** fixed input sometimes acting up when setting a price.
+
+
+### Features
+
+* **db:** support for korean v5.58 update.
+* **desktop:** new icon in sidebar to copy a link to the current page on web.
+* **pricing:** you can now copy earnings or spendings as text.`;
