aa852ed4d1f16d24d794a1016fd3ae17422251e5
Flavien Normand <contact@flavien-normand.fr>
2022-01-14 12:01:03 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,16 +1,17 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** better icon for spearfishing spot page.
-* **desktop:** possible fix for page not persisted when quitting the app.
-* **gearset:** properly remove offhands on gear sync.
-* **gearsets:** fixed stats effects formulas.
-* **layout:** Crafts and Timed first layout now doesn't hide completed rows by default anymore.
-* **layouts:** sidebar collapse button no longer takes space in smaller layouts.
-* **pricing:** fixed input sometimes acting up when setting a price.
+* **community-rotations:** rlvls are now sorted DESC.
+* **db:** fixed some monsters not having drops properly displayed.
+* **lists:** fixed total trades sometimes acting up with materias and other trades.
+* **pricing:** fixed crafting flag and prices not properly filled from previous lists.
+* **pricing:** fixed fill prices on earnings not always getting prices from DC.
+* **pricing:** fixed row amounts not reflecting list row amounts.
 
 
 ### Features
 
-* **db:** support for korean v5.58 update.
-* **desktop:** new icon in sidebar to copy a link to the current page on web.
-* **pricing:** you can now copy earnings or spendings as text.`;
+* **desktop:** new setting to always open on main page.
+* **inventory-optimizer:** new button to generate a list for items used for one recipe.
+* **list-overlay:** added inventory tags to the overlay details.
+* **pricing:** you can now click item names to copy them to clipboard.
+* enable background work by disabling throttling.`;
