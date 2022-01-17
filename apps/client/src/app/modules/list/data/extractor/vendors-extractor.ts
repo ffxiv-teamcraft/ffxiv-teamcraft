1a56b1d8a9a7e428d91a31629febeab78b2ae14f
Flavien Normand <contact@flavien-normand.fr>
2022-01-17 21:11:13 +0100

    fix(db): fixed missing db links on some shops

--- apps/client/src/app/modules/list/data/extractor/vendors-extractor.ts
+++ apps/client/src/app/modules/list/data/extractor/vendors-extractor.ts
@@ -25,28 +25,41 @@ export class VendorsExtractor extends AbstractExtractor<Vendor[]> {
     return combineLatest([
       this.lazyData.getEntry('npcs'),
       this.lazyData.getEntry('gilShopNames'),
-      this.lazyData.getEntry('shops')
+      this.lazyData.getEntry('shops'),
+      this.lazyData.getEntry('monsters')
     ]).pipe(
-      map(([npcs, gilshopNames, shops]) => {
+      map(([npcs, gilshopNames, shops, monsters]) => {
         return uniqBy(shops.filter(shop => {
           return shop.type === 'GilShop' && shop.trades.some(t => t.items.some(i => i.id === itemId));
         }).map(shop => {
           const trade = shop.trades.find(t => t.items.some(i => i.id === itemId));
           return shop.npcs.map(npc => {
-            const npcEntry = npcs[npc];
             const vendor: Vendor = {
               npcId: npc,
               price: trade.currencies[0].amount,
               shopName: gilshopNames[shop.id]
             };
-            if (npcEntry) {
-              const npcPosition = npcEntry.position;
-              if (npcPosition) {
-                vendor.coords = { x: Math.floor((npcPosition as Vector2).x * 10) / 10, y: Math.floor((npcPosition as Vector2).y * 10) / 10 };
-                vendor.zoneId = npcPosition.zoneid;
-                vendor.mapId = npcPosition.map;
+            if (npc > 1000000) {
+              const npcEntry = npcs[npc];
+              if (npcEntry) {
+                const npcPosition = npcEntry.position;
+                if (npcPosition) {
+                  vendor.coords = { x: Math.floor((npcPosition as Vector2).x * 10) / 10, y: Math.floor((npcPosition as Vector2).y * 10) / 10 };
+                  vendor.zoneId = npcPosition.zoneid;
+                  vendor.mapId = npcPosition.map;
+                }
+                vendor.festival = npcEntry.festival || 0;
               }
-              vendor.festival = npcEntry.festival || 0;
+            } else {
+              const monstersEntry = monsters[npc];
+              if (monstersEntry) {
+                const npcPosition = monstersEntry.positions[0];
+                if (npcPosition) {
+                  vendor.coords = { x: Math.floor((npcPosition as Vector2).x * 10) / 10, y: Math.floor((npcPosition as Vector2).y * 10) / 10 };
+                  vendor.zoneId = npcPosition.zoneid;
+                  vendor.mapId = npcPosition.map;
+                }
+              }
             }
             return vendor;
           });
