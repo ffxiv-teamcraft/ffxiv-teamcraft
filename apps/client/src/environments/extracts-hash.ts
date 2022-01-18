89d4f1e8e14c65c9c6ab59411b8d15d75a7ea8fd
Flavien Normand <contact@flavien-normand.fr>
2022-01-18 18:57:34 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/extracts-hash.ts
+++ apps/client/src/environments/extracts-hash.ts
@@ -1,1 +1,1 @@
-export const extractsHash = `db75a5214ae991d9f4a7dae83bd98cac71382189`;
+export const extractsHash = `376489bb80e467c587a45bae563ed1d629442404`;
--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,9 +1,27 @@
 export const patchNotes = `### Bug Fixes
 
-* **gearsets:** fixed specialist crystal and offhand items sometimes polluting gearsets.
-* **mappy:** removed nodes tracking as it's not needed anymore.
-* **pricing:** fixed pricing mode sometimes not loading panels properly.
-* **pricing:** fixed server filter setting not applied properly for marketboard.
+* **alarms:** fixed duplicate map names in custom alarm popup.
+* **db:** fixed "Can contain items" container overflowing.
+* **db:** fixed lvl 10 la noscea node having wrong Y coord.
+* **db:** fixed missing db links on some shops.
+* **db:** fixed wrong collectable flags on some items.
+* **desktop:** fix for slow autofill with non-gathering/crafting obtention.
+* **desktop:** fixed some pages not loading properly when opening the app.
+* **list:** no longer show skip tag if skippable amount is <= 0.
+* **user:** fixed free company ID not applied properly to user profile.
 
 
-### Features`;
+### Features
+
+* **allagan-reports:** added id in [] to avoid confusion, more details on drops as well.
+* **currency-spending:** added Voidrakes and Althyk Lavender.
+* **db:** added monster location to the quick search when known.
+* **inventory-optimizer:** new optimizer for items that are now functioning as NQ.
+* **search:** added gathering node to quick search.
+* **simulator:** new error message for when level or stats are too low for the recipe.
+
+
+### Performance Improvements
+
+* **lists:** improved list details loading performances.
+* **lists:** migrated the history to its own subcollection.`;
