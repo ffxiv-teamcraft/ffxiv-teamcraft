e2e065ea524dcc8702a63d6db8bf7b61b5fb9ff1
Flavien Normand <contact@flavien-normand.fr>
2022-01-20 22:39:55 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,6 +1,19 @@
 export const patchNotes = `### Bug Fixes
 
-* **gearsets:** fixed etro gearset import with identical rings.
-* **inventory-optimizer:** collectables won't be listed as deprecated HQ anymore.
-* **lists:** fixed lists page sometimes not loading properly.
-* **pricing:** prices are now properly populated from vendor prices.`;
+* **commissions:** added not found message when opening a missing commission.
+* **db:** removed collectable icon from some items that cannot be crafted as such anymore.
+* **desktop:** autofill is now faster when not crafting or gathering.
+* **inventory-optimizer:** only consider ingredients for deprecated HQ optimizer.
+* **lists:** fixed FC sharing not always working properly.
+* **lists:** fixed lists page sometimes not loading.
+
+
+### Features
+
+* added min gathering when available in fish tooltips.
+
+
+### Performance Improvements
+
+* **list-details:** fixed performance issue with rows loading useless data.
+* **list-history:** added virtual scrolling to prevent issues loading the popup.`;
