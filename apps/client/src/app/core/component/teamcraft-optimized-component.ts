475934f82334ca5f6bda3d29a6e4a355bc899506
Flavien Normand <contact@flavien-normand.fr>
2022-01-26 16:36:55 +0100

    chore: build fix

--- apps/client/src/app/core/component/teamcraft-optimized-component.ts
+++ apps/client/src/app/core/component/teamcraft-optimized-component.ts
@@ -1,6 +1,9 @@
 import { TeamcraftComponent } from './teamcraft-component';
-import { AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 
+@Component({
+  template: ''
+})
 export class TeamcraftOptimizedComponent extends TeamcraftComponent implements AfterViewInit {
 
   constructor(protected cd: ChangeDetectorRef) {
