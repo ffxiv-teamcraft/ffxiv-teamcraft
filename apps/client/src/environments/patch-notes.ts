28837b58da2311c7a079f2cfb876ff80595f27e0
Flavien Normand <contact@flavien-normand.fr>
2022-01-28 14:25:16 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,12 +1,8 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed list overlay not loading properly.
-* **desynth-guide:** fixed pagination and job list.
-* **list:** fixed attach/detach rotation not working properly.
-* **list:** fixed bulk operations on list rows not working properly.
+* **lists:** fixed various list manipulations, like adding items, not working.
 
 
 ### Performance Improvements
 
-* **db:** more db read optimizations.
-* **list:** hide completed rows and progress bars now update instantly.`;
+* **list:** contribution stats and history features have been disabled for now.`;
