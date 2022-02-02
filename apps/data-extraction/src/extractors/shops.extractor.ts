3d7b84447ba4dae918ed9f64cac77cd27d8ce597
Flavien Normand <contact@flavien-normand.fr>
2022-02-02 17:50:59 +0100

    fix(db): fixed shops using ice shards as currency instead of tomestones

--- apps/data-extraction/src/extractors/shops.extractor.ts
+++ apps/data-extraction/src/extractors/shops.extractor.ts
@@ -181,7 +181,7 @@ export class ShopsExtractor extends AbstractExtractor {
                 entry.id = StaticData.CURRENCIES[entry.id] || entry.id;
               }
 
-              if (specialShop.UseCurrencyType === 4 && entry.id < 10) {
+              if ([2, 4].includes(specialShop.UseCurrencyType) && entry.id < 10) {
                 entry.id = { ...StaticData.CURRENCIES, ...StaticData.TOMESTONES }[entry.id];
               }
               return entry;
