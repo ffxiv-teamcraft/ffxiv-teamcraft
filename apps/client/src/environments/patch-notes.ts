5dff13c8942f92989ede8e5bf503a3911c2d8763
Flavien Normand <contact@flavien-normand.fr>
2022-02-06 18:06:08 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,10 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** SB fish now have the option to add an alarm with fish eyes.
-* **commissions:** disabled possibility to create a commission from an offline list.
-* **desktop:** fixed list overlay tags not up to date with main window.
-* **layout-editor:** fixed list preview.
-* **list:** fixed automatic HQ flags disable checkbox.
-* **list-import:** fixed import from etro with no materias.
-* **lists:** fixed completion popup not showing up when marking final panel as done.
-* **workshops:** fixed drag and drop ordering not saved properly.`;
+* **desktop:** fixed critical crash when completing a list item ingame.`;
