b5def4d714e96aedd5af687bc8f0d18038685436
Flavien Normand <contact@flavien-normand.fr>
2022-03-20 09:15:26 +0100

    feat(core): added tooltip to spawn timers in alarms page and sidebar
    closes #2200

---
+++ apps/client/src/app/core/alarms/timer-tooltip.directive.ts
@@ -0,0 +1,23 @@
+import { ComponentFactoryResolver, Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges, ViewContainerRef } from '@angular/core';
+import { NzTooltipDirective } from 'ng-zorro-antd/tooltip';
+import { DatePipe } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+
+@Directive({
+  selector: '[timerTooltip]'
+})
+export class TimerTooltipDirective extends NzTooltipDirective implements OnChanges {
+
+  @Input('timerTooltip')
+  timerSeconds: number;
+
+  constructor(elementRef: ElementRef, hostView: ViewContainerRef, resolver: ComponentFactoryResolver, renderer: Renderer2,
+              private datePipe: DatePipe, private translate: TranslateService) {
+    super(elementRef, hostView, resolver, renderer);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.title = this.datePipe.transform(new Date(Date.now() + this.timerSeconds * 1000), 'medium', null, this.translate.currentLang);
+    super.ngOnChanges(changes);
+  }
+}
