f7851c049891a259333c97d7bae0d33b55b76c02
Flavien Normand <contact@flavien-normand.fr>
2022-05-03 13:34:45 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,8 +1,12 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** fixed missing ventures data from 6.1 update.
+* **core:** fixed a bug happening randomly with list persistence.
+* **db:** fixed tug name for medium never displayed.
+* **layout:** fixed IS_FROM_HOUSING_VENDOR filter.
+* **lists:** fixed double notification on discord webhook.
+* **log-tracker:** added missing 6.1 recipes.
 
 
 ### Features
 
-* **db:** 6.11 data update.`;
+* **db:** removed ranking components from fish pages.`;
