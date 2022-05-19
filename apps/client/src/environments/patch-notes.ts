756532f5a34a1d4a24c7f7befe8f116beb9ae035
Flavien Normand <contact@flavien-normand.fr>
2022-05-19 18:19:21 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,1 +1,11 @@
-export const patchNotes = ``;
+export const patchNotes = `### Bug Fixes
+
+* **desktop:** fixed an error with jsdelivr loading in China.
+
+
+### Features
+
+* **gearset:** better display for total materia needed.
+* **metrics:** better display logic for label column.
+* **metrics:** hide source column when it's not needed.
+* **metrics:** save table sort accross sessions.`;
