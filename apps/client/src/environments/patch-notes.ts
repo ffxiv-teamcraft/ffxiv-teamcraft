f6793bd5e98c48d23b64d46feebe3a65e3dcfe06
Flavien Normand <contact@flavien-normand.fr>
2022-06-07 21:14:17 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,10 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** load constants from xivcdn instead of jsdelivr for chinese region.`;
+* **marketboard-popup:** change shareReplay() calls to prevent observables staying alive.
+* **packet capture:** prevent world ID updating needlessly.
+
+
+### Features
+
+* **db:** support for 6.15 data.
+* **db:** support for korean v6.01 update.`;
