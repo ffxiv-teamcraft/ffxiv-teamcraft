0cce4f7d6d89a83457eeb6b32bdda93c3a0c9d72
Flavien Normand <contact@flavien-normand.fr>
2022-06-15 10:02:34 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,10 +1,20 @@
 export const patchNotes = `### Bug Fixes
 
-* **data:** fix missing names on various new items.
-* **db:** fix bite times chart not loading.
-* **db:** fix filters reset in fishing misses popup table.
-* **db:** fixed missing translation for deleted comments display.
-* **desktop:** support for 6.15 packet capture changes.
-* **fish-log-tracker:** re-sort/add missing areas/spots.
-* **fish-log-tracker:** reverse area order to be more like in-game log.
-* **log-tracker:** remove lvl mention from fishing spots.`;
+* **db:** add missing log and gathering data from 6.1 update.
+* **db:** added some missing recipes.
+* **desktop:** tentative fix for inventory tracking stopping suddenly.
+* **fishing-overlay:** fixed throw timer not resetting when hooking before bite.
+* **gearsets:** fixed etro import skipping ring materias.
+* **log:** better save logic for completion data.
+* **search:** fixed korean item search.
+* **teams:** fixed discord webhook permission issue.
+
+
+### Features
+
+* **desktop:** better error handling for wrapper errors.
+
+
+### Performance Improvements
+
+* **core:** better Observable cache management.`;
