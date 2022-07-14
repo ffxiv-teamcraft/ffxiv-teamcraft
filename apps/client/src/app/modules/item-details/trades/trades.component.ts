fbb3a4e15c7e4b41ce60a84894ba43f5b44e163f
Flavien Normand <contact@flavien-normand.fr>
2022-07-14 22:46:34 +0200

    feat(db): better layout and performance for trades data display
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- apps/client/src/app/modules/item-details/trades/trades.component.ts
+++ apps/client/src/app/modules/item-details/trades/trades.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ItemDetailsPopup } from '../item-details-popup';
 import { TradeSource } from '../../list/model/trade-source';
 import { Trade } from '../../list/model/trade';
 import { TradeEntry } from '../../list/model/trade-entry';
+import { BehaviorSubject, combineLatest, Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-trades',
@@ -10,27 +12,41 @@ import { TradeEntry } from '../../list/model/trade-entry';
   styleUrls: ['./trades.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TradesComponent extends ItemDetailsPopup {
+export class TradesComponent extends ItemDetailsPopup implements OnChanges {
+
+  private tradeSourcesData$ = new ReplaySubject<TradeSource[]>();
+
+  public isExternalTrades$ = new BehaviorSubject<boolean>(false);
 
   @Input()
-  public externalTradeSources: TradeSource[] = [];
+  set externalTradeSources(ts: TradeSource[]) {
+    this.tradeSourcesData$.next(ts);
+    this.isExternalTrades$.next(true);
+  }
 
   @Input()
   public dbDisplay = false;
 
+  public displayedTrades$ = new BehaviorSubject<number>(5);
+
+  public tradeSources$: Observable<TradeSource[]> = combineLatest([
+    this.tradeSourcesData$,
+    this.displayedTrades$
+  ]).pipe(
+    map(([data, displayed]) => {
+      return data.slice(0, displayed);
+    })
+  );
+
+  public hasMoreAvailable$ = combineLatest([
+    this.tradeSourcesData$,
+    this.displayedTrades$
+  ]).pipe(
+    map(([data, displayed]) => data.length > displayed)
+  );
+
   constructor() {
     super();
-  }
-
-  public get tradeSources(): TradeSource[] {
-    if (this.externalTradeSources.length === 0) {
-      return this.details;
-    }
-    return this.externalTradeSources;
-  }
-
-  public get fromExternalTrades(): boolean {
-    return this.externalTradeSources.length > 0;
   }
 
   public totalPrice(trade: Trade): TradeEntry[] {
@@ -41,6 +57,17 @@ export class TradesComponent extends ItemDetailsPopup {
         amount: Math.ceil(currency.amount * (this.item.amount - this.item.done) / itemsPerTrade)
       };
     });
+  }
+
+  public showMore(): void {
+    this.displayedTrades$.next(this.displayedTrades$.value + 5);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.details && changes.details.currentValue.length > 0) {
+      this.tradeSourcesData$.next(changes.details.currentValue);
+      this.isExternalTrades$.next(false);
+    }
   }
 
 }
