996d69077fde3841d1b5c19ca874ffb1074c8e53
Flavien Normand <contact@flavien-normand.fr>
2022-08-23 20:08:29 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/extracts-hash.ts
+++ apps/client/src/environments/extracts-hash.ts
@@ -1,1 +1,1 @@
-export const extractsHash = `3000b664201cd2bb48db736cfde8af6cdda3912c`;
+export const extractsHash = `5b8cda41d5dfb23bafb1eefba7e2f2589ea5facc`;
--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,11 +1,28 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** fixed broken desynth data links.
-* **inventory-optimizer:** only show recipes you have enough materials for.
-* **list:** fixed trade details not showing in dialog box.
-* **profile:** fix wrong label in character addition button.
+* **alarms:** fix some weather + time issues on fish alarms.
+* **community-rotations:** fixed first visit behavior.
+* **currency-spending:** better feedback when there's no available trades.
+* **db:** fix some broken links to GamerEscape.
+* **db:** removed diadem from FSH log tracker.
+* **desktop:** lighter Machina error messages.
+* **i18n:** better loading system for KO/ZH locales.
+* **inventory:** better error handling behavior.
+* **list:** disable comments on offline lists.
+* **list:** fix disable automatic HQ flags not applying.
+* **map:** fix for tooltips not showing in optimized path.
+* **path-optimizer:** fixed map sometimes not switching properly.
+* **profile:** add character button now adds character, not contact.
+* **profile:** fix stats updating even after closing the autofill popup.
+* **simulator:** fix +/-1 precision issue on quality actions.
+* **ux:** fix fish tooltip position on some resolutions.
 
 
 ### Features
 
-* **gearsets:** you can now clone a gearset from the gearsets page.`;
+* **data:** update for cn patch-6.1.
+* **db:** allagan reports import.
+* **db:** support for 6.2 data.
+* **list:** better NPC rbeakdown grouping logic.
+* **pricing:** copy panel as CSV.
+* **simulator:** support for new quality requirement system.`;
