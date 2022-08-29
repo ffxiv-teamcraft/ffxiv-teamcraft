56e14903c6ac7bd376a3fccaeb5ecd276a5868b2
Flavien Normand <contact@flavien-normand.fr>
2022-08-29 12:41:07 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,1 +1,9 @@
-export const patchNotes = ``;
+export const patchNotes = `### Bug Fixes
+
+* **allagan-reports:** fixed counts being mixed up in topbar link.
+* **desynth:** removed automated extractor to only use allagan reports as resource.
+
+
+### Features
+
+* **lists:** support for Island Sanctuary recipes and buildings.`;
