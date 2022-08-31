32d1fa04b02d008bba1237c9089daf00bda244ef
Flavien Normand <contact@flavien-normand.fr>
2022-08-31 11:44:21 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/extracts-hash.ts
+++ apps/client/src/environments/extracts-hash.ts
@@ -1,1 +1,1 @@
-export const extractsHash = `798e39906d0c9a5d7d8169e0363ea653fbb2cd1a`;
+export const extractsHash = `f3b86ab5d75fc074209dd57a41f9deb89bbf6708`;
--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,9 +1,17 @@
 export const patchNotes = `### Bug Fixes
 
-* **allagan-reports:** fixed counts being mixed up in topbar link.
-* **desynth:** removed automated extractor to only use allagan reports as resource.
+* **alarms:** fixed custom alarms to properly support Island Sanctuary.
+* **allagan-reports:** fixed main page not loading.
+* **db:** fix some island recipes not being added to lists with requirements.
+* **gearset:** fixed materia order optimizer.
+* **list:** fix addition popup not loading Island items properly.
+* **log-tracker:** fixed missing 6.2 fish data.
+* **requirements:** fix requirements popup using wrong name.
+* **simulator:** fix Muscle Memory Primed interaction and steps count.
+* **simulator:** fixed rouding error on some specific quality actions/stats combo.
 
 
 ### Features
 
-* **lists:** support for Island Sanctuary recipes and buildings.`;
+* **data:** update for cn patch-6.11.
+* **desktop:** Island Sanctuary inventory tracking.`;
