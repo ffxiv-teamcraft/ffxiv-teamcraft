69b1b8afc9f57e087ea29d7733d8421786db9afa
Flavien Normand <contact@flavien-normand.fr>
2022-09-11 19:58:23 +0200

    perf(desktop): fixed a big performance issue on app startup
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- apps/client/src/app/core/data-reporting/bnpc-reporter.ts
+++ apps/client/src/app/core/data-reporting/bnpc-reporter.ts
@@ -1,53 +1,24 @@
-import { combineLatest, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ofMessageType } from '../rxjs/of-message-type';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { toIpcData } from '../rxjs/to-ipc-data';
 import { Message } from '@ffxiv-teamcraft/pcap-ffxiv';
 import { DataReporter } from './data-reporter';
 import { debounceBufferTime } from '../rxjs/debounce-buffer-time';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
 
 export class BnpcReporter implements DataReporter {
-  constructor(private apollo: Apollo) {
-  }
-
   getDataReports(packets$: Observable<Message>): Observable<{ bnpcBase: number, bnpcName: number }[]> {
-    const existingReports$ = this.apollo.query<any>({
-      query: gql`query BnpcReporterInitQuery {
-            bnpc {
-              bnpcBase
-              bnpcName
-            }
-          }`,
-      fetchPolicy: 'network-only'
-    }).pipe(
-      map(reports => {
-        return reports.data.bnpc.reduce((acc, report) => {
-          return {
-            ...acc,
-            [report.bnpcBase]: [...(acc[report.bnpcBase] || []), report.bnpcName]
-          };
-        }, {});
-      })
-    );
-    return combineLatest([packets$.pipe(
+    return packets$.pipe(
       ofMessageType('npcSpawn'),
-      toIpcData()),
-      existingReports$
-    ]).pipe(
-      map(([packet, existingReports]) => {
-        if ((existingReports[packet.bNpcBase] || []).includes(packet.bNpcName)) {
-          return null;
-        }
+      toIpcData(),
+      map((packet) => {
         return {
           bnpcBase: packet.bNpcBase,
           bnpcName: packet.bNpcName
         };
       }),
       filter(report => report !== null),
-      debounceBufferTime(10000),
-      tap(console.log)
+      debounceBufferTime(10000)
     );
   }
 
