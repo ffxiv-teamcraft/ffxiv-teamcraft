64b46b97cb6d35f4ee774c7cdb7063b9702b725c
Flavien Normand <contact@flavien-normand.fr>
2022-09-25 20:39:13 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,4 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **island-workshop:** better planning logic for 6-4-6-4 combos.
-* **island-workshop:** removed combo alternatives to prevent optimization issues.`;
+* **island-workshop:** fixed flickering issue on desktop app.`;
