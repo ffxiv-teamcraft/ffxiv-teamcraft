9f27286cb8b6f9dbab2837259cdf2cfe2c6bb35c
Flavien Normand <contact@flavien-normand.fr>
2022-10-09 12:27:53 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,1 +1,15 @@
-export const patchNotes = ``;
+export const patchNotes = `### Bug Fixes
+
+* **collectables:** fix exp formula for exp nerf level comparison.
+* **currency-spending:** fixed cowries and amount sold last week.
+* **desktop:** fixed an issue with fishing reports not sent to the database.
+* **island-workshop:** better planning logic for lower levels.
+* **island-workshop:** fixed pattern recognition for past days and error handling.
+* **island-workshop:** fixed prediction issue and moved planning to the top.
+
+
+### Features
+
+* **db:** imported 144 allagan reports.
+* **db:** support for korean v6.1 update.
+* **loader:** new patron: Darn Tarn and Dam Tam.`;
