b746454f984768d9e0015ed292a295ace9d20a5f
Flavien Normand <contact@flavien-normand.fr>
2022-10-22 09:00:06 +0200

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,3 +1,19 @@
 export const patchNotes = `### Bug Fixes
 
-* **core:** fixed data update failing on gearsets, workshops and other elements.`;
+* **db:** fixed missing recipes from 6.25.
+* **island-workshop:** better planning day availability.
+* **permissions:** fixed an issue preventing permissions from being removed.
+* **permissions:** fixed list access as team member.
+
+
+### Features
+
+* **db:** new charts in fish details page.
+* **db:** new charts in fishing spot details page.
+* **db:** support for korean v6.11 update.
+* **island-workshop:** new section to show materials to prepare for next week.
+
+
+### Performance Improvements
+
+* **core:** better error filter.`;
