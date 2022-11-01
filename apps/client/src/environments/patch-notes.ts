73c3e90cef3a0b38c82ef507923228ab9c36a5e8
Flavien Normand <contact@flavien-normand.fr>
2022-11-01 16:23:13 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,18 +1,11 @@
 export const patchNotes = `### Bug Fixes
 
-* **auth:** fixed an issue with log tracking not updating properly.
-* **core:** better error handling with missing island workshop data.
-* **db:** changed min gathering label and added min gathering data to gathering node details.
-* **db:** fixed alarm button in gathering details not toggling the alarm properly.
-* **db:** fixed weather chart card too small for fish details pages.
-* **island-workshop:** fix for planning changing D2 recommendations on D2 start.
-* **lists:** fixed list options menu not showing correct options.
-* **log-tracking:** fixed marking as done in log not saving properly.
-* **submarine:** fix submarine max rank for kr/cn.
+* **db:** fixed "Show more" button showing when there isn't more to show in trades.
+* **island-workshop:** fixed 20h schedule sometimes happening when no combo found.
+* **log-tracker:** fixed first data updates not working on log state.
+* **log-tracker:** Housing's Other category is now listed as required for achievement.
 
 
 ### Features
 
-* **core:** new github button and stars count that links to the repo (we ♥ stars).
-* **db:** import 54 allagan reports.
-* **workshops:** complete refactor of the permissions propagation system.`;
+* **fishing-overlay:** display when fishing in an unsupported spot.`;
