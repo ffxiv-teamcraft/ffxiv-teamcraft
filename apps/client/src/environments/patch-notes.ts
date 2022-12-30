9c413e2f59fd69731e103946e6d95c3b057c2643
Flavien Normand <contact@flavien-normand.fr>
2022-12-30 15:50:40 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,22 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **db:** fixed monster positions sometimes vanishing after navigation.
-* **db:** fixed Wolf marks sometimes confused with faerie tribe tokens.
-* **desktop:** fixed overlay error after closing it from windows.
-* **gathering-location:** fixed an issue with search breaking after leaving the page.
-* **leveling-equipment:** fixed wrong stat priority for crafting jobs.
-* **list:** fixed list split sometimes forgetting some materials.
-* **list-details:** better UX for list completion popup.
-* **lists:** fixed FC sharing not working with lists.
-* **lists:** fixed log completion on crafting rows not being applied.
-* **lists:** it's no longer possible to archive an offline list.
-* **simulator:** fixed Tricks of the Trade removing Heart and Soul buff.
-
-
-### Features
-
-* **data:** update for cn patch-6.18.
-* **data:** update for cn patch-6.2.
-* **db:** 1200+ allagan reports import.
-* **db:** support for korean v6.15 update.
-* **voyage-tracker:** submarine voyages are now using letter name instead of index.`;
+* **simulator:** fixed an error in icon rendering making it too large for the button.
+* **voyage-tracker:** fixed submarine voyages displayed as object object.`;
