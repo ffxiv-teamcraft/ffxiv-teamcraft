7b77fdb8cb6c938e293b6685f34ea912612ba4ae
Flavien Normand <contact@flavien-normand.fr>
2023-01-15 09:50:43 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,13 +1,12 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed packet capture support for 6.3 update.
-* **gearsets:** fixed clone button in gearsets page not working.
-* **list-details:** fixed notification showing three times on auto mode switch.
-* **list-details:** fixed realtime sharing on two different browsers not working.
-* **search:** fixed html parts not being properly rendered in search results.
-* **trades:** removed more HQ trade currencies.
+* **desktop:** fixed step-by-step overlay not loading.
+* **gathering-location:** fixed type filter not working as intended.
+* **island-workshop:** updated state import message with 6.3 changes.
+* **list:** fixed possible crash after opening a list for too long.
 
 
 ### Features
 
-* **island-sanctuary:** add data for morbol spawn.`;
+* **db:** more allagan reports import.
+* **step-by-step:** better layout and inventory button.`;
