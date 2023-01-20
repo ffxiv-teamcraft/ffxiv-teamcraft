63600b577c01ae03f489d720317e469cafd6c3fe
Flavien Normand <contact@flavien-normand.fr>
2023-01-20 20:00:29 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -4,6 +4,7 @@ export const patchNotes = `### Bug Fixes
 * **lists:** fixed autofill not saving list to database after first item done.
 * **lists:** fixed progress bar display on smaller screen sizes.
 * **lists:** improvements to overlay sync and general list saving method.
+* **simulator:** fixed rotations not opening properly in simulator page.
 * **step-by-step:** fixed display being empty when only crafts are remaining.
 
 
