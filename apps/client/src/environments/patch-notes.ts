add5f98ed89251bc615f3fc93b7aabd84aafd603
Flavien Normand <contact@flavien-normand.fr>
2023-01-20 17:36:40 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,14 +1,12 @@
 export const patchNotes = `### Bug Fixes
 
-* **gathering-location:** fixed search not finding anything with "Any" node type filter.
-* **metrics:** fixed histogram component not loading.
-
-
-### Features
-
-* **db:** allagan reports improt.
+* **alarms:** add small timer comparison margin to prevent alarms order from jumping.
+* **lists:** fixed autofill not saving list to database after first item done.
+* **lists:** fixed progress bar display on smaller screen sizes.
+* **lists:** improvements to overlay sync and general list saving method.
+* **step-by-step:** fixed display being empty when only crafts are remaining.
 
 
 ### Performance Improvements
 
-* **lists:** first step of update system refactoring.`;
+* **list:** better concurrency management with time-based etag.`;
