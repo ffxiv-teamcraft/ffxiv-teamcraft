3db20eaa7c7e288138ba9c5994b5f869d590a385
Flavien Normand <contact@flavien-normand.fr>
2023-01-25 17:50:13 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,4 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **desktop:** fixed inventory tracking and autofill stability.
-* **desktop:** fixed pcap status staying yellow after long cutscenes.`;
+* **core:** fixed some numbers not showing when using PT_BR locale.
+* **desktop:** rollback of recent packet capture changes to fix inventory tracking issues.`;
