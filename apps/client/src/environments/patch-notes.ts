d7d46cd6f506a8a1f94ca1d8e2e72ed98e02547c
Flavien Normand <contact@flavien-normand.fr>
2023-01-25 12:05:20 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,26 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **crafting-replay:** fixed crafting replays not recording properly.
-* **db:** fixed instance rewards sometimes not being listed in details page.
-* **desktop:** better universalis GCF for better reporting and timeout management.
-* **desktop:** prevent autofill crafting/gathering message from showing in overlays.
-* **gearset:** fixed currency total not updating on last materia done.
-* **leveling-equipment:** fixed wrong suggestions for lower level crafting gear.
-* **lists:** fixed offline lists sometimes not updated properly.
-* **search:** removed sort selectors when they cannot be applied to the results.
-* **step-by-step:** fixed Other sources not being displayed.
-* **tooltip:** fixed line break in description not processed properly.
-
-
-### Features
-
-* **db:** 6.31 data update.
-* **desktop:** added a start button in packet capture status tooltip.
-* **island-workshop:** new checkbox to display item metadata in planning.
-* **step-by-step:** added HQ flag on step-by-step display (requires list update).
-
-
-### Performance Improvements
-
-* **alarms:** done checkbox is now saved offline to lower database usage.
-* **desktop:** packet queue optimizations for lower memory usage.`;
+* **desktop:** fixed inventory tracking and autofill stability.
+* **desktop:** fixed pcap status staying yellow after long cutscenes.`;
