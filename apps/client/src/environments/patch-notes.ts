4c80bb86880f92a7f6b24d4bfcf5b885c407b947
Flavien Normand <contact@flavien-normand.fr>
2023-01-29 10:44:19 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,18 +1,5 @@
 export const patchNotes = `### Bug Fixes
 
-* **core:** add structuredClone polyfill for older browsers.
-* **db:** fixed some content not being accessible due to italic characters in their name.
-* **desktop:** fixed omicron tokens listed in island inventory.
-* **food-picker:** now sorting results by picked stat(s) value.
-* **lists:** possible fix for final items sometimes not updating.
-* **step-by-step:** added masterbook icon next to input when required.
-* **step-by-step:** fixed arrows not accurate at all on ultrawide displays.
-* **teams:** fixed discord webhook often sending wrong values in notification.
-* **workshop:** improvements on list deletion workshop update.
-
-
-### Features
-
-* **db:** import of ~500 allagan reports.
-* **gearset:** new button to reset materia and item progression.
-* **gearset:** you can now switch from a class to its job on a gearset.`;
+* **alarms:** fixed alarms sometimes ringing twice.
+* **list:** fixed added items sometimes not updating display properly.
+* **workshop:** filter lists with no name from display.`;
