75a94e344735fc108bfc52eea791ef0dd151a319
Flavien Normand <contact@flavien-normand.fr>
2023-02-08 19:44:00 +0100

    fix(alarms): fix alarms ringing multiple times during early delay
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- apps/client/src/app/core/alarms/alarm-bell.service.ts
+++ apps/client/src/app/core/alarms/alarm-bell.service.ts
@@ -160,9 +160,17 @@ export class AlarmBellService {
             if ((!hasOneGroupEnabled) || !alarm.enabled) {
               return false;
             }
+            const lastPlayed = this.getLastPlayed(alarm);
             // Ceiling on /6 so precision is 1/10
             const timeBeforePlay = Math.round(this.alarmsFacade.getMinutesBefore(date, this.alarmsFacade.getNextSpawn(alarm, date)) / 6) / 10 - this.settings.alarmHoursBefore;
-            return timeBeforePlay <= 0;
+            // Irl alarm duration in ms
+            let irlAlarmDuration = this.eorzeanTime.toEarthTime(alarm.duration * 60) * 1000;
+            // If the alarm has no duration, it's because it has no spawn time and only depends on weather
+            if (irlAlarmDuration === 0) {
+              irlAlarmDuration = this.eorzeanTime.toEarthTime(8 * 60) * 1000;
+            }
+            return Date.now() - lastPlayed >= irlAlarmDuration
+              && timeBeforePlay <= 0;
           });
         })
       ).subscribe(alarmsToPlay => {
