705d6e5cfe9fc4421f5bece249dad1a253b5d539
Flavien Normand <contact@flavien-normand.fr>
2023-02-09 12:29:19 +0100

    chore: patch notes cleanup
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -3,11 +3,9 @@ export const patchNotes = `### Bug Fixes
 * **alarms:** fix alarms ringing multiple times during early delay.
 * **community-rotations:** better support for custom simulator rotations.
 * **db:** add handmade NPC links for gemstone shops.
-* **db:** add handmade NPC links for gemstone shops.
 * **desktop:** support for chinese and korean packet capture.
 * **gearset:** fixed materia edit popup opening multiple times when changing language.
 * **i18n:** fix wrong translation label in masterbooks popup.
-* **island-sanctuary:** fixed state import to history database.
 * **island-sanctuary:** fixed state import to history database.
 * **island-workshop:** fixed days not using translation labels in planning.
 * **island-workshop:** peak patterns updated for 6.3.
@@ -18,4 +16,4 @@ export const patchNotes = `### Bug Fixes
 ### Performance Improvements
 
 * **search:** new search API for better performances.
-* **simulator:** better min stats finder.`;
+* **simulator:** better min stats finder.`;
