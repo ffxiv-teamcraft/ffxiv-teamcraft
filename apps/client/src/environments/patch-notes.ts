9e5ed9dbc0af41b5cc88005c20d8da42a763c942
Flavien Normand <contact@flavien-normand.fr>
2023-02-09 19:37:09 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,21 +1,4 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fix alarms ringing multiple times during early delay.
-* **community-rotations:** better support for custom simulator rotations.
-* **db:** add handmade NPC links for gemstone shops.
-* **desktop:** support for chinese and korean packet capture.
-* **gearset:** fixed materia edit popup opening multiple times when changing language.
-* **i18n:** fix wrong translation label in masterbooks popup.
-* **island-sanctuary:** fixed state import to history database.
-* **island-workshop:** fixed days not using translation labels in planning.
-* **island-workshop:** peak patterns updated for 6.3.
-* **list-aggregates:** fixed reset panel crashing the app.
-* **simulator:** fix macro import with additional auto translate whitespace.
-* **desktop:** fixed lazy data loading not loading at all.
-
-
-### Performance Improvements
-
-* **search:** new search API for better performances.
-* **simulator:** better min stats finder.
-`;
+* **desktop:** better fir for cn/kr packet capture.
+* **search:** fixed search filters not being applied at all.`;
