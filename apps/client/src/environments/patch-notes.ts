6fbcf7bf0ce7e935b7cb47cc89d361e32b83a322
Flavien Normand <contact@flavien-normand.fr>
2023-02-27 15:45:00 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,7 +1,3 @@
 export const patchNotes = `### Bug Fixes
 
-* **layouts:** correctly implement NOT in first line of panel definition.
-* **layouts:** fixed layouts not properly deleted from editor popup.
-* **lodestone:** better character loading and verification for in-app avatars.
-* **step-by-step:** better main city detection for trades.
-* **step-by-step:** no more timer marker overlap.`;
+* **lists:** fixed lists creation being entirely broken.`;
