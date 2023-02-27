f34f2cfc5b5907e423b11539dcdbddfc6171dec3
Flavien Normand <contact@flavien-normand.fr>
2023-02-27 13:33:18 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,17 +1,7 @@
 export const patchNotes = `### Bug Fixes
 
-* **allagan-reports:** fixed list of items with no reports always empty.
-* **db:** fixed db pages not showing reduction sources.
-* **desktop:** prevent open-link ipc event from opening local files.
-* **island-workshop:** allowed items with undetermined C2 peak type to be considered for best item to use.
-* **island-workshop:** fixed schedules ending in 84 when they occur.
-* **island-workshop:** planner now properly assumes worst possible future supply for cowrie calculations.
-* **layouts:** now showing filters on readonly layouts editor.
-* **list:** fixed recipe stars always showing 1 extra star.
-* **lists:** fixed island landmark recipes not loading.
-* **lists:** fixed too many lists or very large lists breaking lazy data system entirely.
-* **log-tracker:** fixed BTN/MIN log tracker pages having no coords or alarms.
-* **search:** fixed stat exclusion checkbox not applied properly.
-* **step-by-step:** fixed beast trades sometimes showing when also from monster drops.
-* **step-by-step:** fixed fishing icon missing on map path.
-* **step-by-step-overlay:** fixed next incomplete map not filtering empty maps.`;
+* **layouts:** correctly implement NOT in first line of panel definition.
+* **layouts:** fixed layouts not properly deleted from editor popup.
+* **lodestone:** better character loading and verification for in-app avatars.
+* **step-by-step:** better main city detection for trades.
+* **step-by-step:** no more timer marker overlap.`;
