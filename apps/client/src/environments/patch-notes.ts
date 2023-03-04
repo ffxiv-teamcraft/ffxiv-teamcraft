be8d7e7cc667f4da7a7bc30776d9f281bfb3f806
Flavien Normand <contact@flavien-normand.fr>
2023-03-04 18:09:29 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,5 +1,13 @@
 export const patchNotes = `### Bug Fixes
 
-* **alarms:** fixed deleted alarms sometimes still ringing.
-* **island-workshop:** better logic for KRCN & global data split.
-* **island(workshop:** filter items based on game version for KR and CN.`;
+* **alarms:** better check for push notification permission before notifying to prevent crash.
+* **allagan-reports:** fixed source change locking form on invalid mode.
+* **collectables:** set nerfed exp to 1000 instead of 10000.
+* **desktop:** fixed fishing reporter overlay.
+* **inventory-optimizer:** remove Market listing from ignored containers.
+* **path-optimizer:** fixed arrow navigation.
+
+
+### Features
+
+* **core:** new fish trains feature.`;
