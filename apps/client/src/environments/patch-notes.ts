36fd1b3c494d4bcf44f905dd4728c46d50809a33
Flavien Normand <contact@flavien-normand.fr>
2023-03-07 09:53:15 +0100

    chore(release): Patch notes update

--- apps/client/src/environments/patch-notes.ts
+++ apps/client/src/environments/patch-notes.ts
@@ -1,9 +1,21 @@
 export const patchNotes = `### Bug Fixes
 
-* **fish-trains:** display improvements and terminus timer addition.
-* **fish-trains:** fixed trains sometimes showing twice in the listing page.
+* **fish-train:** fixed contributions stats never showing.
+* **fish-train:** macro copy button always available in route and closing timer on current step.
+* **fish-train:** removed the ability to join a train as anonymous user.
 
 
 ### Features
 
-* **fish-trains:** added server to trains and datacenter filter in discovery page.`;
+* **db:** 6.35 content update.
+* **fish-train:** added slap and intuition requirements in macros along with better bait details.
+* **fish-train:** new bait breakdown chart.
+* **fish-train:** new fish breakdown chart.
+* **fish-train:** new fish size breakdown chart.
+* **fish-train:** time travelling for replay features on stopped trains.
+* **fish-train:** toggle in contributions chart to filter target fish reports only.
+
+
+### Performance Improvements
+
+* **core:** migrate to apollo 3 for more graphql stability and perfs.`;
