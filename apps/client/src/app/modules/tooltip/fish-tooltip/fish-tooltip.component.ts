22a2edfc772c66b3ae637fa049f4ac9f3243a837
Flavien Normand <contact@flavien-normand.fr>
2023-04-23 19:08:25 +0200

    fix(db): tooltips min gathering value now uses allagan reports data
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- apps/client/src/app/modules/tooltip/fish-tooltip/fish-tooltip.component.ts
+++ apps/client/src/app/modules/tooltip/fish-tooltip/fish-tooltip.component.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { GatheringNodesService } from '../../../core/data/gathering-nodes.service';
-import { GatheringNode } from '@ffxiv-teamcraft/types';
-import { FishingBait } from '@ffxiv-teamcraft/types';
+import { FishingBait, GatheringNode } from '@ffxiv-teamcraft/types';
 import { Observable } from 'rxjs';
-import { FishDataService } from '../../../pages/db/service/fish-data.service';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { observeInput } from '../../../core/rxjs/observe-input';
 
 @Component({
   selector: 'app-fish-tooltip-component',
@@ -14,29 +13,26 @@ import { map } from 'rxjs/operators';
 })
 export class FishTooltipComponent {
 
-  public fshData$: Observable<GatheringNode[]>;
-
-  public minGathering$: Observable<number>;
-
-  constructor(private gatheringNodesService: GatheringNodesService,
-              private fishData: FishDataService) {
-  }
-
-  private _fish: any;
-
-  get fish(): any {
-    return this._fish;
-  }
-
   @Input()
-  set fish(fish: any) {
-    this._fish = fish;
-    this.fshData$ = this.gatheringNodesService.getItemNodes(fish.ID);
-    this.minGathering$ = this.fishData.getStatisticsByFishId(fish.ID).pipe(
-      map(res => {
-        return res.data.stats.aggregate.min.gathering;
-      })
-    );
+  fish: { ID: number, Description: string, ItemKind: number, Icon: string };
+
+  public fshData$: Observable<GatheringNode[]> = observeInput(this, 'fish').pipe(
+    switchMap(fish => {
+      return this.gatheringNodesService.getItemNodes(fish.ID);
+    })
+  );
+
+  public minGathering$: Observable<number> = this.fshData$.pipe(
+    map(data => {
+      return data
+        .map(node => {
+          return node.minGathering;
+        })
+        .sort((a, b) => a - b)[0];
+    })
+  );
+
+  constructor(private gatheringNodesService: GatheringNodesService) {
   }
 
   public trackByNode(index: number, node: GatheringNode): number {
