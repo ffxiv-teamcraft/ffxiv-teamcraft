4635b8cff8c5f81207c1ad75d29264c6118f159f
Matthew <github-gpg@service.matthe815.dev>
2024-04-04 17:26:33 -0400

    refactor: Replace abstraction with Angular 17 functions

--- apps/client/src/app/pages/gearset-folder/gearset-folder/gearset-folder.component.ts
+++ apps/client/src/app/pages/gearset-folder/gearset-folder/gearset-folder.component.ts
@@ -1,10 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { AuthFacade } from '../../../+state/auth.facade';
 import { GearsetsFacade } from '../../../modules/gearsets/+state/gearsets.facade';
 import { FoldersFacade } from '../../../modules/folders/+state/folders.facade';
 import { FolderContentType } from '../../../model/folder/folder-content-type';
 import { ActivatedRoute } from '@angular/router';
-import { SeoTeamcraftComponent } from '../../../core/component/seo-teamcraft-component';
 import { map, takeUntil, tap } from 'rxjs/operators';
 import { combineLatest, Observable } from 'rxjs';
 import { FolderDisplay } from '../../../model/folder/folder-display';
@@ -15,6 +14,8 @@ import { NgIf, AsyncPipe } from '@angular/common';
 import { PageLoaderComponent } from '../../../modules/page-loader/page-loader/page-loader.component';
 import { SeoMetaConfig } from '../../../core/seo/seo-meta-config';
 import { SeoService } from '../../../core/seo/seo.service';
+import { SeoPageComponent } from '../../../core/seo/seo-page-component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-gearset-folder',
@@ -23,19 +24,22 @@ import { SeoService } from '../../../core/seo/seo.service';
     standalone: true,
     imports: [PageLoaderComponent, NgIf, FolderPageComponent, GearsetRowComponent, AsyncPipe]
 })
-export class GearsetFolderComponent extends SeoTeamcraftComponent {
+export class GearsetFolderComponent {
   public userId$ = this.authFacade.userId$;
 
   public display$: Observable<FolderDisplay<TeamcraftGearset>>;
 
   public loading = true;
 
+  private seoService = inject(SeoService)
+
+  private destroyRef = inject(DestroyRef)
+
   constructor(private authFacade: AuthFacade, private gearsetsFacade: GearsetsFacade,
-              private foldersFacade: FoldersFacade, private activatedRoute: ActivatedRoute,
-              private seo: SeoService) {
-    super(seo);
+              private foldersFacade: FoldersFacade, private activatedRoute: ActivatedRoute) {
+
     this.activatedRoute.paramMap.pipe(
-      takeUntil(this.onDestroy$)
+      takeUntilDestroyed(this.destroyRef)
     ).subscribe(paramMap => {
       const key = paramMap.get('folderId');
       this.foldersFacade.load(key);
@@ -49,18 +53,19 @@ export class GearsetFolderComponent extends SeoTeamcraftComponent {
     ).pipe(
       tap(() => this.loading = false)
     );
-  }
 
-  protected getSeoMeta(): Observable<Partial<SeoMetaConfig>> {
-    return combineLatest([this.display$, this.userId$]).pipe(
+    combineLatest([this.display$, this.userId$]).pipe(
       map(([display, userId]) => {
         return {
           title: `${display.folder.name} - FFXIV Teamcraft`,
           description: `A folder containing ${display.content.length} gearsets.`,
           url: `https://ffxivteamcraft.com/user/${userId}/gearsets/folder/${display.folder.$key}`,
           image: 'https://ffxivteamcraft.com/assets/logo.png'
-        };
-      })
-    )
+        }
+      }),
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe((seoData) => {
+      this.seoService.setConfig(seoData)
+    });
   }
 }
