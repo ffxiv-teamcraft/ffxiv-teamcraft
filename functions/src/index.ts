6ecca2e0ae7d973399766d3dd4fd419927bcd976
Flavien Normand <contact@flavien-normand.fr>
2024-06-18 12:10:43 +0200

    chore(functions): migrate updateAppVersion and update functions runtime
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,14 @@
-import { RuntimeOptions } from 'firebase-functions';
+import { https, RuntimeOptions } from 'firebase-functions';
 import { subDays } from 'date-fns';
 import axios from 'axios';
+import * as express from 'express';
 
 const functions = require('firebase-functions');
 require('firebase/app');
 require('firebase/firestore');
 const crypto = require('crypto');
-const admin = require('firebase-admin');
-const { PubSub } = require('@google-cloud/pubsub');
+import admin from 'firebase-admin';
+import { PubSub } from '@google-cloud/pubsub';
 admin.initializeApp();
 const firestore = admin.firestore();
 firestore.settings({ timestampsInSnapshots: true });
@@ -247,7 +248,7 @@ export const firestoreIslandStatePreventiveLock = functions.runWith(runtimeOpts)
         }],
         username: 'Teamcraft Cloud Function'
       }).catch(err => {
-        console.log(`[DISCORD ERROR HOOK] ${err.message}`)
+        console.log(`[DISCORD ERROR HOOK] ${err.message}`);
       });
     }
     await monitoringRef.transaction(current => {
@@ -410,6 +411,7 @@ export const commissionEditionNotifications = functions.runWith(runtimeOpts).fir
       }
     }));
   }
+  return Promise.resolve();
 });
 
 export const subscribeToUserTopic = functions.runWith(runtimeOpts).https.onCall((data, context) => {
@@ -452,7 +454,6 @@ export const userIdValidator = functions.runWith(runtimeOpts).https.onRequest((r
   const userId = request.query.userId;
   if (validatedCache[userId] !== undefined) {
     response.status(200).set('Content-Type', 'application/json').send(`{"valid": ${validatedCache[userId]}}`);
-    return;
   }
   return firestore.collection('users').doc(userId).get().then(snap => {
     validatedCache[userId] = snap.exists;
@@ -501,6 +502,20 @@ export const getUserByEmail = functions.runWith(runtimeOpts).https.onCall((data,
     .catch(error => {
       console.log(error);
     });
+});
+
+export const updateAppVersion = functions.runWith(runtimeOpts).https.onRequest((req: https.Request, res: express.Response) => {
+  res.set('Access-Control-Allow-Origin', '*')
+    .set('Access-Control-Allow-Headers', 'Content-Type');
+  const ref = admin.database().ref('/app_version');
+  const payload = req.body.payload;
+  if (payload.action === 'published' && !payload.release.prerelease && !payload.release.draft) {
+    return ref.set(payload.release.tag_name.replace('v', '')).then(() => {
+      res.sendStatus(200);
+    });
+  } else {
+    return res.sendStatus(201);
+  }
 });
 
 function getTokenClaims(user) {
