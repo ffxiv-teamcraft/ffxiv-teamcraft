f56ff0b0dcfaa74d067d1af19848725c7a561a4b
Flavien Normand <contact@flavien-normand.fr>
2024-07-01 15:26:25 +0200

    chore: error cleanup
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- apps/client/src/app/core/interceptor/error-interceptor.ts
+++ apps/client/src/app/core/interceptor/error-interceptor.ts
@@ -1,16 +1,10 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, delay, retry } from 'rxjs/operators';
-import { NzMessageService } from 'ng-zorro-antd/message';
-import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
-  constructor(private message: NzMessageService, private translate: TranslateService) {
-  }
-
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.url.includes('//localhost:14500')) {
       return next.handle(req).pipe(catchError(() => of(null)));
@@ -20,17 +14,7 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
     return next.handle(req).pipe(
       delay(1100),
-      retry(2),
-      catchError((err: HttpErrorResponse) => {
-        if (req.url.toLowerCase().indexOf('xivapi.com/search') > -1) {
-          this.message.error(this.translate.instant('ERRORS.Http_request_error', { tool: 'XIVAPI Search' }));
-        } else if (req.url.toLowerCase().indexOf('xivapi.com') > -1 && req.url.indexOf('character') === -1) {
-          this.message.error(this.translate.instant('ERRORS.Http_request_error', { tool: 'XIVAPI' }));
-        } else if (req.url.toLowerCase().indexOf('garlandtools.com') > -1) {
-          this.message.error(this.translate.instant('ERRORS.Http_request_error', { tool: 'Garlandtools' }));
-        }
-        return throwError(err);
-      })
+      retry(2)
     );
   }
 
