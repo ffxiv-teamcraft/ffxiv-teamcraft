9de3e98dc108717dc7a4a7a9ba5e28ec861199ce
Flavien Normand <contact@flavien-normand.fr>
2024-11-05 17:44:21 +0100

    chore: update deploy script
    Signed-off-by: Flavien Normand <contact@flavien-normand.fr>

--- tools/cdn/deploy.sh
+++ tools/cdn/deploy.sh
@@ -11,11 +11,11 @@ PACKAGE_VERSION=$(cat package.json \
 echo VERSION ${PACKAGE_VERSION}
 
 touch ~/.ssh/known_hosts
-ssh-keyscan -H ssh.ffxivteamcraft.com >> ~/.ssh/known_hosts
+ssh-keyscan -H 51.83.37.191 >> ~/.ssh/known_hosts
 
 ls ./dist
 
-rsync -avz ./dist/apps/client/* dalamud@ssh.ffxivteamcraft.com:~/cdn.ffxivteamcraft.com/${PACKAGE_VERSION} --log-file ./rsync.log
+rsync -avz ./dist/apps/client/* dalamud@51.83.37.191:~/cdn.ffxivteamcraft.com/${PACKAGE_VERSION} --log-file ./rsync.log
 
 ssh dalamud@51.83.37.191 << EOF
   cp -rn ./cdn.ffxivteamcraft.com/latest/* ./cdn.ffxivteamcraft.com/${PACKAGE_VERSION}/
