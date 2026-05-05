64acdb8936e0c8ece7d0f1d73ee1c894022c79ba
Ra-Workspace <ra.workspace@gmail.com>
2026-05-05 09:27:55 +0900

    chore(simulator): remove simulator-kr/cn aliases as versions are now synchronized

--- apps/client/src/app/core/simulation/simulation.ts
+++ apps/client/src/app/core/simulation/simulation.ts
@@ -1,14 +1,10 @@
 import * as Simulator from '@ffxiv-teamcraft/simulator';
-import * as SimulatorKR from '@ffxiv-teamcraft/simulator-kr';
-import * as SimulatorCN from '@ffxiv-teamcraft/simulator-cn';
 import { Region } from '@ffxiv-teamcraft/types';
 
 export function getSimulator(region: Region): typeof Simulator {
   switch (region) {
     case Region.Korea:
-      return SimulatorKR as any;
     case Region.China:
-      return SimulatorCN as any;
     default:
       return Simulator;
   }
