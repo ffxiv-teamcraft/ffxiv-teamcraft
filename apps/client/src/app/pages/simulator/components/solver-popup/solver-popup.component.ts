2e5f4ec152b7522c6fe43928d019f96107bd0eea
Pawana McKay <pawana.schirkeinst@googlemail.com>
2026-07-24 15:24:02 +0200

    Added Popup Component

---
+++ apps/client/src/app/pages/simulator/components/solver-popup/solver-popup.component.ts
@@ -0,0 +1,71 @@
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { FlexModule } from "@angular/flex-layout";
+import { TranslateModule } from "@ngx-translate/core";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzProgressModule } from "ng-zorro-antd/progress";
+import { ActionComponent } from "../action/action.component";
+import { DialogComponent } from "../../../../../app/core/dialog.component";
+import { Craft, CraftingAction, CrafterStats } from "@ffxiv-teamcraft/simulator";
+import { Subscription } from "rxjs";
+import { SolverService } from "../../service/solver.service";
+import { NzModalRef } from "ng-zorro-antd/modal";
+
+@Component({
+  selector: 'app-solver-popup',
+  templateUrl: './solver-popup.component.html',
+  styleUrls: ['./solver-popup.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    FlexModule,
+    NzProgressModule,
+    NzButtonModule,
+    TranslateModule,
+    ActionComponent
+  ]
+})
+export class SolverPopupComponent extends DialogComponent implements OnInit, OnDestroy {
+  recipe: Craft;
+  stats: CrafterStats;
+  hqIngredients: { id: number; amount: number }[] = [];
+
+  running = true;
+  depth = 0;
+  bestQuality = 0;
+  bestSuccess = false;
+  resultActions: CraftingAction[] = [];
+
+  private sub: Subscription;
+  private solver: SolverService = inject(SolverService);
+  private modalRef: NzModalRef = inject(NzModalRef);
+
+  constructor() {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.patchData();
+    this.sub = this.solver.solve(this.recipe, this.stats, this.hqIngredients).subscribe({
+      next: ({ progress, result }) => {
+        if (progress) {
+          this.depth = progress.depth;
+          this.bestQuality = progress.bestQuality;
+          this.bestSuccess = progress.bestSuccess;
+        }
+        if (result) {
+          this.resultActions = result;
+          this.running = false;
+        }
+      },
+      error: () => this.running = false
+    });
+  }
+
+  apply(): void {
+    this.modalRef.close(this.resultActions);
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+}
