e16b840998064beec5f5431634a1a4d98b8d39d8
Ra-Workspace <ra.workspace@gmail.com>
2026-07-28 17:12:30 +0900

    feat(db): support for korean v7.55 update

--- apps/client/src/environments/game-env.ts
+++ apps/client/src/environments/game-env.ts
@@ -1,6 +1,6 @@
 export const gameEnv = {
-  globalGameVersion: 7.51,
-  koreanGameVersion: 7.51,
-  chineseGameVersion: 7.51,
+  globalGameVersion: 7.55,
+  koreanGameVersion: 7.55,
+  chineseGameVersion: 7.55,
   traditionalChineseGameVersion: 7.11
 };
